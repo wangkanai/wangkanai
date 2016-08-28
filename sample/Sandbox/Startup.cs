8d49ed0bec6d8481373fb84e7f223ba13545a47a
sarin <sarin@wangkanai.com>
2016-08-28 12:47:37 +0700

    add smaple project

---
+++ sample/Sandbox/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Wangkanai.Browser;
+
+namespace Sandbox
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                var device = new DeviceResolver(context.Request).DeviceInfo;
+                await context.Response.WriteAsync($"<p>{context.Request.Headers["User-Agent"]}");                
+                await context.Response.WriteAsync($"<p>{device.Device.ToString()}");
+            });
+        }
+    }
+}
