fae93e1f9eeff41fb984aba62421fcbaa33d03b5
Sarin Na Wangkanai <sarin@wangkanai.com>
2016-08-30 12:57:25 +0700

    adding abstractions repo

---
+++ src/Wangkanai.Browser.Abstractions/IBrowserDetector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wangkanai.Browser.Abstractions
+{
+    public interface IBrowserDetector
+    {
+        string Device { get; }
+        string Platform { get;}
+        string Engine { get; }
+    }
+}
---
+++ src/Wangkanai.Browser/BrowserServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class BrowserServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBrowserDetector(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
