628b72a3856934e66aab52d47afbc8d86c5678da
Sarin Na Wangkanai <sarin@wangkanai.com>
2016-09-02 15:30:15 +0700

    add license comments

--- src/Wangkanai.Browser/IClientBuilder.cs
+++ src/Wangkanai.Browser/IClientBuilder.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿// Copyright (c) 2016 Sarin Na Wangkanai, All Rights Reserved.
+// The GNU GPLv3. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Wangkanai.Browser
 {
