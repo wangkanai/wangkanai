1daa8d63ae6e7fa887953c03c88d457f61ad4ea3
Sarin Na Wangkanai <Sarin@wangkanai.com>
2016-09-10 23:06:57 +0700

    refactoring resolver

--- src/Wangkanai.Browser/Resolvers/DeviceResolver.cs
+++ src/Wangkanai.Browser/Resolvers/DeviceResolver.cs
@@ -1,7 +1,21 @@
 ﻿// Copyright (c) 2016 Sarin Na Wangkanai, All Rights Reserved.
 // The GNU GPLv3. See License.txt in the project root for license information.
 
+using System;
+
 namespace Wangkanai.Browser.Resolvers
 {
-    internal class DeviceResolver { }
+    public sealed class DeviceResolver {
+        private readonly IClientService _service;
+        public DeviceResolver(IClientService service)
+        {
+            if (service == null) throw new ArgumentNullException(nameof(service);
+
+            _service = service;
+        }
+        private Device Resolve()
+        {
+            return new Device();
+        }
+    }
 }
