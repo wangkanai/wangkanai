6ebd2ca1b67089bb87b3be11fd6766bb2c58c355
Sarin Na Wangkanai <sarin@wangkanai.com>
2016-09-19 14:19:24 +0700

    building sandbox for each used case

---
+++ sample/Device/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Wangkanai.Detection.Abstractions;
+
+namespace Sandbox.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IClientInfo _client;        
+
+        public HomeController(IClientInfo client)
+        {
+            _client = client;            
+        }
+
+        public IActionResult Index()
+        {
+            return View(_client);
+        }
+    }
+}
