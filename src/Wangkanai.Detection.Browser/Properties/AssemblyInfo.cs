3edbf940c33234f27a013e6fc0adcef00edce1c5
Sarin Na Wangkanai <sarin@wangkanai.com>
2017-10-01 00:54:24 +0700

    Public 2.0.0 beta1 to nuget

--- src/Wangkanai.Detection.Browser/Properties/AssemblyInfo.cs
+++ src/Wangkanai.Detection.Browser/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sarin Na Wangkanai")]
 [assembly: AssemblyProduct("Wangkanai.Detection.Browser")]
 [assembly: AssemblyTrademark("")]
 
