32db2f0528072083c7cd5b14c35a2f2db4f04fec
Sarin Na Wangkanai <sarin@wangkanai.com>
2017-10-01 18:54:07 +0700

    add Device request extension tests

---
+++ test/Wangkanai.Detection.Device.Test/DeviceRequestExtensionTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Microsoft.AspNetCore.Http;
+using Wangkanai.Detection.Test;
+using Wangkanai.Detection;
+using Moq;
+
+namespace Wangkanai.Detection.Test
+{
+    public class DeviceRequestExtensionTest : DeviceTestAbstract
+    {
+        [Fact]
+        public void CreateNewDeviceFromRequestExtension()
+        {
+            // arrange
+            var context = CreateContext("test");
+            // act
+            var device = context.Request.Device();
+            // assert
+            Assert.Equal((new Device()).Type, device.Type);
+        }
+    }
+}
