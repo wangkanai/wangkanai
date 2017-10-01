e1231964938ea6d8816b9d2ad1d0ebff72ca5054
Sarin Na Wangkanai <sarin@wangkanai.com>
2017-10-01 15:18:56 +0700

    add DeviceNotFoundException tests

---
+++ test/Wangkanai.Detection.Device.Test/DeviceNotFoundExceptionTest.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace Wangkanai.Detection.Test
+{
+    public class DeviceNotFoundExceptionTest
+    {
+        [Fact]
+        public void ExceptionNotNull()
+        {
+            // arrange 
+            var exception = new DeviceNotFoundException("test");
+            // act
+            var message = exception.Message;
+            // assert
+            Assert.Equal("test", message);
+        }
+
+        [Fact]
+        public void ExceptionInvalidDeviceName()
+        {
+            // arrange 
+            var exception = new DeviceNotFoundException("param", "watch", "test");
+            // act
+            var message = exception.Message;
+            // assert
+            Assert.Equal("test\r\nwatch", message);
+
+        }
+    }
+}
