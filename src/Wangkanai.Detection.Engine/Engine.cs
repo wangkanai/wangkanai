77bf9b9286a852090d78f9ff6305b4a1f9e13fd9
Sarin Na Wangkanai <sarin@wangkanai.com>
2017-10-22 13:21:11 +0700

    #16 perform separate of concerns

--- src/Wangkanai.Detection.Engine/Engine.cs
+++ src/Wangkanai.Detection.Engine/Engine.cs
@@ -7,5 +7,7 @@ namespace Wangkanai.Detection
     {
         public string Name { get; set; }
         public string Maker { get; set; }
+        public EngineType Type { get; set; }
+        public Version Version { get; set; }
     }
 }
