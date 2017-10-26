484b2d5a29991785085ec66661ff1d166d4c0306
Sarin Na Wangkanai <sarin@wangkanai.com>
2017-10-26 11:29:27 +0700

    refactoring

---
+++ src/Wangkanai.Detection.Device/CrawlerCollection.cs
@@ -0,0 +1,11 @@
+﻿namespace Wangkanai.Detection
+{
+    internal static class CrawlerCollection
+    {
+        public static string[] Keywords => new string[] {
+            "bot",
+            "slurp",
+            "spider"
+        };
+    }
+}
