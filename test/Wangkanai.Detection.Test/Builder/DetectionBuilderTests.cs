ab02d473c7ffa15135cb91e02356b40434537be1
KostaVlev <kostadinvlev@gmail.com>
2017-11-14 07:52:15 -0500

    Add DetectionBuilder.cs tests.

---
+++ test/Wangkanai.Detection.Test/Builder/DetectionBuilderTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using Wangkanai.Detection.Builder;
+using Xunit;
+
+namespace Wangkanai.Detection.Test.Builder
+{
+    public class DetectionBuilderTests
+    {
+        [Fact]
+        public void Cotr_IServiceCollection_Success()
+        {
+            var serviceCollection = new ServiceCollection();
+            var builder = new DetectionBuilder(serviceCollection);
+
+            Assert.NotNull(builder.Services);
+        }
+
+        [Fact]
+        public void Cotr_Null_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => (new DetectionBuilder(null)));
+        }
+    }
+}
