926df7a07533e3d4d6abb88903816caec2d13098
Wangkanai <sarin@wangkanai.com>
2017-11-28 15:39:23 +0700

    license header

--- test/Wangkanai.Detection.Test/DetectionServiceCollectionExtensionsTests.cs
+++ test/Wangkanai.Detection.Test/DetectionServiceCollectionExtensionsTests.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿// Copyright (c) 2016 Sarin Na Wangkanai, All Rights Reserved.
+// The GNU GPLv3. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
--- test/Wangkanai.Detection.Test/DetectionServiceTests.cs
+++ test/Wangkanai.Detection.Test/DetectionServiceTests.cs
@@ -1,4 +1,7 @@
-﻿
+﻿// Copyright (c) 2016 Sarin Na Wangkanai, All Rights Reserved.
+// The GNU GPLv3. See License.txt in the project root for license information.
+
+
 using Microsoft.AspNetCore.Http;
 using System;
 using Xunit;
