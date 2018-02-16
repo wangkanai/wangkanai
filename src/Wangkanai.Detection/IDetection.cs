c67e57c84a1d6a16b5d1b06e57175d9e01d01406
Sarin Na Wangkanai <sarin@wangkanai.com>
2018-02-16 15:19:45 +0700

    add global detection

---
+++ src/Wangkanai.Detection/IDetection.cs
@@ -0,0 +1,12 @@
+﻿namespace Wangkanai.Detection
+{
+    public interface IDetection
+    {
+        IBrowser Browser { get; }
+        ICrawler Crawler { get; }
+        IDevice Device { get; }
+        IEngine Engine { get; }
+        IPlatform Platform { get; }
+        IUserAgent UserAgent { get; }
+    }
+}
