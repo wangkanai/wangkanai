fa7a57c33f72f1c1b19ff98efdf1166fdd6ff714
Sarin Na Wangkanai <sarin@wangkanai.com>
2019-03-06 12:02:13 +0700

    update license header

--- Detection.Core/test/DetectionVersionExtensionsTest.cs
+++ Detection.Core/test/DetectionVersionExtensionsTest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) 2019 Sarin Na Wangkanai, All Rights Reserved.
+// The Apache v2. See License.txt in the project root for license information.
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
--- Responsive/src/Abstractions/IResponsiveFactory.cs
+++ Responsive/src/Abstractions/IResponsiveFactory.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2018 Sarin Na Wangkanai, All Rights Reserved.
+﻿// Copyright (c) 2019 Sarin Na Wangkanai, All Rights Reserved.
 // The Apache v2. See License.txt in the project root for license information.
 
 namespace Wangkanai.Responsive
