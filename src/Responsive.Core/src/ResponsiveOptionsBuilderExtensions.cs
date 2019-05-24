f7233ba34b3750e47af69e055eafaaff1005880c
Sarin Na Wangkanai <sarin@wangkanai.com>
2019-05-24 14:04:54 +0700

    #101 UseResponsive Options with MapView()

--- src/Responsive.Core/src/ResponsiveOptionsBuilderExtensions.cs
+++ src/Responsive.Core/src/ResponsiveOptionsBuilderExtensions.cs
@@ -8,20 +8,29 @@ namespace Microsoft.AspNetCore.Builder
 {
     public static class ResponsiveOptionsBuilderExtensions
     {
+        public static IResponsiveOptionsBuilder MapView(
+            this IResponsiveOptionsBuilder optionsBuilder,
+            DeviceType target,
+            DeviceType preferred)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Obsolete]
         public static IResponsiveOptionsBuilder DefaultTablet(
             this IResponsiveOptionsBuilder optionsBuilder,
             DeviceType preferred)
         {
             throw new NotImplementedException();
         }
-
+        [Obsolete]
         public static IResponsiveOptionsBuilder DefaultMobile(
             this IResponsiveOptionsBuilder optionsBuilder,
             DeviceType preferred)
         {
             throw new NotImplementedException();
         }
-
+        [Obsolete]
         public static IResponsiveOptionsBuilder DefaultDesktop(
             this IResponsiveOptionsBuilder optionsBuilder,
             DeviceType preferred)
