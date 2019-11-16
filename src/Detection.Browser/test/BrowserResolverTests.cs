ebe2d2e23a7506e4b90f9382689f3cc6e7badc3b
Phil Hunt <phunt@localgovcorp.com>
2019-11-15 22:58:36 -0600

    Add detection for IE 11 (resolves issue #62)

--- src/Detection.Browser/test/BrowserResolverTests.cs
+++ src/Detection.Browser/test/BrowserResolverTests.cs
@@ -38,6 +38,7 @@ namespace Wangkanai.Detection.Test
         [Theory]
         [InlineData("Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)")]
         [InlineData("Mozilla/5.0 (IE 11.0; Windows NT 6.3; Trident/7.0; .NET4.0E; .NET4.0C; rv:11.0) like Gecko")]
+        [InlineData("Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko")]
         public void Resolve_IE(string agent)
         {
             // arrange
