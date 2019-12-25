55ac6a452e6968e3a43de854dc04f83a6d0b7698
Sarin Na Wangkanai <sarin@wangkanai.com>
2019-12-25 07:48:45 +0700

    update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
-./dotnet-install.ps1 -Channel Current -version 2.1.202
+./dotnet-install.ps1 -version 2.1.202
 dotnet --version
 dotnet restore
 dotnet build
 dotnet test
-dotnet pack -c Release -o .\artifacts
+dotnet pack .\src\**\*.csproj -c Release -o .\artifacts
