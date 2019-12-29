5f708e6bd37a9ca3e2ddcce1ecf8238c8704d9ce
Sarin Na Wangkanai <sarin@wangkanai.com>
2019-12-29 08:24:14 +0700

    remove dependency

--- src/Detection.Engine/src/Abstractions/IEngine.cs
+++ src/Detection.Engine/src/Abstractions/IEngine.cs
@@ -1,10 +1,14 @@
 // Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
 // The Apache v2. See License.txt in the project root for license information.
 
+using System;
+
 namespace Wangkanai.Detection
 {
-    public interface IEngine : IManufacturer
+    public interface IEngine
     {
+        string Maker { get; }
+        Version Version { get; }
         EngineType Type { get; }
     }
 }
