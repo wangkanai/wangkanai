f8b6e3d304dc16f037a2e95e61c2b1bf46308984
Sarin Na Wangkanai <sarin@wangkanai.com>
2019-12-29 08:24:14 +0700

    remove dependency

--- src/Detection.Platform/src/Abstractions/IPlatform.cs
+++ src/Detection.Platform/src/Abstractions/IPlatform.cs
@@ -1,10 +1,14 @@
 // Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
 // The Apache v2. See License.txt in the project root for license information.
 
+using System;
+
 namespace Wangkanai.Detection
 {
-    public interface IPlatform : IManufacturer
+    public interface IPlatform
     {
+        string Maker { get; }
+        Version Version { get; }
         OperatingSystem OS { get; }
         Processor CPU { get; }
     }
