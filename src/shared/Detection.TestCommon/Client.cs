b33caffbfac6dc6b53bcd6a934edc06840431c94
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-01-02 21:21:02 +0700

    fixed class name

--- src/shared/Detection.TestCommon/Client.cs
+++ src/shared/Detection.TestCommon/Client.cs
@@ -5,10 +5,10 @@ namespace Wangkanai.Detection
 {
     public class Client
     {
-        public DeviceType Device { get; set; }
-        public BrowserType Browser { get; set; }
-        public PlatformType Platform { get; set; }
+        public Device Device { get; set; }
+        public Browser Browser { get; set; }
+        //public PlatformType Platform { get; set; }
         public EngineType Engine { get; set; }
-        public string Agent { get; set; }
+        public string? Agent { get; set; }
     }
 }
