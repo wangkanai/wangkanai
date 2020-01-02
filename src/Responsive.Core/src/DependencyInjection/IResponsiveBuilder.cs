0a9f15cb282ea6acf404559d3f94de920c98c0b1
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-01-02 21:27:24 +0700

    refactoring to align with asp.net core 3.0 best practise

---
+++ src/Responsive.Core/src/DependencyInjection/IResponsiveBuilder.cs
@@ -0,0 +1,12 @@
+// Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
+// The Apache v2. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Wangkanai.Responsive
+{
+    public interface IResponsiveBuilder
+    {
+        IServiceCollection Services { get; }
+    }
+}
--- src/Responsive.Core/src/DependencyInjection/IResponsiveCoreBuilder.cs
+++ src/Responsive.Core/src/DependencyInjection/IResponsiveCoreBuilder.cs
@@ -1,15 +1,10 @@
-// Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
+﻿// Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
 // The Apache v2. See License.txt in the project root for license information.
 
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Wangkanai.Responsive
 {
-    public interface IResponsiveBuilder
-    {
-        IServiceCollection Services { get; }
-    }
-
     public interface IResponsiveCoreBuilder
     {
         IServiceCollection Services { get; }
