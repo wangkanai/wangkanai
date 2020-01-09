98251ac22d59239ae6ed487ad2b9722f4bd6c705
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-01-09 16:09:06 +0700

    clean obselete method in base resolver

--- src/Detection.Core/src/BaseResolver.cs
+++ src/Detection.Core/src/BaseResolver.cs
@@ -2,7 +2,6 @@
 // The Apache v2. See License.txt in the project root for license information.
 
 using System;
-using System.Linq;
 
 using Microsoft.AspNetCore.Http;
 using Wangkanai.Detection.Services;
@@ -20,7 +19,8 @@ namespace Wangkanai.Detection
         /// Get HttpContext of the application service
         /// </summary>
         protected HttpContext Context => _service.Context;
-        protected readonly IUserAgentService _service;
+
+        private readonly IUserAgentService _service;
 
         public BaseResolver(IUserAgentService service)
             => _service = service ?? throw new ArgumentNullException(nameof(service));
