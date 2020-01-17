719eb3f11540026f5d8229452df77ea0ca7ff907
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-01-17 18:11:45 +0700

    refactor crawler service

--- src/Services/Defaults/DefaultCrawlerService.cs
+++ src/Services/Defaults/DefaultCrawlerService.cs
@@ -2,6 +2,7 @@
 // The Apache v2. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Wangkanai.Detection.DependencyInjection.Options;
 using Wangkanai.Detection.Extensions;
@@ -14,16 +15,21 @@ namespace Wangkanai.Detection.Services
         public bool IsCrawler { get; } = false;
         public Crawler Type { get; } = Crawler.Unknown;
         public Version Version { get; }
+
         private readonly UserAgent _useragent;
+        private readonly DetectionOptions _options;
 
         public DefaultCrawlerService(IUserAgentService useragent, DetectionOptions options)
         {
             _useragent = useragent.UserAgent;
-            Type = FindCrawlerInUserAgent(_useragent);
+            _options = options;
+
+            Type = FindCrawlerInUserAgent(_useragent, _options.Crawler.Others);
             IsCrawler = isCrawler(Type);
             Version = GetVersion(_useragent);
         }
-        private static Crawler FindCrawlerInUserAgent(UserAgent useragent)
+
+        private static Crawler FindCrawlerInUserAgent(UserAgent useragent, List<string> others)
         {
             if (useragent.IsNullOrEmpty())
                 return Crawler.Unknown;
@@ -33,7 +39,9 @@ namespace Wangkanai.Detection.Services
                 if (agent.Contains(name.ToLower()))
                     return (Crawler)Enum.Parse(typeof(Crawler), name);
 
-            //foreach(var name in )
+            foreach (var name in others)
+                if (agent.Contains(name.ToLower()))
+                    return Crawler.Others;
 
             if (useragent.ToString().ToLower().Contains("bot"))
                 return Crawler.Others;
@@ -62,9 +70,15 @@ namespace Wangkanai.Detection.Services
         private static string FindBot(string agent)
         {
             var split = agent.ToString().Split(' ');
-            return split.Where(x => Enum.GetNames(typeof(Crawler))
-                                        .Count(y => x.ToLower().Contains(y.ToLower())) == 1)
+            return split.Where(x => CrawlerLookup(x))
                         .FirstOrDefault();
+        }
+
+        private static bool CrawlerLookup(string x)
+        {
+            var count = Enum.GetNames(typeof(Crawler))
+                       .Count(y => x.ToLower().Contains(y.ToLower()));
+            return count > 0;
         }
 
         private static bool isCrawler(Crawler type)
