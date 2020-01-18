09813b02678859109fbfec8c4d31ec8c18ed655b
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-01-18 14:11:37 +0700

    refactor detection options

---
+++ src/DependencyInjection/Options/CrawlerOptions.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
+// The Apache v2. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace Wangkanai.Detection.DependencyInjection.Options
+{
+    public class CrawlerOptions
+    {
+        public List<string> Others { get; set; } = new List<string>();
+    }
+}
+
--- src/DependencyInjection/Options/DetectionOptions.cs
+++ src/DependencyInjection/Options/DetectionOptions.cs
@@ -1,7 +1,6 @@
 // Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
 // The Apache v2. See License.txt in the project root for license information.
 
-using System.Collections.Generic;
 using Wangkanai.Detection.Models;
 
 namespace Wangkanai.Detection.DependencyInjection.Options
@@ -10,11 +9,6 @@ namespace Wangkanai.Detection.DependencyInjection.Options
     {
         public CrawlerOptions Crawler { get; set; } = new CrawlerOptions();
         public ResponsiveOptions Responsive { get; set; } = new ResponsiveOptions();
-    }
-
-    public class CrawlerOptions
-    {
-        public List<string> Others { get; set; } = new List<string>();
     }
 }
 
