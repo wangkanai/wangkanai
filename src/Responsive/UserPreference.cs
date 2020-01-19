0e583e959164020c6e2695eff14c5f036827c678
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-01-19 18:41:53 +0700

    Refactor the user preference

--- src/Responsive/UserPreference.cs
+++ src/Responsive/UserPreference.cs
@@ -1,30 +1,27 @@
 // Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
 // The Apache v2. See License.txt in the project root for license information.
 
+using System;
 using Wangkanai.Detection.Models;
 
 namespace Wangkanai.Detection.Responsive
 {
     public class UserPreference
     {
-        public string Resolver { get; set; }
-
-        public string Cookie { get; set; }
-
-        public string Preferred => (Resolver != Cookie) ? Cookie : Resolver;
-
         public UserPreference(string resolver, string cookie)
         {
-            if (resolver is null)
-                throw new System.ArgumentNullException(nameof(resolver));
-            if (cookie is null)
-                throw new System.ArgumentNullException(nameof(cookie));
-
-            Resolver = resolver;
-            Cookie = cookie;
+            Resolver = resolver ?? throw new ArgumentNullException(nameof(resolver));
+            Cookie = cookie ?? throw new ArgumentNullException(nameof(cookie));
         }
 
-        public UserPreference(Device resolver, Device cookie)
-            : this(resolver.ToString(), cookie.ToString()) { }
+        public UserPreference(Device resolver, Device cookie) : this(resolver.ToString(), cookie.ToString())
+        {
+        }
+
+        public string Resolver { get; }
+
+        public string Cookie { get; }
+
+        public string Preferred => Resolver != Cookie ? Cookie : Resolver;
     }
 }
