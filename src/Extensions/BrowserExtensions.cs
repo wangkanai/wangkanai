2487d2f40fcc6bd5dda4c0a8df9de151e4fbd3b4
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-01-20 13:51:24 +0700

    clean up browser factory orhpan

---
+++ src/Extensions/BrowserExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Wangkanai.Detection.Extensions
+{
+    internal static class BrowserExtensions
+    {
+        public static bool IsInternetExplorer(this string agent)
+            => true;
+    }
+}
