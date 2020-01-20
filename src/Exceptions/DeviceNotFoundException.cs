dac0759eec93c0a3de423f136303d85d8340f35f
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-01-20 12:21:28 +0700

    clean up orphan code

--- src/Exceptions/DeviceNotFoundException.cs
+++ src/Exceptions/DeviceNotFoundException.cs
@@ -8,34 +8,22 @@ using System.Runtime.CompilerServices;
 
 namespace Wangkanai.Detection
 {
-    public class DeviceNotFoundException : ArgumentException
-    {
-        private readonly string _invalidDeviceName; // unrecognized device name
-
-        public DeviceNotFoundException() : base(DefaultMessage) { }
-
-        public DeviceNotFoundException(string message) : base(message) { }
-
-        public DeviceNotFoundException(string paramName, string message) : base(message, paramName) { }
-
-        public DeviceNotFoundException(string message, Exception innerException) : base(message, innerException) { }
-
-        public DeviceNotFoundException(string message, string invalidDeviceName, Exception innerException) : base(message, innerException)
-            => _invalidDeviceName = invalidDeviceName;
-
-        public DeviceNotFoundException(string paramName, string invalidDeviceName, string message) : base(message, paramName)
-            => _invalidDeviceName = invalidDeviceName;
-
-        private static string DefaultMessage => "Device Not Supported";
-        public virtual string InvalidDeviceName => _invalidDeviceName;
-
-        public override string Message
-        {
-            get
-            {
-                var s = base.Message;
-                return _invalidDeviceName != null ? s + Environment.NewLine + InvalidDeviceName : s;
-            }
-        }
-    }
+    // public class DeviceNotFoundException : ArgumentException
+    // {
+    //     private readonly string _invalidDeviceName; // unrecognized device name
+    //
+    //     public DeviceNotFoundException(string paramName, string invalidDeviceName, string message) : base(message, paramName)
+    //         => _invalidDeviceName = invalidDeviceName;
+    //
+    //     public virtual string InvalidDeviceName => _invalidDeviceName;
+    //
+    //     public override string Message
+    //     {
+    //         get
+    //         {
+    //             var s = base.Message;
+    //             return _invalidDeviceName != null ? s + Environment.NewLine + InvalidDeviceName : s;
+    //         }
+    //     }
+    // }
 }
--- test/Exceptions/DeviceNotFoundExceptionTest.cs
+++ test/Exceptions/DeviceNotFoundExceptionTest.cs
@@ -9,49 +9,27 @@ namespace Wangkanai.Detection.Exceptions
 {
     public class DeviceNotFoundExceptionTest
     {
-        [Fact]
-        public void ExceptionDefaultBehavuor()
-        {
-            // arrange
-            var exception = new DeviceNotFoundException();
-            // act
-            var message = exception.Message;
-            // assert
-            Assert.Equal("Device Not Supported", message);
-        }
+        // [Fact]
+        // public void ExceptionInvalidDeviceName()
+        // {
+        //     // arrange
+        //     var exception = new DeviceNotFoundException("param", "watch", "test");
+        //     // act
+        //     var message = exception.Message;
+        //     // assert
+        //     Assert.Equal("test (Parameter 'param')\r\nwatch", message);
+        // }
 
-        [Fact]
-        public void ExceptionNotNull()
-        {
-            // arrange
-            var exception = new DeviceNotFoundException("test");
-            // act
-            var message = exception.Message;
-            // assert
-            Assert.Equal("test", message);
-        }
-
-        [Fact]
-        public void ExceptionInvalidDeviceName()
-        {
-            // arrange
-            var exception = new DeviceNotFoundException("param", "watch", "test");
-            // act
-            var message = exception.Message;
-            // assert
-            Assert.Equal("test (Parameter 'param')\r\nwatch", message);
-        }
-
-        [Fact]
-        public void ExceptionWithInnerException()
-        {
-            // arrange
-            var inner = new Exception("inner");
-            var exception = new DeviceNotFoundException("test", inner);
-            // act
-            var message = exception.Message;
-            // assert
-            Assert.Equal("test", message);
-        }
+        // [Fact]
+        // public void ExceptionWithInnerException()
+        // {
+        //     // arrange
+        //     var inner = new Exception("inner");
+        //     var exception = new DeviceNotFoundException("test", inner);
+        //     // act
+        //     var message = exception.Message;
+        //     // assert
+        //     Assert.Equal("test", message);
+        // }
     }
 }
