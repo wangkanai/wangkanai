5d15280008ff3e444cee524a42fce39f77ce098e
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-01-21 23:23:52 +0700

    Make Internal class visible to test project using AssemblyInfo.cs

--- src/Extensions/BrowserExtensions.cs
+++ src/Extensions/BrowserExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace Wangkanai.Detection.Extensions
+﻿// Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
+// The Apache v2. See License.txt in the project root for license information.
+
+namespace Wangkanai.Detection.Extensions
 {
     internal static class BrowserExtensions
     {
--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 // Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
 // The Apache v2. See License.txt in the project root for license information.
 
-namespace Wangkanai.Detection.Services
-{
-    internal class MarkerService
-    {
-    }
-}
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Wangkanai.Detection.Test")]
