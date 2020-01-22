ed35e4a8d40b4f4c0ac6a25dbb40c5b0807a7606
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-01-22 13:33:42 +0700

    Do some house cleaning

--- test/Extensions/HttpContextExtensionsTests.cs
+++ test/Extensions/HttpContextExtensionsTests.cs
@@ -27,7 +27,6 @@ namespace Wangkanai.Detection.Hosting
         //public void SetDevice_Null_UserPerference_ThrowsArgumentNullException()
         //{
         //    Assert.Throws<SetDeviceArgumentNullException>(() => ((HttpContext)null).SetDevice(new UserPreference()));
-        //}
 
         //[Fact]
         //public void SetDevice_HttpContext_Null_ThrowsArgumentNullException()
