d64467c0244c89d5e915c1dd1a1a1cdc4c1b5145
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-01-23 17:01:36 +0700

    Add unit test responsive service

--- src/Services/ResponsiveService.cs
+++ src/Services/ResponsiveService.cs
@@ -1,6 +1,7 @@
 // Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
 // The Apache v2. See License.txt in the project root for license information.
 
+using System;
 using Wangkanai.Detection.DependencyInjection.Options;
 using Wangkanai.Detection.Models;
 
@@ -11,7 +12,13 @@ namespace Wangkanai.Detection.Services
         public Device View { get; }
 
         public ResponsiveService(IDeviceService deviceService, DetectionOptions options)
-            => View = GetView(deviceService.Type, options?.Responsive);
+        {
+            if (options == null)
+                options = new DetectionOptions();
+            View = deviceService != null
+                       ? GetView(deviceService.Type, options?.Responsive)
+                       : Device.Desktop;
+        }
 
         private static Device GetView(Device device, ResponsiveOptions options)
             => device switch
---
+++ test/Services/ResponsiveServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using Wangkanai.Detection.DependencyInjection.Options;
+using Wangkanai.Detection.Models;
+using Xunit;
+
+namespace Wangkanai.Detection.Services
+{
+    public class ResponsiveServiceTest
+    {
+        [Fact]
+        public void Ctor_Null()
+        {
+            var resolver = MockResponsiveService(null);
+            Assert.NotNull(resolver);
+            Assert.Equal(Device.Desktop, resolver.View);
+        }
+
+        [Fact]
+        public void Ctor_Null_Options_Null()
+        {
+            var resolver = MockResponsiveService(null, null);
+            Assert.NotNull(resolver);
+            Assert.Equal(Device.Desktop, resolver.View);
+        }
+
+        [Fact]
+        public void DefaultDesktop()
+        {
+
+        }
+        private static ResponsiveService MockResponsiveService(string agent, DetectionOptions options = null)
+        {
+            var service  = MockService.CreateService(agent);
+            var device   = new DeviceService(service);
+            var resolver = new ResponsiveService(device, options);
+            return resolver;
+        }
+    }
+}
