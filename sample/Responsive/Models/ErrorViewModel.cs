87a8d9029fdf74434675a73b8a861cab64dfa209
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-01-23 23:12:29 +0700

    Compare sample responsive with responsive options

---
+++ sample/Responsive/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Responsive.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- sample/Responsive/Startup.cs
+++ sample/Responsive/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Wangkanai.Detection.Models;
 
 namespace Responsive
 {
