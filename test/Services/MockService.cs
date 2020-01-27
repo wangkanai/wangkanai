c3940038ebd25b5d0d26061ec16f8da38b6cacda
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-01-27 10:58:30 +0700

    Refactor Mock Engine Service

--- test/Services/MockService.cs
+++ test/Services/MockService.cs
@@ -12,24 +12,22 @@ namespace Wangkanai.Detection.Services
     [DebuggerStepThrough]
     public static class MockService
     {
-        public static CrawlerService CreateCrawlerService(string agent, DetectionOptions options = null)
+        public static EngineService CreateEngineService(string agent)
         {
             var service  = CreateService(agent);
-            return new CrawlerService(service, options);;
+            var platform = new PlatformService(service);
+            return new EngineService(service, platform);;
         }
         
-        public static DeviceService CreateDeviceService(string value, string header)
-        {
-            var service  = CreateService(value, header);
-            return new DeviceService(service);;
-        }
+        public static CrawlerService CreateCrawlerService(string agent, DetectionOptions options = null) 
+            => new CrawlerService(CreateService(agent), options);
 
-        public static DeviceService CreateDeviceService(string agent)
-        {
-            var service  = CreateService(agent);
-            return new DeviceService(service);
-        }
-        
+        public static DeviceService CreateDeviceService(string value, string header) 
+            => new DeviceService(CreateService(value, header));
+
+        public static DeviceService CreateDeviceService(string agent) 
+            => new DeviceService(CreateService(agent));
+
         public static IUserAgentService CreateService(string agent)
             => MockUserAgentService(agent).Object;
 
