b8a5abbcc977958866288497b2ccbf98d1aa5bec
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-01-27 10:54:03 +0700

    Refactor unit tests to better shared code

--- test/Services/MockService.cs
+++ test/Services/MockService.cs
@@ -4,6 +4,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Moq;
+using Wangkanai.Detection.DependencyInjection.Options;
 using Wangkanai.Detection.Models;
 
 namespace Wangkanai.Detection.Services
@@ -11,15 +12,44 @@ namespace Wangkanai.Detection.Services
     [DebuggerStepThrough]
     public static class MockService
     {
-        private static HttpContext DefaultHttpContext()
-            => new DefaultHttpContext();
+        public static CrawlerService CreateCrawlerService(string agent, DetectionOptions options = null)
+        {
+            var service  = CreateService(agent);
+            return new CrawlerService(service, options);;
+        }
+        
+        public static DeviceService CreateDeviceService(string value, string header)
+        {
+            var service  = CreateService(value, header);
+            return new DeviceService(service);;
+        }
 
+        public static DeviceService CreateDeviceService(string agent)
+        {
+            var service  = CreateService(agent);
+            return new DeviceService(service);
+        }
+        
         public static IUserAgentService CreateService(string agent)
             => MockUserAgentService(agent).Object;
 
         public static IUserAgentService CreateService(string value, string header)
             => MockUserAgentService(value, header).Object;
 
+        #region internal
+        private static HttpContext DefaultHttpContext()
+            => new DefaultHttpContext();
+        
+        private static HttpContext CreateContext(string value)
+            => CreateContext(value, "User-Agent");
+
+        private static HttpContext CreateContext(string value, string header)
+        {
+            var context = DefaultHttpContext();
+            context.Request.Headers.Add(header, new[] {value});
+            return context;
+        }
+        
         private static Mock<IUserAgentService> MockUserAgentService(string value, string header)
             => CreateContext(value, header).SetupUserAgent(null);
 
@@ -34,15 +64,6 @@ namespace Wangkanai.Detection.Services
             service.Setup(f => f.UserAgent).Returns(new UserAgent(agent));
             return service;
         }
-
-        public static HttpContext CreateContext(string value)
-            => CreateContext(value, "User-Agent");
-
-        public static HttpContext CreateContext(string value, string header)
-        {
-            var context = DefaultHttpContext();
-            context.Request.Headers.Add(header, new[] {value});
-            return context;
-        }
+        #endregion
     }
 }
