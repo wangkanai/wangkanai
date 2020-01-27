e0eaeaad3d7a70e2af15536bbdcac4cc8a9012fb
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-01-27 12:03:34 +0700

    Preference tag helper show if been set

---
+++ src/TagHelpers/PreferenceTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Wangkanai.Detection.Services;
+
+namespace Microsoft.AspNetCore.Mvc.TagHelpers
+{
+    [HtmlTargetElement(ElementName, TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class PreferenceTagHelper : TagHelper
+    {
+        private const string ElementName = "preference";
+
+        protected        IHtmlGenerator     Generator { get; }
+        private readonly IPreferenceService _preference;
+
+        public PreferenceTagHelper(IHtmlGenerator generator, IPreferenceService preference)
+        {
+            Generator   = generator;
+            _preference = preference;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            output.TagName = null;
+
+            if (!_preference.IsSet) 
+                output.SuppressOutput();
+        }
+    }
+}
