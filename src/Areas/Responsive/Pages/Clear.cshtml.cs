631c4d4baed9e3c10173f64018b723b268eb2d9b
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-01-28 16:13:29 +0700

    Preference default UI

---
+++ src/Areas/Responsive/Pages/Clear.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Wangkanai.Detection.Areas.Responsive.Pages
+{
+    public class Clear : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ src/Areas/Responsive/Pages/Prefer.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Wangkanai.Detection.Areas.Responsive.Pages
+{
+    public class Prefer : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
