048926a6bc395b446b72edc12b7f950d8f33a06b
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-01-28 17:38:01 +0700

    Add responsive default UI into application builder extension

--- src/DependencyInjection/BuilderExtensions/Responsive.cs
+++ src/DependencyInjection/BuilderExtensions/Responsive.cs
@@ -2,8 +2,13 @@
 // The Apache v2. See License.txt in the project root for license information.
 
 using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
 using Wangkanai.Detection.Hosting;
 using Wangkanai.Detection.Services;
 
@@ -13,7 +18,7 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         public static IDetectionBuilder AddResponsiveService(this IDetectionBuilder builder)
         {
-            if (builder is null)
+            if (builder == null)
                 throw new ArgumentNullException(nameof(builder));
 
             builder.Services.TryAddScoped<IPreferenceService, PreferenceService>();
@@ -24,14 +29,66 @@ namespace Microsoft.Extensions.DependencyInjection
                 {
                     options.ViewLocationExpanders.Add(new ResponsiveViewLocationExpander(ResponsiveViewLocationFormat.Suffix));
                     options.ViewLocationExpanders.Add(new ResponsiveViewLocationExpander(ResponsiveViewLocationFormat.Subfolder));
-                    // options.ViewLocationFormats.Clear();
-                    // options.ViewLocationFormats.Add
-                    //     ("/MyViewsFolder/{1}/{0}" + RazorViewEngine.ViewExtension);
-                    // options.ViewLocationFormats.Add
-                    //     ("/MyViewsFolder/Shared/{0}" + RazorViewEngine.ViewExtension);
                 });
 
+            builder.Services.ConfigureOptions<ResponsiveDefaultUIConfigurationOptions>();
+
             return builder;
+        }
+    }
+
+    internal class ResponsiveDefaultUIConfigurationOptions : IPostConfigureOptions<RazorPagesOptions>
+    {
+        private const string              ResponsiveUIDefaultAreaName = "Responsive";
+        public        IWebHostEnvironment Environment { get; }
+
+        public ResponsiveDefaultUIConfigurationOptions(IWebHostEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void PostConfigure(string name, RazorPagesOptions options)
+        {
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            var convention = new ResponsivePageModelConvention();
+            options.Conventions.AddAreaFolderApplicationModelConvention(
+                ResponsiveUIDefaultAreaName,
+                "/",
+                pam => convention.Apply(pam));
+        }
+    }
+
+    internal class ResponsivePageModelConvention : IPageApplicationModelConvention
+    {
+        public void Apply(PageApplicationModel model)
+        {
+            var defaultUIAttribute = model.ModelType.GetCustomAttribute<ResponsiveDefaultUIAttribute>();
+            if (defaultUIAttribute == null)
+                return;
+
+            ValidateTemplate(defaultUIAttribute.Template);
+            model.ModelType = defaultUIAttribute.Template.GetTypeInfo();
+        }
+
+        private void ValidateTemplate(Type template)
+        {
+            if (template.IsAbstract || template.IsGenericTypeDefinition)
+                throw new InvalidOperationException("Implementation type can't be abstract or generic");
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    internal sealed class ResponsiveDefaultUIAttribute : Attribute
+    {
+        public Type Template { get; }
+
+        public ResponsiveDefaultUIAttribute(Type implementationTemplate)
+        {
+            Template = implementationTemplate;
         }
     }
 }
