1282ce0edcb33ff8af9d8142a2f5809fcf397b43
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-01-28 21:52:25 +0700

    Add working razor class library default

---
+++ UI/Wangkanai.Detection.Design/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Wangkanai.Detection.Design.MyFeature.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
