aa64dbadd0cf12e71a7dec272a8081d8ef0c6e39
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-01-28 23:49:59 +0700

    Let see if adding view controllers area is better solution

---
+++ src/Areas/Detection/Controllers/Preference.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wangkanai.Detection.Areas.Detection.Controllers
+{
+    [Area("Detection")]
+    public class Preference : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Prefer()
+        {
+            return View();
+        }
+
+        public IActionResult Clear()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Wangkanai.Detection.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; } = "";
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
