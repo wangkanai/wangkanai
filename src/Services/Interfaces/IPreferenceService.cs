8fad5e5cc054197f602ef7e1f41696f027fecfd4
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-01-29 13:45:33 +0700

    code clean up

--- src/Services/Interfaces/IPreferenceService.cs
+++ src/Services/Interfaces/IPreferenceService.cs
@@ -5,7 +5,7 @@ namespace Wangkanai.Detection.Services
     public interface IPreferenceService
     {
         public Device Preferred { get; }
-        public bool IsSet { get; }
+        public bool   IsSet     { get; }
         public void   Set(Device preferred);
         public void   Clear();
     }
