f55a80844226af506593fe757330ec0143791105
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-01-30 08:12:14 +0700

    Add Smart Watch to device

--- src/Models/Device.cs
+++ src/Models/Device.cs
@@ -11,9 +11,10 @@ namespace Wangkanai.Detection.Models
         Desktop = 0,      // Windows, Mac, Linux
         Tablet  = 1 << 0, // iPad, Android
         Mobile  = 1 << 1, // iPhone, Android
-        Tv      = 1 << 2, // Samsung, LG
-        Console = 1 << 3, // XBox, Play Station
-        Car     = 1 << 4, // Ford, Toyota
-        IoT     = 1 << 5  // Raspberry Pi
+        Watch   = 1 << 2, // Smart Watchs
+        Tv      = 1 << 3, // Samsung, LG
+        Console = 1 << 4, // XBox, Play Station
+        Car     = 1 << 5, // Ford, Toyota
+        IoT     = 1 << 6  // Raspberry Pi
     }
 }
