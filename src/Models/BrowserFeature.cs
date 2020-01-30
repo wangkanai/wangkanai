63385ad1630e69e8dbcf6337cc08091123eca734
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-01-30 14:00:20 +0700

    Leave out browser features for the time being

--- src/Models/BrowserFeature.cs
+++ src/Models/BrowserFeature.cs
@@ -3,14 +3,17 @@
 
 namespace Wangkanai.Detection.Models
 {
-    public class BrowserFeature
-    {
-        public bool Frames { get; set; } = false;
-        public bool Iframes { get; set; } = false;
-        public bool Cookie { get; set; } = false;
-        public bool JavaScript { get; set; } = false;
-        public bool VbScript { get; set; } = false;
-        public bool JavaApplets { get; set; } = false;
-        public bool ActiveX { get; set; } = false;
-    }
+    /// <summary>
+    /// Not Implementing yet, might be in the future.
+    /// </summary>
+    // public class BrowserFeature
+    // {
+    //     public bool Frames { get; set; } = false;
+    //     public bool Iframes { get; set; } = false;
+    //     public bool Cookie { get; set; } = false;
+    //     public bool JavaScript { get; set; } = false;
+    //     public bool VbScript { get; set; } = false;
+    //     public bool JavaApplets { get; set; } = false;
+    //     public bool ActiveX { get; set; } = false;
+    // }
 }
