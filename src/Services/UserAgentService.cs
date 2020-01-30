1ac91573f55e348613de646ab4b1e060a04b3975
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-01-30 17:16:36 +0700

    Refactor HttpContext

--- src/Services/UserAgentService.cs
+++ src/Services/UserAgentService.cs
@@ -2,6 +2,7 @@
 // The Apache v2. See License.txt in the project root for license information.
 
 using System;
+using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Wangkanai.Detection.Models;
@@ -13,17 +14,18 @@ namespace Wangkanai.Detection.Services
         public HttpContext Context   { get; }
         public UserAgent   UserAgent { get; }
 
-        public UserAgentService(IServiceProvider services)
+        public UserAgentService(IServiceProvider services)//, HttpContext context)
         {
             if (services == null)
                 throw new ArgumentNullException(nameof(services));
 
             Context = services.GetRequiredService<IHttpContextAccessor>().HttpContext;
 
-            if (Context.IsNull())
+            if (Context == null)
                 throw new ArgumentNullException(nameof(Context));
 
-            UserAgent = Context.GetUserAgent();
+            var agent = Context.Request.Headers["User-Agent"].FirstOrDefault();
+            UserAgent = new UserAgent(agent);
         }
     }
 }
