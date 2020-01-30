8a38b85047871eb240bfd73272f449d991b119cf
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-01-30 10:17:43 +0700

    Add Mock Test Client #268

---
+++ test/Mocks/MockClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using Wangkanai.Detection.Models;
+using Xunit;
+
+namespace Wangkanai.Detection.Mocks
+{
+    internal static class MockClient
+    {
+        public static HttpRequestMessage CreateRequest(Device agent, string url = "/") 
+            => CreateRequest(agent.ToString(), url);
+
+        public static HttpRequestMessage CreateRequest(string agent, string url = "/")
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.Add("User-Agent", agent);
+            return request;
+        }
+    }
+}
