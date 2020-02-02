e7d29ba942bf5f41ebd9b92b8cfa7480a1b32f8d
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-02-02 11:50:20 +0700

    Razor Pages View Location Expander container to work on

--- src/DependencyInjection/BuilderExtensions/Responsive.cs
+++ src/DependencyInjection/BuilderExtensions/Responsive.cs
@@ -2,8 +2,10 @@
 // The Apache v2. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Wangkanai.Detection.Hosting;
 using Wangkanai.Detection.Services;
@@ -44,6 +46,7 @@ namespace Microsoft.Extensions.DependencyInjection
             {
                 options.ViewLocationExpanders.Add(new ResponsiveViewLocationExpander(ResponsiveViewLocationFormat.Suffix));
                 options.ViewLocationExpanders.Add(new ResponsiveViewLocationExpander(ResponsiveViewLocationFormat.Subfolder));
+                options.ViewLocationExpanders.Add(new PageViewLocationExpander());
             });
 
         private static IServiceCollection AddRazorPageLocation(this IServiceCollection services)
--- src/Hosting/PageLocationExpander.cs
+++ src/Hosting/PageLocationExpander.cs
@@ -1,18 +1,38 @@
 using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Wangkanai.Detection.Models;
 
 namespace Wangkanai.Detection.Hosting
 {
-    public class ResponsivePageLocationExpander : IViewLocationExpander{
-        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
-        {
-            throw new NotImplementedException();
-        }
-
+    public class ResponsivePageLocationExpander : IViewLocationExpander
+    {
+        private const string ValueKey = "device-page";
         public void PopulateValues(ViewLocationExpanderContext context)
         {
-            throw new NotImplementedException();
+            if (context == null) 
+                throw new ArgumentNullException(nameof(context));
+
+            context.Values[ValueKey] = context.ActionContext.HttpContext.GetDevice().ToString();
+        }
+
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            if (context == null) 
+                throw new ArgumentNullException(nameof(context));
+            if (viewLocations == null) 
+                throw new ArgumentNullException(nameof(viewLocations));
+
+            context.Values.TryGetValue(ValueKey, out var value);
+
+            if (string.IsNullOrEmpty(value))
+                return viewLocations;
+
+            Enum.TryParse(value, true, out Device device);
+
+            // What should I manipulate? 
+            return viewLocations;
         }
     }
 }
