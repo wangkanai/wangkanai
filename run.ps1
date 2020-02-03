351d64dba87400fe90e31a3f4e6e49fd2d1fe7da
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2020-02-03 10:43:09 +0700

    comment out dotnet pack -p for now

--- run.ps1
+++ run.ps1
@@ -3,4 +3,4 @@ dotnet --version
 dotnet restore
 dotnet build -c Release
 dotnet test
-dotnet pack -c Release -o .\artifacts 
+dotnet pack -c Release -o .\artifacts # -p:nuspecfile=..\detection.nuspec
