0ac8363e3a7f5f9303883bddaad8861bd89d8c55
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-02-08 22:20:33 +0700

    add license header

--- sample/Detection/Models/DetectionViewModel.cs
+++ sample/Detection/Models/DetectionViewModel.cs
@@ -1,4 +1,7 @@
-﻿namespace Detection.Models
+﻿// Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
+// The Apache v2. See License.txt in the project root for license information.
+
+namespace Detection.Models
 {
     public class DetectionViewModel
     {
