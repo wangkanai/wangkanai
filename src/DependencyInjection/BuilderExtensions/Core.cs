6d09514103e6e9212b962b88e205547a6a63405d
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-02-10 22:42:28 +0700

    Change OperatingSystem to Platform
    Their is a conflict with System.OperatingSystem and our OperatingSystem. Therefore, its better to change it to Platform that reflect more to our domain.

--- src/DependencyInjection/BuilderExtensions/Core.cs
+++ src/DependencyInjection/BuilderExtensions/Core.cs
@@ -1,7 +1,6 @@
 // Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
 // The Apache v2. See License.txt in the project root for license information.
 
-using System;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
 using Wangkanai.Detection.DependencyInjection.Options;
--- src/Extensions/UserAgentExtensions.cs
+++ src/Extensions/UserAgentExtensions.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.AspNetCore.Http;
 using Wangkanai.Detection.Models;
 
 namespace Wangkanai.Detection.Extensions
