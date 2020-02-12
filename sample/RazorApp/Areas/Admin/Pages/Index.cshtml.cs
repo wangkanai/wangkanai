0e1baf69cbc3b11836942db02193ff5299cfb997
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-02-12 07:53:57 +0700

    Add Razor Pages in Areas
    Look like Layout is not working yet with Areas. Another todo

---
+++ sample/RazorApp/Areas/Admin/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorApp.Areas.Admin.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
