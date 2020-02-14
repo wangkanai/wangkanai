bfb6da2fff38d03fabf3c4642506060315e3ba45
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-02-14 21:32:19 +0700

    Adding the first page route model convention test

--- test/Hosting/PageRouteModelConventionTest.cs
+++ test/Hosting/PageRouteModelConventionTest.cs
@@ -1,13 +1,22 @@
-﻿using Xunit;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Xunit;
 
 namespace Wangkanai.Detection.Hosting
 {
     public class PageRouteModelConventionTest
     {
         [Fact]
-        public void EnsureValidPageName_ThrowsIfPageNameIsNullOrEmpty()
+        public void Apply_Responsive()
         {
-            
+            var relativePath   = "";
+            var viewEnginePath = "";
+            var areaName       = "";
+
+            var model      = new PageRouteModel(relativePath, viewEnginePath, areaName);
+            var convention = new ResponsivePageRouteModelConvention();
+            convention.Apply(model);
+            Assert.Same("", model.Selectors.Single().AttributeRouteModel.Template);
         }
     }
 }
