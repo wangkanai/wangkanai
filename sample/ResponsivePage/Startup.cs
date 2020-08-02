cd34cab3100a6847a1960a43e37c6a2a7ff1372e
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-08-02 11:20:57 +0700

    add ipad mini testing

--- sample/ResponsivePage/Startup.cs
+++ sample/ResponsivePage/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Wangkanai.Detection.Models;
 
 namespace ResponsivePage
 {
@@ -23,7 +24,10 @@ namespace ResponsivePage
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDetection();
+            services.AddDetection(options =>
+            {
+                //options.Responsive.DefaultTablet.Tablet = Device.Mobile;
+            });
 
             services.AddRazorPages();
         }
