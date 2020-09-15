da17197939a7eda9bd07f0da4d4c0d47f7f46283
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-09-15 17:37:39 +0700

    dotnet build

--- run.ps1
+++ run.ps1
@@ -1,6 +1,7 @@
-./dotnet-install.ps1 -version 2.1.810
+#./dotnet-install.ps1 -version 2.1.810
 dotnet --version
 dotnet restore
 dotnet build
+dotnet publish
 dotnet test
 dotnet pack -c Release -o .\artifacts
