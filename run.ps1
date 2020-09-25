f11a3e48a1618355d9595e90e39fc3d87fe902da
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-09-26 05:32:31 +0700

    publish 2.0.1 fixed

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
-./dotnet-install.ps1 -version 2.1.810
+#./dotnet-install.ps1 -version 2.1.810
 dotnet --version
 dotnet restore
-dotnet build
-dotnet test
-dotnet pack -c Release -o .\artifacts
+dotnet build -c Release -p:Version=2.0.1
+# dotnet test
+dotnet pack -c Release -p:PackageVersion=2.0.1 -o C:\Users\sarin\source\Detection\artifacts
