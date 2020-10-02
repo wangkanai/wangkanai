51423ece0c55ff06da06d0da624b8e285133a25c
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-10-02 19:19:54 +0700

    add strong name key

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 // Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
 // The Apache v2. See License.txt in the project root for license information.
 
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Wangkanai.Detection.Test")]
+[assembly: AssemblyKeyFile("key.snk")]
