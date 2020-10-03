efb761c92af69e372724be3f2e0ee766eb5e1a79
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-10-03 22:28:23 +0700

    clean up and public alpha 12

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,8 +1,7 @@
 // Copyright (c) 2014-2020 Sarin Na Wangkanai, All Rights Reserved.
 // The Apache v2. See License.txt in the project root for license information.
 
-using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 [assembly: InternalsVisibleTo("Wangkanai.Detection.Test")]
-[assembly: AssemblyKeyFile("key.snk")]
