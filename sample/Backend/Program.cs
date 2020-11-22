a28647356abd43bc394cab2b536863553c57716a
Sarin Na Wangkanai <sarin@wangkanai.com>
2020-11-22 13:49:38 +0700

    Nullable cleanup

---
+++ sample/Backend/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+using Wangkanai.Detection.Services;
+
+namespace Backend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var agent   = "useragent";
+            var context = new DefaultHttpContext();
+            context.Request.Headers["User-Agent"] = agent;
+
+            var accessor  = new HttpContextAccessor {HttpContext = context};
+            var useragent = new UserAgentService(accessor);
+            Console.WriteLine(useragent.UserAgent);
+        }
+    }
+}
