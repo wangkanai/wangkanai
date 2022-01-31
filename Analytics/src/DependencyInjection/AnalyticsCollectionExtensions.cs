713db98c9444bf5e954cb4316f25c3e923bdd53a
wangkanai <sarin@wangkanai.com>
2022-01-31 08:03:52 +0700

    add analytics project to repo

---
+++ Analytics/src/DependencyInjection/AnalyticsCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Wangkanai.Analytics.DependencyInjection.Options;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class AnalyticsCollectionExtensions
+    {
+        public static IAnalyticsBuilder AddAnalytics(this IServiceCollection services, Action<AnalyticsOptions> setAction)
+            => services.Configure(setAction)
+                       .AddAnalytics();
+        
+        public static IAnalyticsBuilder AddAnalytics(this IServiceCollection services)
+            => services.AddAnalyticsBuilder();
+        
+        internal static IAnalyticsBuilder AddAnalyticsBuilder(this IServiceCollection services)
+            => new AnalyticsBuilder(services);
+    }
+}
---
+++ Analytics/test/Mocks/MockServer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Wangkanai.Analytics.DependencyInjection.Options;
+
+namespace Wangkanai.Analytics.Tests.Mocks
+{
+    public static class MockServer
+    {
+        public static TestServer Server()
+            => Server(WebHostBuilder());
+
+        public static TestServer Server(Action<AnalyticsOptions> options)
+            => Server(WebHostBuilder(options));
+
+        public static TestServer Server(IWebHostBuilder builder)
+            => new TestServer(builder);
+
+        public static IWebHostBuilder WebHostBuilder()
+            => WebHostBuilder(options => { });
+
+        public static IWebHostBuilder WebHostBuilder(Action<AnalyticsOptions> options)
+            => new WebHostBuilder()
+               .ConfigureServices(services => services.AddAnalytics(options))
+               .Configure(app => { app.Run(ContextHandler()); });
+
+        private static RequestDelegate ContextHandler()
+            => context => context.Response.WriteAsync("Analytics");
+    }
+}
