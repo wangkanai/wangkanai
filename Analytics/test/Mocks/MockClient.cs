4dd84083670c788239a60fa73524971ce62769cb
wangkanai <sarin@wangkanai.com>
2022-02-18 15:36:08 +0700

    refactor analytics

--- Analytics/test/Mocks/MockClient.cs
+++ Analytics/test/Mocks/MockClient.cs
@@ -1,14 +1,13 @@
 ﻿using System.Net.Http;
 
-namespace Wangkanai.Analytics.Tests.Mocks
+namespace Wangkanai.Analytics.Tests.Mocks;
+
+public static class MockClient
 {
-    public static class MockClient
+    public static HttpRequestMessage RequestMessage(string url = "/")
     {
-        public static HttpRequestMessage RequestMessage(string url = "/")
-        {
-            var request = new HttpRequestMessage(HttpMethod.Get, url);
-            request.Headers.Add("User-Agent", "analytics");
-            return request;
-        }
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+        request.Headers.Add("User-Agent", "analytics");
+        return request;
     }
 }
