16882150846eecc582efa4417c8d9d3019d5ab60
wangkanai <sarin@wangkanai.com>
2022-02-19 11:00:47 +0700

    update analytics

---
+++ Analytics/src/Builder/AnalyticsApplicationExtensions.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using Microsoft.AspNetCore.Builder;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Pipeline extension methods for adding Analytics
+/// </summary>
+public static class AnalyticsApplicationExtensions
+{
+    public static IApplicationBuilder UseAnalytics(this IApplicationBuilder app)
+    {
+        Check.NotNull(app);
+        
+        // todo
+        
+        return app;
+    }
+}
