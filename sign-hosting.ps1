4e4a071be62fa298de30d5bc3747c87a754cd079
wangkanai <sarin@wangkanai.com>
2022-06-23 13:13:01 +0700

    update project dependency

---
+++ sign-hosting.ps1
@@ -0,0 +1,18 @@
+$suffix = "alpha";
+
+push-location -path .\hosting\
+dotnet --version
+dotnet clean .\src\
+dotnet restore .\src\
+dotnet build .\src\ -c Release #--version-suffix $suffix
+signtool sign /n "Sarin Na Wangkanai" .\src\bin\release\net6.0\Wangkanai.Hosting.dll
+Remove-Item .\artifacts\*.*
+dotnet pack .\src\ -c Release -o .\artifacts #--version-suffix $suffix
+nuget sign .\artifacts\*.nupkg `
+  -CertificateStoreLocation CurrentUser `
+  -CertificateStoreName My `
+  -CertificateSubjectName 'Sarin Na Wangkanai' `
+  -Timestamper http://ts.ssl.com `
+  -OutputDirectory .\signed
+
+pop-location
