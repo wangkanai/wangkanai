21c5bfccb4d7d456bf771cd7d1e11e0d00627217
wangkanai <sarin@wangkanai.com>
2022-06-24 09:55:16 +0700

    Add Hosting CI Testing

--- Hosting/src/Services/MarkerService.cs
+++ Hosting/src/Services/MarkerService.cs
@@ -4,7 +4,6 @@ using Wangkanai.Hosting.Abstractions;
 
 namespace Wangkanai.Hosting.Services;
 
-internal class MarkerService: IMarkerService
+internal class MarkerService : IMarkerService
 {
-    
 }
