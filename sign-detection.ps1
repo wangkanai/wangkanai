f7cc377b7f19716a049d98c1ba20da87b9f74cd0
wangkanai <sarin@wangkanai.com>
2022-08-20 10:04:21 +0700

    update detection with symbols

--- sign-detection.ps1
+++ sign-detection.ps1
@@ -7,12 +7,17 @@ dotnet restore .\src\
 dotnet build .\src\ -c Release #--version-suffix $suffix
 signtool sign /n "Sarin Na Wangkanai" .\src\bin\Release\net6.0\Wangkanai.Detection.dll
 Remove-Item .\artifacts\*.*
-dotnet pack .\src\ -c Release -o .\artifacts #--version-suffix $suffix
+dotnet pack .\src\ -c Release -o .\artifacts --include-symbols -p:SymbolPackageFormat=snupkg #--version-suffix $suffix
 nuget sign .\artifacts\*.nupkg `
   -CertificateStoreLocation CurrentUser `
   -CertificateStoreName My `
   -CertificateSubjectName 'Sarin Na Wangkanai' `
   -Timestamper http://ts.ssl.com `
   -OutputDirectory .\signed
-
+nuget sign .\artifacts\*.snupkg `
+  -CertificateStoreLocation CurrentUser `
+  -CertificateStoreName My `
+  -CertificateSubjectName 'Sarin Na Wangkanai' `
+  -Timestamper http://ts.ssl.com `
+  -OutputDirectory .\signed
 pop-location
