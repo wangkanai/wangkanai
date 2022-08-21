e9f3eb2b31c46d9cadd42ab9bfc3d9dfc2c9a816
wangkanai <sarin@wangkanai.com>
2022-08-21 13:47:41 +0700

    update package with symbol server

--- sign-universal.ps1
+++ sign-universal.ps1
@@ -5,7 +5,7 @@ dotnet --version
 dotnet clean .\src\
 dotnet restore .\src\
 dotnet build .\src\ -c Release #--version-suffix $suffix
-signtool sign /n "Sarin Na Wangkanai" .\src\bin\release\net6.0\Wangkanai.u = Universal.dll
+signtool sign /n "Sarin Na Wangkanai" .\src\bin\release\net6.0\Wangkanai.Universal.dll
 Remove-Item .\artifacts\*.*
 dotnet pack .\src\ -c Release -o .\artifacts --include-symbols -p:SymbolPackageFormat=snupkg #--version-suffix $suffix
 nuget sign .\artifacts\*.nupkg `
