069090222460825c15be754cdd380f827aebb6e1
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2022-09-01 02:27:27 +0700

    update tabler demo

---
+++ Tabler/src/Components/SvgIconComponent.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using Microsoft.AspNetCore.Components;
+
+namespace Wangkanai.Tabler.Components;
+
+public class SvgIconComponent : BaseTablerComponent
+{
+    [Parameter]
+    public string Name { get; set; } = string.Empty;
+}
