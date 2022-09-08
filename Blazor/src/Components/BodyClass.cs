c3ec378a71c87836fd1dd7a15be9e1660b756b0d
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2022-09-08 23:43:20 +0700

    add blazor body class css changer

---
+++ Blazor/src/Components/BodyClass.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Microsoft.AspNetCore.Components.Web;
+
+public class BodyClass : ComponentBase
+{
+    [Parameter] public string Default { get; set; } = default!;
+    [Parameter] public string Add     { get; set; } = default!;
+    [Parameter] public string Remove  { get; set; } = default!;
+}
