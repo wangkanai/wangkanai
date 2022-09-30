001070ff25e0ade03afcb5246ac083db24ea5c66
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2022-09-30 17:54:05 +0700

    Code Signing dotnet tool

---
+++ Sign/src/Internal/IConsole.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Sign.Internal;
+
+public interface IConsole
+{
+    event ConsoleCancelEventHandler CancelKeyPress;
+}
