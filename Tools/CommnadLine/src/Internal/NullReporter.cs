81e1e7882dd2fdae2bf1ed61ab2e5c146160d0c7
sarin <sarin@wangkanai.com>
2022-10-26 15:19:37 +0700

    completed CommandLineApplication common features

---
+++ Tools/CommnadLine/src/Internal/NullReporter.cs
@@ -0,0 +1,15 @@
+// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Extensions.CommandLine.Internal;
+
+public class NullReporter : IReporter
+{
+    private NullReporter() { }
+
+    public static IReporter Singleton { get; } = new NullReporter();
+
+    public void Warn(string    message) { }
+    public void Error(string   message) { }
+    public void Output(string  message) { }
+    public void Verbose(string message) { }
+}
