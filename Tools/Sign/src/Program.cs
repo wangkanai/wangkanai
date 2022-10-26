338bf55edec72434496efff843ff9cb28c4574e6
sarin <sarin@wangkanai.com>
2022-10-26 17:01:25 +0700

    Add AnsiConsole

--- Tools/Sign/src/Program.cs
+++ Tools/Sign/src/Program.cs
@@ -1,10 +1,8 @@
 // Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
 using System.IO;
-using System.Windows.Input;
 
-using Wangkanai.Reflection;
-using Wangkanai.Sign.Internal;
+using Wangkanai.Extensions.CommandLine.Internal;
 
 namespace Wangkanai.Sign;
 
