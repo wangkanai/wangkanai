79698728aa99d414278dc9997673c914ba5bdefb
Sarin Na Wangkanai <sarin@wangkanai.com>
2022-11-23 22:28:18 +0700

    add TypeExtensions.cs

--- Runtime/src/Usings.cs
+++ Runtime/src/Usings.cs
@@ -1,6 +1,7 @@
 // Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
 global using System;
+global using System.Collections;
 global using System.Collections.Generic;
 global using System.Diagnostics;
 global using System.Linq;
