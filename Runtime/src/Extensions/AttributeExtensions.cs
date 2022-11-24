bc52e87044c56c344d9664e19a1ac568dc38b960
Sarin Na Wangkanai <sarin@wangkanai.com>
2022-11-24 11:51:52 +0700

    add AttributeExtensions.cs to runtime

---
+++ Runtime/src/Extensions/AttributeExtensions.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Extensions;
+
+public static class AttributeExtensions
+{
+    public static string[] SplitString(this Attribute attribute, string original, char separator)
+        => string.IsNullOrEmpty(original)
+               ? Array.Empty<string>()
+               : original.Split(separator)
+                         .Select(x => x.Trim())
+                         .Where(x => !string.IsNullOrEmpty(x))
+                         .ToArray();
+}
