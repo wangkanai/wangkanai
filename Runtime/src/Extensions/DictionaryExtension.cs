3f887ced7f31f2014c07f3b0d762e0a01973783e
Sarin Na Wangkanai <sarin@wangkanai.com>
2022-11-24 12:50:53 +0700

    add DictionaryExtension.cs to runtime

---
+++ Runtime/src/Extensions/DictionaryExtension.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using System.Net.Http.Headers;
+
+namespace Wangkanai.Extensions;
+
+public static class DictionaryExtension
+{
+    public static TValue GetValueOrThrow<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, string exceptionMessage)
+        => !dictionary.TryGetValue(key, out var value)
+               ? throw new KeyNotFoundException(exceptionMessage)
+               : value;
+
+    /// <summary>
+    /// Doesn't throw an exception when the key is null or does not exist
+    /// </summary>
+    public static TValue GetValueSafe<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        => dictionary.GetValueSafe(key, default);
+    
+    /// <summary>
+    /// Doesn't throw an exception when the key is null or does not exist
+    /// </summary>
+    public static TValue GetValueSafe<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue) 
+        => key != null && dictionary.TryGetValue(key, out var value) 
+               ? value 
+               : defaultValue;
+
+    /// <summary>
+    /// Doesn't throw an exception when the key is null
+    /// </summary>
+    public static bool TryGetValueSafe<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, out TValue value)
+    {
+        if (key != null)
+            return dictionary.TryGetValue(key, out value);
+
+        value = default;
+
+        return false;
+    }
+}
