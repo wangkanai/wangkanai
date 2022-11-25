c904d0c39d8f739da4933f9448987be5e0c79dd7
Sarin Na Wangkanai <sarin@wangkanai.com>
2022-11-25 21:14:12 +0700

    add TreeExtensions.cs

---
+++ Runtime/src/Extensions/TreeExtensions.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+
+namespace Wangkanai.Extensions;
+
+public static class TreeExtensions
+{
+    public static IEnumerable<T> Traverse<T>(this T node, Func<T, IEnumerable<T>> children)
+    {
+        yield return node;
+
+        var childNodes = children(node);
+        if(children != null)
+            foreach (var child in childNodes.SelectMany(n=> n.Traverse(children)))
+                yield return child;
+    }
+
+    public static IEnumerable<TItem> GetAncestors<TItem>(TItem item, Func<TItem, TItem> getParentFunc)
+    {
+        Check.NotNull(getParentFunc);
+        
+        if(ReferenceEquals(item, null))
+            yield break;
+        
+        for(TItem curItem = getParentFunc(item); !ReferenceEquals(curItem, null); curItem = getParentFunc(curItem))
+            yield return curItem;
+    }
+}
