345dd8c14da31af2f53f28807c6bbdadecfd151f
Sarin Na Wangkanai <sarin@wangkanai.com>
2022-11-26 07:48:41 +0700

    publish extensions

--- sign-extensions.ps1
+++ sign-extensions.ps1
@@ -1,25 +1,39 @@
 push-location -path .\extensions\
 
-dotnet --version
-dotnet clean .\src\
-dotnet restore .\src\
-dotnet build .\src\ -c Release
-Get-ChildItem .\src\ -Recurse Wangkanai.*.dll  | foreach {
-    signtool sign /fd SHA256 /n "Sarin Na Wangkanai" $_.FullName
-}
-Remove-Item .\artifacts\*.*
-dotnet pack .\src\ -c Release -o .\artifacts --include-symbols -p:SymbolPackageFormat=snupkg
-nuget sign .\artifacts\*.nupkg `
-  -CertificateStoreLocation CurrentUser `
-  -CertificateStoreName My `
-  -CertificateSubjectName 'Sarin Na Wangkanai' `
-  -Timestamper http://ts.ssl.com `
-  -OutputDirectory .\signed
-nuget sign .\artifacts\*.snupkg `
-  -CertificateStoreLocation CurrentUser `
-  -CertificateStoreName My `
-  -CertificateSubjectName 'Sarin Na Wangkanai' `
-  -Timestamper http://ts.ssl.com `
-  -OutputDirectory .\signed
+remove-item .\signed\*.*
+new-item -Path signed -ItemType Directory -Force
 
+get-childitem .\ -directory | where {$_.Name -ne 'signed'} | foreach{
+
+    push-location -path $_.Name
+    
+    Remove-Item .\artifacts\*.*
+    remove-item .\signed\*.*
+
+    dotnet --version
+    dotnet clean .\src\
+    dotnet restore .\src\
+    dotnet build .\src\ -c Release
+    Get-ChildItem .\src\ -Recurse Wangkanai.*.dll  | foreach {
+        signtool sign /fd SHA256 /n "Sarin Na Wangkanai" $_.FullName
+    }
+
+    dotnet pack .\src\ -c Release -o .\artifacts --include-symbols -p:SymbolPackageFormat=snupkg
+    nuget sign .\artifacts\*.nupkg `
+      -CertificateStoreLocation CurrentUser `
+      -CertificateStoreName My `
+      -CertificateSubjectName 'Sarin Na Wangkanai' `
+      -Timestamper http://ts.ssl.com `
+      -OutputDirectory .\signed
+    nuget sign .\artifacts\*.snupkg `
+      -CertificateStoreLocation CurrentUser `
+      -CertificateStoreName My `
+      -CertificateSubjectName 'Sarin Na Wangkanai' `
+      -Timestamper http://ts.ssl.com `
+      -OutputDirectory .\signed
+    
+    copy-item -path .\signed\*.* -destination .\..\signed\ -force
+    
+    pop-location
+}
 pop-location
