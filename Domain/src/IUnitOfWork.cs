02fb871b2c5c47dbe92bb7b09429010deed959e8
Sarin Na Wangkanai <sarin@wangkanai.com>
2022-11-28 00:45:30 +0700

    add unit of work & repository

---
+++ Domain/src/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using System.Threading.Tasks;
+
+namespace Wangkanai.Domain;
+
+public interface IUnitOfWork
+{
+    int       Commit();
+    Task<int> CommitAsync();
+}
