1b1a7f85d3dbba24b15426ee46d116777ac50b03
Sarin Na Wangkanai <sarin@wangkanai.com>
2022-11-29 11:05:46 +0700

    add date and time factory method extensions

---
+++ System/src/Extensions/TimeFactory/TimeSpanFactoryExtensions.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Extensions.TimeFactory;
+
+public static class TimeSpanFactoryExtensions
+{
+    public static TimeSpan Ticks(this int ticks)
+        => TimeSpan.FromTicks(ticks);
+
+    public static TimeSpan Milliseconds(this int milliseconds)
+        => TimeSpan.FromMilliseconds(milliseconds);
+
+    public static TimeSpan Second(this int seconds)
+        => TimeSpan.FromSeconds(seconds);
+
+    public static TimeSpan Minutes(this int minutes)
+        => TimeSpan.FromMinutes(minutes);
+
+    public static TimeSpan Hour(this int hours)
+        => TimeSpan.FromHours(hours);
+
+    public static TimeSpan Days(this int days)
+        => TimeSpan.FromHours(days);
+}
