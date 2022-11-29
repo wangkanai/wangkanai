b26221e896ccc2bb0cb3739dfdd9e886eb662a0f
Sarin Na Wangkanai <sarin@wangkanai.com>
2022-11-29 12:28:54 +0700

    Domain Driven Design Core Components

---
+++ Domain/src/Events/IEvent.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using System;
+
+using Wangkanai.Domain.Messages;
+
+namespace Wangkanai.Domain.Events;
+
+public interface IEvent : IEntity, IMessage
+{
+    int            Version    { get; set; }
+    DateTimeOffset TimeOffset { get; set; }
+}
---
+++ Domain/src/Messages/ICancellableHandler.cs
@@ -0,0 +1,11 @@
+// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Wangkanai.Domain.Messages;
+
+public interface ICancellableHandler<in T> where T : IMessage
+{
+    Task HandleAsync(T message, CancellationToken token = default);
+}
