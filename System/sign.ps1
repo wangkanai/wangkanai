9a7632f0f7aa7f2155b7a950bc55bb18e6759eb1
Sarin Na Wangkanai <sarin@wangkanai.com>
2022-12-22 17:29:15 +0700

    fixed dotnet nuget sign

--- System/sign.ps1
+++ System/sign.ps1
@@ -11,6 +11,7 @@ Remove-Item .\artifacts\*.*
 dotnet pack .\src\ -c Release -o .\artifacts --include-symbols -p:SymbolPackageFormat=snupkg
 
 signtool sign /fd SHA256 /tr http://ts.ssl.com /td sha256 /n "Sarin Na Wangkanai" .\artifacts\*.nupkg 
+
 dotnet nuget sign .\artifacts\*.nupkg -v diag --timestamper http://timestamp.digicert.com --certificate-subject-name "Sarin Na Wangkanai" -o .\signed 
 dotnet nuget sign .\artifacts\*.snupkg -v diag --timestamper http://timestamp.digicert.com --certificate-subject-name "Sarin Na Wangkanai" -o .\signed
 
