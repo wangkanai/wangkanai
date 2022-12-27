d394115dd76dc08c741b9c828e0b58b76b5262ad
Sarin Na Wangkanai <sarin@wangkanai.com>
2022-12-27 16:02:50 +0700

    publish extensions

--- Extensions/sign.ps1
+++ Extensions/sign.ps1
@@ -6,17 +6,17 @@ get-childitem .\ -directory | where { $_.Name -ne 'signed' } | foreach{
     push-location -path $_.Name
 
     Remove-Item .\artifacts\*.*
-    remove-item .\signed\*.*
+    Remove-Item .\signed\*.*
 
     dotnet --version
     dotnet clean .\src\
     dotnet restore .\src\
     dotnet build .\src\ -c Release
     Get-ChildItem .\src\ -Recurse Wangkanai.*.dll | where { $_.Name -like "*release*" } | foreach {
-        signtool sign /fd SHA256 /n "Sarin Na Wangkanai" $_.FullName
+        signtool sign /fd SHA256 /tr http://ts.ssl.com /td sha256 /n "Sarin Na Wangkanai" $_.FullName
     }
 
-    dotnet pack .\src\ -c Release -o .\artifacts --include-symbols -p:SymbolPackageFormat = snupkg
+    dotnet pack .\src\ -c Release -o .\artifacts --include-symbols -p:SymbolPackageFormat=snupkg
 
     dotnet nuget sign .\artifacts\*.nupkg -v diag --timestamper http://timestamp.digicert.com --certificate-subject-name "Sarin Na Wangkanai" -o .\signed
     dotnet nuget sign .\artifacts\*.snupkg -v diag --timestamper http://timestamp.digicert.com --certificate-subject-name "Sarin Na Wangkanai" -o .\signed
@@ -27,4 +27,4 @@ get-childitem .\ -directory | where { $_.Name -ne 'signed' } | foreach{
 }
 
 dotnet nuget push .\signed\*.nupkg -k $env:NUGET_API_KEY -s https://api.nuget.org/v3/index.json --skip-duplicate
-dotnet nuget push .\signed\*.snupkg -k $env:NUGET_API_KEY -s https://api.nuget.org/v3/index.json --skip-duplicate
+# dotnet nuget push .\signed\*.snupkg -k $env:NUGET_API_KEY -s https://api.nuget.org/v3/index.json --skip-duplicate
