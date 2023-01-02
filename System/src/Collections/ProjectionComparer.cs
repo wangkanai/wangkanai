91d6ec059e6e32e82478651e55c2bfe6a41912b9
Sarin Na Wangkanai <sarin@wangkanai.com>
2023-01-02 11:28:07 +0700

    working concept of Projection Comparer

--- System/src/Collections/ProjectionComparer.cs
+++ System/src/Collections/ProjectionComparer.cs
@@ -4,5 +4,36 @@ namespace Wangkanai.Collections;
 
 public static class ProjectionComparer
 {
-		
+	public static ProjectionComparer<TSource, TKey> Create<TSource, TKey>(Func<TSource, TKey> projection)
+		=> new(projection);
+
+	public static ProjectionComparer<TSource, TKey> Create<TSource, TKey>(TSource ignored, Func<TSource, TKey> projection)
+		=> new(projection);
+}
+
+public static class ProjectionComparer<TSource>
+{
+	public static ProjectionComparer<TSource, TKey> Create<TKey>(Func<TSource, TKey> projection)
+		=> new(projection);
+}
+
+public class ProjectionComparer<TSource, TKey> : IComparer<TSource>
+{
+	private readonly Func<TSource, TKey> _projection;
+	private readonly IComparer<TKey>     _comparer;
+
+	public ProjectionComparer(Func<TSource, TKey> projection, IComparer<TKey> comparer = null)
+	{
+		_projection = projection.ThrowIfNull();
+		_comparer   = comparer ?? Comparer<TKey>.Default;
+	}
+
+	public int Compare(TSource x, TSource y)
+		=> (x, y) switch
+		{
+			(null, null) => 0,
+			(null, _)    => -1,
+			(_, null)    => 1,
+			_            => _comparer.Compare(_projection(x), _projection(y))
+		};
 }
---
+++ System/tests/Collections/ProjectionComparerTests.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using Xunit;
+
+namespace Wangkanai.Collections;
+
+public class ProjectionComparerTest
+{
+	static readonly NameAndNumber a10 = new("Aaaa", 5);
+	static readonly NameAndNumber b15 = new("Bbbb", 15);
+
+	[Fact]
+	public void ProjectToStringWithIgnoredParameter()
+	{
+		IComparer<NameAndNumber> comparer = ProjectionComparer.Create(a10, x=> x.Name);
+		TestComparisons(comparer);
+	}
+
+	private static void TestComparisons(IComparer<NameAndNumber> comparer)
+	{
+		Assert.Equal(0, comparer.Compare(a10, a10));
+		Assert.Equal(0, comparer.Compare(b15, b15));
+		Assert.Equal(0, comparer.Compare(null, null));
+		
+		Assert.True(comparer.Compare(null, a10) < 0);
+		Assert.True(comparer.Compare(a10, null) > 0);
+		
+		Assert.True(comparer.Compare(a10, b15) < 0);
+		Assert.True(comparer.Compare(b15, a10) > 0);
+	}
+}
+
+record NameAndNumber(string Name, int Number);
