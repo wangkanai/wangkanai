d2b254578bdb8d9f071de51765619aac432fca46
Sarin Na Wangkanai <sarin@wangkanai.com>
2023-01-09 18:11:15 +0700

    completed reverse comparer unit tests

---
+++ System/tests/Collections/RangeIteratorTests.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using Xunit;
+
+namespace Wangkanai.Collections;
+
+public class RangeIteratorTests
+{
+	[Fact]
+	public void InclusiveRange() { }
+}
---
+++ System/tests/Collections/ReverseComparerTests.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using Xunit;
+
+namespace Wangkanai.Collections;
+
+public class ReverseComparerTests
+{
+	[Fact]
+	public void NullComparer()
+	{
+		Assert.Throws<ArgumentNullException>(() => new ReverseComparer<int>(null));
+	}
+
+	[Fact]
+	public void NormalComparer()
+	{
+		var original = StringComparer.Ordinal;
+		var subject  = new ReverseComparer<string>(original);
+
+		Assert.Equal(original.Compare("x", "y"), subject.Compare("y", "x"));
+		Assert.Equal(0, subject.Compare("x", "x"));
+	}
+}
