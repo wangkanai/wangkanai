0ef838cd85057108c104f34dc7f5b6b168bd5520
Sarin Na Wangkanai <sarin@wangkanai.com>
2023-01-10 11:08:49 +0700

    add static random to system

--- System/src/StaticRandom.cs
+++ System/src/StaticRandom.cs
@@ -4,12 +4,35 @@ namespace Wangkanai;
 
 public static class StaticRandom
 {
-	static Random _random = new Random();
-	static object _lock = new object();
+	static readonly Random _random = new Random();
+	static readonly object _lock = new object();
 	
 	public static int Next()
 	{
-		lock (_lock)
-			return _random.Next();
+		lock (_lock) return _random.Next();
+	}
+	
+	public static int Next(int max)
+	{
+		lock (_lock) return _random.Next(max);
+	}
+	
+	public static int Next(int min, int max)
+	{
+		lock (_lock) return _random.Next(min, max);
+	}
+	
+	public static double NextDouble()
+	{
+		lock (_lock) return _random.NextDouble();
+	}
+
+	public static void NextBytes(byte[] buffer)
+	{
+		lock (_lock) _random.NextBytes(buffer);
+	}
+	public static void NextBytes(Span<byte> buffer)
+	{
+		lock (_lock) _random.NextBytes(buffer);
 	}
 }
