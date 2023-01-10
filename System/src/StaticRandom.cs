57073934550b89781d106147f70d273577302a04
Sarin Na Wangkanai <sarin@wangkanai.com>
2023-01-10 10:59:20 +0700

    Create static random helper class

---
+++ System/src/StaticRandom.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai;
+
+public static class StaticRandom
+{
+	static Random _random = new Random();
+	static object _lock = new object();
+	
+	public static int Next()
+	{
+		lock (_lock)
+			return _random.Next();
+	}
+}
