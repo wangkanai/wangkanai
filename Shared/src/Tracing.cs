598b916bd042c864798a31cc29f16fd9e67936af
Sarin Na Wangkanai <sarin@wangkanai.com>
2023-01-11 20:38:55 +0700

    complete stack tracing

--- Shared/src/Tracing.cs
+++ Shared/src/Tracing.cs
@@ -13,15 +13,37 @@ internal static class Tracing
 	/// </summary>
 	public static string ServiceVersion => AssemblyVersion.ToString(3);
 
+	/// <summary>
+	/// Service name for base tracing
+	/// </summary>
 	public static ActivitySource BasicActivitySource { get; } = new(TraceNames.Basic, ServiceVersion);
+
+	/// <summary>
+	/// Service name for store tracing
+	/// </summary>
 	public static ActivitySource StoreActivitySource { get; } = new(TraceNames.Store, ServiceVersion);
+
+	/// <summary>
+	/// Service name for cache tracing
+	/// </summary>
+	public static ActivitySource CacheActivitySource { get; } = new(TraceNames.Cache, ServiceVersion);
+
+	/// <summary>
+	/// Service name for service tracing
+	/// </summary>
+	public static ActivitySource ServiceActivitySource { get; } = new(TraceNames.Service, ServiceVersion);
+
+	/// <summary>
+	/// Service name for validation tracing
+	/// </summary>
+	public static ActivitySource ValidationActivitySource { get; } = new(TraceNames.Validation, ServiceVersion);
 
 	public static class TraceNames
 	{
 		public static string Basic      => "Wangkanai";
 		public static string Store      => Basic + ".Store";
 		public static string Cache      => Basic + ".Cache";
-		public static string Services   => Basic + ".Services";
+		public static string Service    => Basic + ".Service";
 		public static string Validation => Basic + ".Validation";
 	}
 }
