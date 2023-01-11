71323e5d5c1356ccc3730cbf1c829354266a2e50
Sarin Na Wangkanai <sarin@wangkanai.com>
2023-01-11 20:33:35 +0700

    Add stack tracing to the core library

--- Shared/src/LinkerFlags.cs
+++ Shared/src/LinkerFlags.cs
@@ -6,18 +6,20 @@ namespace Wangkanai.Internal;
 
 internal static class LinkerFlags
 {
-    /// <summary>
-    ///     Flags for a member that is JSON (de)serialized.
-    /// </summary>
-    public const DynamicallyAccessedMemberTypes JsonSerialized = DynamicallyAccessedMemberTypes.PublicConstructors | DynamicallyAccessedMemberTypes.PublicFields | DynamicallyAccessedMemberTypes.PublicProperties;
+	/// <summary>
+	///     Flags for a member that is JSON (de)serialized.
+	/// </summary>
+	public const DynamicallyAccessedMemberTypes JsonSerialized = DynamicallyAccessedMemberTypes.PublicConstructors |
+	                                                             DynamicallyAccessedMemberTypes.PublicFields |
+	                                                             DynamicallyAccessedMemberTypes.PublicProperties;
 
-    /// <summary>
-    ///     Flags for a component
-    /// </summary>
-    public const DynamicallyAccessedMemberTypes Component = DynamicallyAccessedMemberTypes.All;
+	/// <summary>
+	///     Flags for a component
+	/// </summary>
+	public const DynamicallyAccessedMemberTypes Component = DynamicallyAccessedMemberTypes.All;
 
-    /// <summary>
-    ///     Flags for a JSInvokable type.
-    /// </summary>
-    public const DynamicallyAccessedMemberTypes JSInvokable = DynamicallyAccessedMemberTypes.PublicMethods;
+	/// <summary>
+	///     Flags for a JSInvokable type.
+	/// </summary>
+	public const DynamicallyAccessedMemberTypes JSInvokable = DynamicallyAccessedMemberTypes.PublicMethods;
 }
---
+++ Shared/src/Tracing.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using System.Diagnostics;
+
+namespace Wangkanai.Internal;
+
+internal static class Tracing
+{
+	private static readonly Version AssemblyVersion = typeof(Tracing).Assembly.GetName().Version;
+
+	/// <summary>
+	/// Service version
+	/// </summary>
+	public static string ServiceVersion => AssemblyVersion.ToString(3);
+
+	public static ActivitySource BasicActivitySource { get; } = new(TraceNames.Basic, ServiceVersion);
+	public static ActivitySource StoreActivitySource { get; } = new(TraceNames.Store, ServiceVersion);
+
+	public static class TraceNames
+	{
+		public static string Basic      => "Wangkanai";
+		public static string Store      => Basic + ".Store";
+		public static string Cache      => Basic + ".Cache";
+		public static string Services   => Basic + ".Services";
+		public static string Validation => Basic + ".Validation";
+	}
+}
