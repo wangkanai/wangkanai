c3de6ac1be888fb2044ee9c4170edf276979404f
Sarin Na Wangkanai <sarin@wangkanai.com>
2023-01-15 11:33:09 +0700

    todo: DiscoveryResponseMaker completion

--- Federation/src/Federation/Responses/DiscoveryResponseMaker.cs
+++ Federation/src/Federation/Responses/DiscoveryResponseMaker.cs
@@ -1,19 +1,24 @@
 // Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
+using Microsoft.Extensions.Logging;
+
 using Wangkanai.Federation.Models;
 
 namespace Wangkanai.Federation.Responses;
 
 public class DiscoveryResponseMaker : IDiscoveryResponseMaker
 {
-	private readonly FederationOptions _options;
+	private readonly FederationOptions               _options;
+	private readonly ILogger<DiscoveryResponseMaker> _logger;
 
 	public DiscoveryResponseMaker(
-		FederationOptions options)
+		FederationOptions               options,
+		ILogger<DiscoveryResponseMaker> logger)
 	{
 		_options = options;
+		_logger  = logger;
 	}
-	
+
 	public Task<Dictionary<string, object>> CreateResultAsync(string issuerUri, string baseUri)
 	{
 		throw new NotImplementedException();
