889701c962b67fbc10f9f7649ae4e0d3a59fd37b
Sarin Na Wangkanai <sarin@wangkanai.com>
2023-03-14 22:38:45 +0800

    federation endpoint structures

---
+++ Federation/src/Federation/Endpoints/TokenEndpoint.cs
@@ -0,0 +1,15 @@
+// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using Microsoft.AspNetCore.Http;
+
+using Wangkanai.Federation.Hosting;
+
+namespace Wangkanai.Federation.Endpoints;
+
+public class TokenEndpoint: IEndpointHandler
+{
+	public async Task<IEndpointResult> ProcessAsync(HttpContext context)
+	{
+		throw new NotImplementedException();
+	}
+}
