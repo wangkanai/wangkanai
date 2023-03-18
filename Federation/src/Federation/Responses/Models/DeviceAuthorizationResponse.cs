fa23b6207dbb1ade4f2681c6a137dad17cd956a0
Sarin Na Wangkanai <sarin@wangkanai.com>
2023-03-18 16:37:36 +1000

    update federation response model

---
+++ Federation/src/Federation/Responses/Models/DeviceAuthorizationResponse.cs
@@ -0,0 +1,13 @@
+// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Federation.Responses;
+
+public class DeviceAuthorizationResponse
+{
+	public string DeviceCode              { get; set; }
+	public string UserCode                { get; set; }
+	public string VerificationUri         { get; set; }
+	public string VerificationUriComplete { get; set; }
+	public int    DeviceCodeLifetime      { get; set; }
+	public int    Interval                { get; set; }
+}
