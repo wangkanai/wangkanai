b33625c05b605d3eb02f217cbf387adcaaca0ba7
Sarin Na Wangkanai <sarin@wangkanai.com>
2023-04-03 08:59:29 +0700

    fixed graph error

--- Graph/src/Extensions/GraphClaimsIdentityExtensions.cs
+++ Graph/src/Extensions/GraphClaimsIdentityExtensions.cs
@@ -1,5 +1,7 @@
 // Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
+using Microsoft.Graph.Models;
+
 namespace Wangkanai.Graph.Extensions;
 
 public static class GraphClaimsIdentityExtensions
