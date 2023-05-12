3f46c62f35900e526fb0145cea106f417dafe2dd
wangkanai <sarin@wangkanai.com>
2023-05-12 08:18:30 +0700

    I will remove markdown from my main web first.

--- Web/src/Program.cs
+++ Web/src/Program.cs
@@ -8,7 +8,7 @@ builder.Host.UseSerilog();
 // Add services to the container.
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
-builder.Services.AddMarkdownPages();
+// builder.Services.AddMarkdownPages();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
@@ -33,7 +33,7 @@ app.UseRouting();
 app.UseAuthorization();
 app.UseSerilogRequestLogging();
 
-app.MapMarkdownPages();
+// app.MapMarkdownPages();
 app.MapRazorPages();
 
 app.Run();
