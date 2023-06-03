5e23e47a7c56abe523e0b679f76ece5bbf4d74d4
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-03 23:10:31 +0700

    add unit testing for cryptography `Adler32`

---
+++ System/tests/Cryptography/Adler32Tests.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using Xunit;
+
+namespace Wangkanai.Cryptography;
+
+public class Adler32Tests
+{
+	[Fact]
+	public void ComputeChecksumThrowIfNull()
+	{
+		Assert.Throws<ArgumentNullException>(() => Adler32.ComputeChecksum(0, null!));
+		Assert.Throws<ArgumentNullException>(() => Adler32.ComputeChecksum(0, null!, 0, 0));
+	}
+}
