feba32ea187dfd0b09e68d0fb16d096bfcb7ccf9
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-06 15:21:36 +0700

    #687 [System] Check if positive and negative
    Lets get this function into the base system.

---
+++ System/src/Exceptions/ArgumentNegativeException.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using Wangkanai.Resources;
+
+namespace Wangkanai.Exceptions;
+
+[Serializable]
+public class ArgumentNegativeException : ArgumentException
+{
+	public ArgumentNegativeException() : base(SystemResources.ArgumentNegativeGeneric) { }
+	public ArgumentNegativeException(string paramName) : base(SystemResources.ArgumentZeroGeneric, paramName) { }
+	public ArgumentNegativeException(string message,   Exception innerException) : base(message, innerException) { }
+	public ArgumentNegativeException(string paramName, string    message) : base(message, paramName) { }
+}
---
+++ System/src/Exceptions/ArgumentPositiveException.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using Wangkanai.Resources;
+
+namespace Wangkanai.Exceptions;
+
+[Serializable]
+public class ArgumentPositiveException : ArgumentException
+{
+	public ArgumentPositiveException() : base(SystemResources.ArgumentPositiveGeneric) { }
+	public ArgumentPositiveException(string paramName) : base(SystemResources.ArgumentZeroGeneric, paramName) { }
+	public ArgumentPositiveException(string message,   Exception innerException) : base(message, innerException) { }
+	public ArgumentPositiveException(string paramName, string    message) : base(message, paramName) { }
+}
