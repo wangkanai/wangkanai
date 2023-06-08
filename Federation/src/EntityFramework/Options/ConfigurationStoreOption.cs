69b6886b9c8599d0bbce23e6f45f0e8d29da8239
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-08 17:11:43 +0700

    fixed the recurring method

---
+++ Federation/src/EntityFramework/Options/ConfigurationStoreOption.cs
@@ -0,0 +1,10 @@
+// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+#nullable enable
+namespace Wangkanai.Federation.Options;
+
+public class ConfigurationStoreOption
+{
+	public string? DefaultSchema { get; set; } = null;
+	
+}
---
+++ Federation/src/EntityFramework/Options/TableConfiguration.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Federation.Options;
+
+#nullable enable
+
+public class TableConfiguration
+{
+	public string  Name   { get; }
+	public string? Schema { get; }
+
+	public TableConfiguration(string name)
+		=> Name = name;
+
+	public TableConfiguration(string name, string schema)
+		: this(name)
+		=> Schema = schema;
+}
