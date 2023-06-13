cdaebad26560065c7c232ec113481d6cca13314b
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-13 12:26:05 +0700

    ิีbuild sonar before dotcover test

--- dotnet-sonar.ps1
+++ dotnet-sonar.ps1
@@ -1,5 +1,5 @@
 dotnet restore
 dotnet sonarscanner begin /k:wangkanai_wangkanai /o:wangkanai /d:sonar.host.url=https://sonarcloud.io /d:sonar.cs.dotcover.reportsPaths=dotCover.Output.html
-dotnet build  –no-incremental
+dotnet build -c release
 dotnet dotcover test --dcReportType=HTML
 dotnet sonarscanner end
