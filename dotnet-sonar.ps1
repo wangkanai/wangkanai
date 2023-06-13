2048481667a72b40a0b6cf9feca369fb83c13047
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-13 12:15:54 +0700

    Rethink what is exclusion of duplication copy-paste detection

--- dotnet-sonar.ps1
+++ dotnet-sonar.ps1
@@ -1,6 +1,5 @@
-rimraf .sonarqube/out
 dotnet restore
-dotnet sonarscanner begin /k:wangkanai_wangkanai /o:wangkanai /d:sonar.cs.dotcover.reportsPaths=dotCover.Output.html
+dotnet sonarscanner begin /k:wangkanai_wangkanai /o:wangkanai /d:sonar.host.url=https://sonarcloud.io /d:sonar.cs.dotcover.reportsPaths=dotCover.Output.html
 dotnet build  –no-incremental
 dotnet dotcover test --dcReportType=HTML
 dotnet sonarscanner end
