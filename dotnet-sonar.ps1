af4604d87de1b5278fcb4e7daa93d6f09d620289
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-14 00:09:55 +0700

    convert from .properties to Analysis.xml

--- dotnet-sonar.ps1
+++ dotnet-sonar.ps1
@@ -1,5 +1,5 @@
 dotnet restore
-dotnet sonarscanner begin /k:wangkanai_wangkanai /o:wangkanai /d:sonar.host.url=https://sonarcloud.io /d:sonar.verbose=true /d:sonar.cs.dotcover.reportsPaths=dotCover.Output.html 
+dotnet sonarscanner begin /k:wangkanai_wangkanai /o:wangkanai 
 dotnet build -c release
 dotnet dotcover test --dcReportType=HTML
 dotnet sonarscanner end
