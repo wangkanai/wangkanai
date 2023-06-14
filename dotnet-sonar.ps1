5d5886597a54148ab8e44f8b697d948003d8e627
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-14 14:54:06 +0700

    pass the filter to dotcover via command line

--- dotnet-sonar.ps1
+++ dotnet-sonar.ps1
@@ -1,6 +1,5 @@
 dotnet restore
 dotnet sonarscanner begin /k:wangkanai_wangkanai /o:wangkanai /d:sonar.host.url=https://sonarcloud.io
 dotnet build -c release
-dotnet dotcover cover coverage.xml
-dotnet dotcover test --dcReportType=HTML
+dotnet dotcover test --dcReportType=HTML --dcFilters=-:*Test*
 dotnet sonarscanner end
