3abda6cb8cbe79ada27cb998d7d12fadaa047b6a
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-14 14:50:04 +0700

    update dotcover configuration filters

--- dotnet-sonar.ps1
+++ dotnet-sonar.ps1
@@ -1,5 +1,6 @@
 dotnet restore
 dotnet sonarscanner begin /k:wangkanai_wangkanai /o:wangkanai /d:sonar.host.url=https://sonarcloud.io
 dotnet build -c release
+dotnet dotcover cover coverage.xml
 dotnet dotcover test --dcReportType=HTML
 dotnet sonarscanner end
