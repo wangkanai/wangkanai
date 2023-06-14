7ac72e6b932c5f6857a1308a0277cad14e40b608
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-15 00:39:26 +0700

    trying out with dotnet-coverage

--- dotnet-sonar.ps1
+++ dotnet-sonar.ps1
@@ -1,5 +1,5 @@
 dotnet restore
-dotnet sonarscanner begin /k:wangkanai_wangkanai /o:wangkanai /d:sonar.host.url=https://sonarcloud.io
+dotnet sonarscanner begin /k:wangkanai_wangkanai /o:wangkanai /d:sonar.host.url=https://sonarcloud.io  /d:sonar.cs.vscoveragexml.reportsPaths=coverage.xml
 dotnet build -c release
-dotnet dotcover test --dcReportType=HTML --dcFilters=-:*Test*
+dotnet-coverage collect "dotnet test" -f xml -o "coverage.xml"
 dotnet sonarscanner end
