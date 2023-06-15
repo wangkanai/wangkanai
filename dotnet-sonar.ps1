d7cea7172011119322bd77cf1e2234f3c5d58b6d
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-15 12:04:57 +0700

    Have a working sonar configuration

--- dotnet-sonar.ps1
+++ dotnet-sonar.ps1
@@ -1,5 +1,5 @@
 dotnet restore
-dotnet sonarscanner begin /k:wangkanai_wangkanai /o:wangkanai /d:sonar.host.url=https://sonarcloud.io /d:sonar.cs.vscoveragexml.reportsPaths=coverage.xml
+dotnet sonarscanner begin /k:wangkanai_wangkanai /o:wangkanai /d:sonar.host.url=https://sonarcloud.io /s:"D:\Sources\Wangkanai\SonarQube.Analysis.xml"
 dotnet build -c release
 dotnet-coverage collect "dotnet test" -f xml -o "coverage.xml"
 dotnet sonarscanner end
