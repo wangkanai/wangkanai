95710e81aa704b87e2c8528b60cd0e97555cb6bc
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-15 19:07:32 +0700

    reconfig sonar

--- dotnet-sonar.ps1
+++ dotnet-sonar.ps1
@@ -1,5 +1,6 @@
 dotnet restore
 dotnet sonarscanner begin /k:wangkanai_wangkanai /o:wangkanai /d:sonar.host.url=https://sonarcloud.io /s:"D:\Sources\Wangkanai\SonarQube.Analysis.xml"
 dotnet build -c release
-dotnet-coverage collect "dotnet test" -f xml -o "coverage.xml"
+dotnet coverage collect "dotnet test" -f xml -o "coverage.xml"
+# dotnet test --collect "Code coverage" 
 dotnet sonarscanner end
