a282e2eaf37df072579983ef0377d5576c652ab2
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-15 19:52:30 +0700

    [Webmaster] Regex require timeout to limit the execution time
    Not specifying a timeout for regular expressions can lead to a Denial-of-Service attack. Pass a timeout when using System.Text.RegularExpressions to process untrusted input because a malicious user might craft a value for which the evaluation lasts excessively long.

--- Webmaster/src/Routing/LanguageRouteContraint.cs
+++ Webmaster/src/Routing/LanguageRouteContraint.cs
@@ -8,28 +8,28 @@ namespace Wangkanai.Webmaster.Routing;
 
 public sealed class EnglishLanguageRouteConstraint : RegexRouteConstraint
 {
-    public EnglishLanguageRouteConstraint() : base(new Regex("^[a-zA-Z]*$"))
-    {
-    }
+    public EnglishLanguageRouteConstraint() : base(new Regex("^[a-zA-Z]*$", RegexOptions.Compiled, Constants.RegexTimeout))
+	{
+	}
 }
 
 public sealed class ThaiLanguageRouteConstraint : RegexRouteConstraint
 {
-    public ThaiLanguageRouteConstraint() : base(new Regex(@"^\p{IsThai}*$"))
+    public ThaiLanguageRouteConstraint() : base(new Regex(@"^\p{IsThai}*$", RegexOptions.Compiled, Constants.RegexTimeout))
     {
     }
 }
 
 public sealed class LaoLanguageRouteConstraint : RegexRouteConstraint
 {
-    public LaoLanguageRouteConstraint() : base(new Regex(@"^\p{IsLao}*$"))
+    public LaoLanguageRouteConstraint() : base(new Regex(@"^\p{IsLao}*$", RegexOptions.Compiled, Constants.RegexTimeout))
     {
     }
 }
 
 public sealed class MyanmarLanguageRouteConstraint : RegexRouteConstraint
 {
-    public MyanmarLanguageRouteConstraint() : base(new Regex("^[U+1000–U+109F]*$"))
+    public MyanmarLanguageRouteConstraint() : base(new Regex("^[U+1000–U+109F]*$", RegexOptions.Compiled, Constants.RegexTimeout))
     {
     }
 }
