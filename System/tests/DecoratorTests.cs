c2d6b0cc651d5a884277c16ddb9891dfddc3e318
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-16 11:02:39 +0700

    add decorator unit tests

--- System/tests/DecoratorTests.cs
+++ System/tests/DecoratorTests.cs
@@ -61,4 +61,88 @@ public class DecoratorTests
 		var decorator = new Decorator<string, string>(null!);
 		Assert.Null(decorator.Instance);
 	}
+	
+	[Fact]
+	public void ImplementationIsNotNull()
+	{
+		var decorator = new Decorator<string, string>("test");
+		Assert.NotNull(decorator.Instance);
+	}
+	
+	[Fact]
+	public void ImplementationIsEqual()
+	{
+		var decorator = new Decorator<string, string>("test");
+		Assert.Equal("test", decorator.Instance);
+	}
+	
+	[Fact]
+	public void ImplementationIsNotEqual()
+	{
+		var decorator = new Decorator<string, string>("test");
+		Assert.NotEqual("test1", decorator.Instance);
+	}
+	
+	[Fact]
+	public void ImplementationIsNotEqualWithNull()
+	{
+		var decorator = new Decorator<string, string>("test");
+		Assert.NotEqual(null, decorator.Instance);
+	}
+	
+	[Fact]
+	public void ImplementationIsNotEqualWithNullString()
+	{
+		var decorator = new Decorator<string, string>("test");
+		Assert.NotEqual(string.Empty, decorator.Instance);
+	}
+	
+	[Fact]
+	public void ImplementationIsNotEqualWithNullObject()
+	{
+		var decorator = new Decorator<string, string>("test");
+		Assert.NotEqual(new object(), decorator.Instance);
+	}
+	
+	[Fact]
+	public void DisposableInstanceIsNull()
+	{
+		var decorator = new DisposableDecorator<string>(null!);
+		Assert.Null(decorator.Instance);
+	}
+	
+	[Fact]
+	public void DisposableInstanceIsNotNull()
+	{
+		var decorator = new DisposableDecorator<string>("test");
+		Assert.NotNull(decorator.Instance);
+	}
+	
+	[Fact]
+	public void DisposableInstanceIsEqual()
+	{
+		var decorator = new DisposableDecorator<string>("test");
+		Assert.Equal("test", decorator.Instance);
+	}
+	
+	[Fact]
+	public void DisposableInstanceIsNotEqual()
+	{
+		var decorator = new DisposableDecorator<string>("test");
+		Assert.NotEqual("test1", decorator.Instance);
+	}
+	
+	[Fact]
+	public void DisposableInstanceIsNotEqualWithNull()
+	{
+		var decorator = new DisposableDecorator<string>("test");
+		Assert.NotEqual(null, decorator.Instance);
+	}
+	
+	[Fact]
+	public void DisposableInstanceIsNotEqualWithNullString()
+	{
+		var decorator = new DisposableDecorator<string>("test");
+		Assert.NotEqual(string.Empty, decorator.Instance);
+	}
 }
