c3c8ad2abaa3f0e66ba9a088c4a3b2ff666a6c3a
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-16 10:40:32 +0700

    [Validation] need to drop support of .net standard 2.1 also

---
+++ System/tests/DecoratorTests.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using Xunit;
+
+namespace Wangkanai;
+
+public class DecoratorTests
+{
+	[Fact]
+	public void InstanceIsNull()
+	{
+		var decorator = new Decorator<string>(null!);
+		Assert.Null(decorator.Instance);
+	}
+	
+	[Fact]
+	public void InstanceIsNotNull()
+	{
+		var decorator = new Decorator<string>("test");
+		Assert.NotNull(decorator.Instance);
+	}
+}
