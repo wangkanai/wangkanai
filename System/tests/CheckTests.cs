d72263a009ad0537d7a541e07e0fd8e2e739dee1
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-17 21:04:28 +0700

    add unit test for `FalseIfNull()`

--- System/tests/CheckTests.cs
+++ System/tests/CheckTests.cs
@@ -355,10 +355,10 @@ public class CheckTests
 
 	#endregion
 
-	#region TrueIfNull
+	#region BooleanIfNull
 
 	[Fact]
-	public void IfNullThenReturnTrue()
+	public void CheckTrueIfNullThenReturnTrue()
 	{
 		byte?    _byte    = null;
 		short?   _short   = null;
@@ -382,7 +382,7 @@ public class CheckTests
 	}
 
 	[Fact]
-	public void TrueIfNullReturnFalse()
+	public void CheckTrueIfExistReturnFalse()
 	{
 		byte?    _byte    = 0;
 		short?   _short   = 0;
@@ -403,6 +403,54 @@ public class CheckTests
 		Assert.False(_decimal.TrueIfNull());
 		Assert.False(_char.TrueIfNull());
 		Assert.False(_string.TrueIfNull());
+	}
+	
+	[Fact]
+	public void CheckFalseIfNullThenReturnFalse()
+	{
+		byte?    _byte    = null;
+		short?   _short   = null;
+		int?     _int     = null;
+		long?    _long    = null;
+		float?   _float   = null;
+		double?  _double  = null;
+		decimal? _decimal = null;
+		char?    _char    = null;
+		string?  _string  = null;
+
+		Assert.False(_byte.FalseIfNull());
+		Assert.False(_short.FalseIfNull());
+		Assert.False(_int.FalseIfNull());
+		Assert.False(_long.FalseIfNull());
+		Assert.False(_float.FalseIfNull());
+		Assert.False(_double.FalseIfNull());
+		Assert.False(_decimal.FalseIfNull());
+		Assert.False(_char.FalseIfNull());
+		Assert.False(_string.FalseIfNull());
+	}
+
+	[Fact]
+	public void CheckFalseIfExistReturnFalse()
+	{
+		byte?    _byte    = 0;
+		short?   _short   = 0;
+		int?     _int     = 0;
+		long?    _long    = 0;
+		float?   _float   = 0;
+		double?  _double  = 0.0;
+		decimal? _decimal = 0;
+		char?    _char    = 'a';
+		string?  _string  = "xyz";
+
+		Assert.True(_byte.FalseIfNull());
+		Assert.True(_short.FalseIfNull());
+		Assert.True(_int.FalseIfNull());
+		Assert.True(_long.FalseIfNull());
+		Assert.True(_float.FalseIfNull());
+		Assert.True(_double.FalseIfNull());
+		Assert.True(_decimal.FalseIfNull());
+		Assert.True(_char.FalseIfNull());
+		Assert.True(_string.FalseIfNull());
 	}
 
 	#endregion
