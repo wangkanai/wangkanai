3aa85ee4cace5cfbb1cd38ad4d9561437a166a9c
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-17 23:38:31 +0700

    disable runtime .net8

--- System/benchmark/CheckBenchmark.cs
+++ System/benchmark/CheckBenchmark.cs
@@ -7,7 +7,7 @@ using Xunit;
 namespace Wangkanai;
 
 [SimpleJob(RuntimeMoniker.Net70, baseline: true)]
-[SimpleJob(RuntimeMoniker.Net80)]
+// [SimpleJob(RuntimeMoniker.Net80)]
 [RPlotExporter]
 [MemoryDiagnoser]
 public class CheckBenchmark
