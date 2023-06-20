0a049827ea6528cc23ec1eb9dd5f8c74b03db45a
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-20 11:45:44 +0700

    [System] string extensions convert to enum

---
+++ System/tests/Extensions/Strings/StringEnumTests.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using Xunit;
+
+namespace Wangkanai.Extensions.Strings;
+
+#nullable enable
+
+public class StringEnumTests
+{
+	string? _null  = null;
+	string? _empty = string.Empty;
+	string? _space = " ";
+	
+	[Fact]
+	public void ValueExistToChar()
+	{
+		var name  = "sarin";
+		var result = name.ToEnum<Names>();
+		Assert.Equal(Names.Sarin, result);
+	}
+
+	private enum Names
+	{
+		Sarin,
+		Tanya,
+		James,
+		Wangkanai
+	}
+}
+
