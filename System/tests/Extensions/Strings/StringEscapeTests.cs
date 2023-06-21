8fa81c931f0bb1a15a57a6c2940ab2dad1f71015
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-21 07:53:04 +0700

    [System] string extensions escape search

--- System/tests/Extensions/Strings/StringEscapeTests.cs
+++ System/tests/Extensions/Strings/StringEscapeTests.cs
@@ -1,12 +1,22 @@
 ﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
+#nullable enable
+
+using Xunit;
+
 namespace Wangkanai.Extensions.Strings;
 
 public class StringEscapeTests
 {
-	string? _null  = null;
-	string? _empty = string.Empty;
-	string? _space = " ";
-	string? text   = "abcde";
-	
+	string? _null     = null;
+	string? _empty    = string.Empty;
+	string? _space    = " ";
+	string? text      = "abcde";
+	string? text_plus = "a+b+c+d+e";
+
+	[Fact]
+	public void Normal()
+	{
+		Assert.Equal(text, text.EscapeSearchTerm());
+	}
 }
