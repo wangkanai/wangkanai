d19c831f04f7c5e78891b00ed8330cb4a27ed907
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-22 14:00:50 +0700

    sonar refactor powershell configuration

--- sonar-begin.ps1
+++ sonar-begin.ps1
@@ -1,21 +1,22 @@
 if ("$( sonar.pullrequest.base )" -ne "$( sonar.pullrequest.branch )")
 {
+    $version     = 1.6
     $pullrequest = $true
-    $branch      = $( System.PullRequest.SourceBranch )
-    $key         = "$( Build.SourceBranch )".Split("/")
-    $key
+    $source      = $(Build.SourcesDirectory)
+    $base        = $(System.PullRequest.TargetBranch)
+    $branch      = $(System.PullRequest.SourceBranch)
+    $key         = "$(Build.SourceBranch)".Split("/")[2]
 
     Write-Host "PR Yes:" $pullrequest
-    Write-Host "sonar.pullrequest.base : "   $(sonar.pullrequest.base)
+    Write-Host "sonar.pullrequest.base : "   $base
     Write-Host "sonar.pullrequest.branch : " $branch
     Write-Host "sonar.pullrequest.key : "    $key
 
-    dotnet sonarscanner begin /k:wangkanai_wangkanai /o:wangkanai /v:1.5 /s:$( Build.SourcesDirectory )/SonarQube.Analysis.xml /d:sonar.host.url = https://sonarcloud.io /d:sonar.cs.vscoveragexml.reportsPaths = $( Build.SourcesDirectory )/coverage.xml /d:sonar.pullrequest.base = $( sonar.pullrequest.base ) /d:sonar.pullrequest.branch = $branch /d:sonar.pullrequest.key = $key
-
+    dotnet sonarscanner begin /k:wangkanai_wangkanai /o:wangkanai /v:$version /s:$source/SonarQube.Analysis.xml /d:sonar.host.url=https://sonarcloud.io /d:sonar.cs.vscoveragexml.reportsPaths=$source/coverage.xml /d:sonar.pullrequest.base=$base /d:sonar.pullrequest.branch=$branch /d:sonar.pullrequest.key=$key
 }
 else
 {
     Write-Host "PR Not:" $pullrequest
 
-    dotnet sonarscanner begin /k:wangkanai_wangkanai /o:wangkanai /v:1.5 /s:$( Build.SourcesDirectory )/SonarQube.Analysis.xml /d:sonar.host.url = https://sonarcloud.io /d:sonar.cs.vscoveragexml.reportsPaths = $( Build.SourcesDirectory )/coverage.xml /d:sonar.branch.name = main
+    dotnet sonarscanner begin /k:wangkanai_wangkanai /o:wangkanai /v:$version /s:$source/SonarQube.Analysis.xml /d:sonar.host.url=https://sonarcloud.io /d:sonar.cs.vscoveragexml.reportsPaths=$source/coverage.xml /d:sonar.branch.name=$base
 }
