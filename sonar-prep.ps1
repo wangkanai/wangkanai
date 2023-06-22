1b86fd3867ed3e7d0114eeb610601df6a0cffa67
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-22 16:45:22 +0700

    config sonar

--- sonar-prep.ps1
+++ sonar-prep.ps1
@@ -1,19 +1,22 @@
-if ("main" -ne $env:Build_SourceBranchName) {
-    $version     = 1.6
+if ("main" -ne $env:Build_SourceBranchName)
+{
+    $version = 1.6
     $pullrequest = $true
-    $directory   = $env:Build_SourcesDirectory
-    $source      = $env:Build_SourceBranch
-    $base        = $env:System_PullRequest_TargetBranch
-    $branch      = $env:System_PullRequest_SourceBranch
-    $key         = $source.Split("/")[2]
-    
+    $directory = $env:Build_SourcesDirectory
+    $source = $env:Build_SourceBranch
+    $base = $env:System_PullRequest_TargetBranch
+    $branch = $env:System_PullRequest_SourceBranch
+    $key = $source.Split("/")[2]
+
     Write-Host "PR Yes:" $pullrequest
     Write-Host "sonar.pullrequest.base : "   $base
     Write-Host "sonar.pullrequest.branch : " $branch
     Write-Host "sonar.pullrequest.key : "    $key
-    
-} else {
+
+}
+else
+{
     $pullrequest = $false
-    
+
     Write-Host "PR Not:" $pullrequest
 } 
