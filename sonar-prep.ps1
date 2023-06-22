d185f2893815554b4cd8e0b4930f270a057624b3
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-22 14:03:38 +0700

    update configuration

--- sonar-prep.ps1
+++ sonar-prep.ps1
@@ -1,14 +1,18 @@
-[bool] $pullrequest = $false
-
 if ("$(sonar.pullrequest.base)" -ne "$(sonar.pullrequest.branch)") {
+    $version     = 1.6
     $pullrequest = $true
+    $source      = $(Build.SourcesDirectory)
+    $base        = $(System.PullRequest.TargetBranch)
+    $branch      = $(System.PullRequest.SourceBranch)
+    $key         = "$(Build.SourceBranch)".Split("/")[2]
+    
     Write-Host "PR Yes:" $pullrequest
+    Write-Host "sonar.pullrequest.base : "   $base
+    Write-Host "sonar.pullrequest.branch : " $branch
+    Write-Host "sonar.pullrequest.key : "    $key
+    
 } else {
     $pullrequest = $false
+    
     Write-Host "PR Not:" $pullrequest
 } 
-
-Write-Host "sonar.pullrequest.base : "   $(sonar.pullrequest.base)
-Write-Host "sonar.pullrequest.branch : " $(sonar.pullrequest.branch)
-Write-Host "sonar.pullrequest.key : "    $(sonar.pullrequest.key)
-Write-Host "sonar.pullrequest : "        $(sonar.pullrequest)
