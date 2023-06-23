2672086348f034851b2d18088348fd10da49a846
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-23 09:50:22 +0700

    remove duplicated code

--- System/src/Check.cs
+++ System/src/Check.cs
@@ -15,155 +15,121 @@ public static class Check
 	#region Throw
 
 	// Throw if value is null (all possible types)
-	[DebuggerStepThrough]
 	public static bool? ThrowIfNull(this bool? value)
 		=> ThrowIfNull<ArgumentNullException>(value);
 
-	[DebuggerStepThrough]
 	public static sbyte? ThrowIfNull(this sbyte? value)
 		=> ThrowIfNull<ArgumentNullException>(value);
 
-	[DebuggerStepThrough]
 	public static byte? ThrowIfNull(this byte? value)
 		=> ThrowIfNull<ArgumentNullException>(value);
 
-	[DebuggerStepThrough]
 	public static ushort? ThrowIfNull(this ushort? value)
 		=> ThrowIfNull<ArgumentNullException>(value);
 
-	[DebuggerStepThrough]
 	public static short? ThrowIfNull(this short? value)
 		=> ThrowIfNull<ArgumentNullException>(value);
 
-	[DebuggerStepThrough]
 	public static int? ThrowIfNull(this int? value)
 		=> ThrowIfNull<ArgumentNullException>(value);
 
-	[DebuggerStepThrough]
 	public static uint? ThrowIfNull(this uint? value)
 		=> ThrowIfNull<ArgumentNullException>(value);
 
-	[DebuggerStepThrough]
 	public static nint? ThrowIfNull(this nint? value)
 		=> ThrowIfNull<ArgumentNullException>(value);
 
-	[DebuggerStepThrough]
 	public static nuint? ThrowIfNull(this nuint? value)
 		=> ThrowIfNull<ArgumentNullException>(value);
 
-	[DebuggerStepThrough]
 	public static long? ThrowIfNull(this long? value)
 		=> ThrowIfNull<ArgumentNullException>(value);
 
-	[DebuggerStepThrough]
 	public static ulong? ThrowIfNull(this ulong? value)
 		=> ThrowIfNull<ArgumentNullException>(value);
 
-	[DebuggerStepThrough]
 	public static float? ThrowIfNull(this float? value)
 		=> ThrowIfNull<ArgumentNullException>(value);
 
-	[DebuggerStepThrough]
 	public static decimal? ThrowIfNull(this decimal? value)
 		=> ThrowIfNull<ArgumentNullException>(value);
 
-	[DebuggerStepThrough]
 	public static double? ThrowIfNull(this double? value)
 		=> ThrowIfNull<ArgumentNullException>(value);
 
-	[DebuggerStepThrough]
 	public static char? ThrowIfNull(this char? value)
 		=> ThrowIfNull<ArgumentNullException>(value);
 
-	[DebuggerStepThrough]
 	public static string ThrowIfNull(this string? value)
 		=> ThrowIfNull<ArgumentNullException>(value);
 
-	[DebuggerStepThrough]
 	public static string ThrowIfNull(this string? value, string message)
 		=> ThrowIfNull<ArgumentNullException>(value, message);
 
-	[DebuggerStepThrough]
 	public static bool? ThrowIfNull<T>(this bool? value)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static sbyte? ThrowIfNull<T>(this sbyte? value)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static byte? ThrowIfNull<T>(this byte? value)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static short? ThrowIfNull<T>(this short? value)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static ushort? ThrowIfNull<T>(this ushort? value)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static int? ThrowIfNull<T>(this int? value)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static uint? ThrowIfNull<T>(this uint? value)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static nint? ThrowIfNull<T>(this nint? value)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static nuint? ThrowIfNull<T>(this nuint? value)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static long? ThrowIfNull<T>(this long? value)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static ulong? ThrowIfNull<T>(this ulong? value)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static float? ThrowIfNull<T>(this float? value)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static double? ThrowIfNull<T>(this double? value)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static decimal? ThrowIfNull<T>(this decimal? value)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static char? ThrowIfNull<T>(this char? value)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static string ThrowIfNull<T>(this string? value)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static string ThrowIfNull<T>(this string? value, string message)
 		where T : ArgumentException
 		=> value ?? throw CreateArgumentExceptionInstance<T>(nameof(value), message);
@@ -171,19 +137,15 @@ public static class Check
 	#endregion
 
 	// Throw is value string is null or empty
-	[DebuggerStepThrough]
 	public static string ThrowIfNullOrEmpty(this string? value)
 		=> value.ThrowIfNullOrEmpty<ArgumentNullOrEmptyException>();
 
-	[DebuggerStepThrough]
 	public static string ThrowIfNullOrEmpty(this string? value, string message)
 		=> value.ThrowIfNullOrEmpty<ArgumentNullOrEmptyException>(message);
 
-	[DebuggerStepThrough]
 	public static string ThrowIfNullOrEmpty(this string? value, string message, string paramName)
 		=> value.ThrowIfNullOrEmpty<ArgumentNullOrEmptyException>(message, paramName);
 
-	[DebuggerStepThrough]
 	public static string ThrowIfNullOrEmpty<T>(this string? value)
 		where T : ArgumentException
 	{
@@ -192,7 +154,6 @@ public static class Check
 		return value!;
 	}
 
-	[DebuggerStepThrough]
 	public static string ThrowIfNullOrEmpty<T>(this string? value, string message)
 		where T : ArgumentException
 	{
@@ -201,7 +162,6 @@ public static class Check
 		return value!;
 	}
 
-	[DebuggerStepThrough]
 	public static string ThrowIfNullOrEmpty<T>(this string? value, string message, string paramName)
 		where T : ArgumentException
 	{
@@ -212,33 +172,27 @@ public static class Check
 
 	// Throw if value string is empty
 
-	[DebuggerStepThrough]
 	public static string ThrowIfEmpty(this string? value)
 		=> ThrowIfEmpty<ArgumentEmptyException>(value);
 
-	[DebuggerStepThrough]
 	public static string ThrowIfEmpty(this string? value, string message)
 		=> ThrowIfEmpty<ArgumentEmptyException>(value, message);
 
-	[DebuggerStepThrough]
 	public static string ThrowIfEmpty(this string? value, string message, string paramName)
 		=> ThrowIfEmpty<ArgumentEmptyException>(value, message, paramName);
 
-	[DebuggerStepThrough]
 	public static string ThrowIfEmpty<T>(this string? value)
 		where T : ArgumentException
 		=> value.ThrowIfNull<T>().IsEmpty()
 			   ? throw CreateArgumentExceptionInstance<T>(nameof(value))
 			   : value!;
 
-	[DebuggerStepThrough]
 	public static string ThrowIfEmpty<T>(this string? value, string message)
 		where T : ArgumentException
 		=> value.ThrowIfNull<T>().IsEmpty()
 			   ? throw CreateArgumentExceptionInstance<T>(nameof(value), message)
 			   : value!;
 
-	[DebuggerStepThrough]
 	public static string ThrowIfEmpty<T>(this string? value, string message, string paramName)
 		where T : ArgumentException
 		=> value.ThrowIfNull<T>().IsEmpty()
@@ -246,25 +200,21 @@ public static class Check
 			   : value!;
 
 	// Throw if value string is null or whitespace
-	[DebuggerStepThrough]
+	
 	public static string ThrowIfNullOrWhitespace(this string? value)
 		=> ThrowIfNullOrWhitespace<ArgumentNullOrWhitespaceException>(value);
 
-	[DebuggerStepThrough]
 	public static string ThrowIfNullOrWhitespace(this string? value, string message)
 		=> ThrowIfNullOrWhitespace<ArgumentNullOrWhitespaceException>(value, message);
 
-	[DebuggerStepThrough]
 	public static string ThrowIfNullOrWhitespace<T>(this string? value)
 		where T : ArgumentException
 		=> value.ThrowIfNullOrWhitespace<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static string ThrowIfNullOrWhitespace<T>(this string? value, string message)
 		where T : ArgumentException
 		=> value.ThrowIfNullOrWhitespace<T>(message, nameof(value));
 
-	[DebuggerStepThrough]
 	public static string ThrowIfNullOrWhitespace<T>(this string? value, string message, [InvokerParameterName] string paramName)
 		where T : ArgumentException
 		=> value.IsNullOrWhiteSpace()
@@ -272,36 +222,30 @@ public static class Check
 			   : value!;
 
 	// Throw if value object is null
-	[DebuggerStepThrough]
+	
 	public static object ThrowIfNull<T>(this object? value)
 		where T : Exception
 		=> value ?? throw CreateGenericExceptionInstance<T>();
 
-	[DebuggerStepThrough]
 	public static object ThrowIfNull<T>(this object? value, string message)
 		where T : Exception
 		=> value ?? throw CreateGenericExceptionInstance<T>(message);
 
-	[DebuggerStepThrough]
 	public static T ThrowIfNull<T>(this T value)
 		=> value ?? throw CreateArgumentExceptionInstance<ArgumentNullException>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static T ThrowIfNull<T>(this T value, string message)
 		=> value ?? throw CreateArgumentExceptionInstance<ArgumentNullException>(message);
 
 
 	// Throw if value is not equal to expected
-	[DebuggerStepThrough]
 	public static bool ThrowIfEqual(this int value, int expected)
 		=> value.ThrowIfEqual<ArgumentEqualException>(expected, nameof(value));
 
-	[DebuggerStepThrough]
 	public static bool ThrowIfEqual<T>(this int value, int expected)
 		where T : ArgumentException
 		=> value.ThrowIfEqual<T>(expected, nameof(value));
 
-	[DebuggerStepThrough]
 	public static bool ThrowIfEqual<T>(this int value, int expected, [InvokerParameterName] string paramName)
 		where T : ArgumentException
 		=> value == expected
@@ -309,16 +253,14 @@ public static class Check
 			   : false;
 
 	// Throw if value is not equal to expected
-	[DebuggerStepThrough]
+	
 	public static bool ThrowIfNotEqual(this int value, int expected)
 		=> value.ThrowIfNotEqual<ArgumentNotEqualException>(expected, nameof(value));
 
-	[DebuggerStepThrough]
 	public static bool ThrowIfNotEqual<T>(this int value, int expected)
 		where T : ArgumentException
 		=> value.ThrowIfNotEqual<T>(expected, nameof(value));
 
-	[DebuggerStepThrough]
 	public static bool ThrowIfNotEqual<T>(this int value, int expected, [InvokerParameterName] string paramName)
 		where T : ArgumentException
 		=> value != expected
@@ -326,20 +268,17 @@ public static class Check
 			   : true;
 
 	// Throw if value is less than expected
-	[DebuggerStepThrough]
+	
 	public static bool ThrowIfLessThan(this int value, int expected)
 		=> value.ThrowIfLessThan<ArgumentLessThanException>(expected, nameof(value));
 
-	[DebuggerStepThrough]
 	public static bool ThrowIfLessThan(this int value, int expected, [InvokerParameterName] string paramName)
 		=> value.ThrowIfLessThan<ArgumentLessThanException>(expected, paramName);
 
-	[DebuggerStepThrough]
 	public static bool ThrowIfLessThan<T>(this int value, int expected)
 		where T : ArgumentException
 		=> value.ThrowIfLessThan<T>(expected, nameof(value));
 
-	[DebuggerStepThrough]
 	public static bool ThrowIfLessThan<T>(this int value, int expected, [InvokerParameterName] string paramName)
 		where T : ArgumentException
 		=> value < expected
@@ -347,20 +286,17 @@ public static class Check
 			   : true;
 
 	// Throw if value is more than expected
-	[DebuggerStepThrough]
+	
 	public static bool ThrowIfMoreThan(this int value, int expected)
 		=> value.ThrowIfMoreThan<ArgumentMoreThanException>(expected, nameof(value));
 
-	[DebuggerStepThrough]
 	public static bool ThrowIfMoreThan(this int value, int expected, [InvokerParameterName] string paramName)
 		=> value.ThrowIfMoreThan<ArgumentMoreThanException>(expected, paramName);
 
-	[DebuggerStepThrough]
 	public static bool ThrowIfMoreThan<T>(this int value, int expected)
 		where T : ArgumentException
 		=> value.ThrowIfMoreThan<T>(expected, nameof(value));
 
-	[DebuggerStepThrough]
 	public static bool ThrowIfMoreThan<T>(this int value, int expected, [InvokerParameterName] string paramName)
 		where T : ArgumentException
 		=> value > expected
@@ -368,20 +304,17 @@ public static class Check
 			   : true;
 
 	// Throw if value is negative
-	[DebuggerStepThrough]
+	
 	public static int ThrowIfNegative(this int value)
 		=> value.ThrowIfNegative<ArgumentNegativeException>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static int ThrowIfNegative(this int value, [InvokerParameterName] string paramName)
 		=> value.ThrowIfNegative<ArgumentNegativeException>(paramName);
 
-	[DebuggerStepThrough]
 	public static int ThrowIfNegative<T>(this int value)
 		where T : ArgumentException
 		=> value.ThrowIfNegative<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static int ThrowIfNegative<T>(this int value, [InvokerParameterName] string paramName)
 		where T : ArgumentException
 		=> value < 0
@@ -389,20 +322,17 @@ public static class Check
 			   : value;
 
 	// Throw if value is positive
-	[DebuggerStepThrough]
+	
 	public static int ThrowIfPositive(this int value)
 		=> value.ThrowIfPositive<ArgumentPositiveException>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static int ThrowIfPositive(this int value, [InvokerParameterName] string paramName)
 		=> value.ThrowIfPositive<ArgumentPositiveException>(paramName);
 
-	[DebuggerStepThrough]
 	public static int ThrowIfPositive<T>(this int value)
 		where T : ArgumentException
 		=> value.ThrowIfPositive<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static int ThrowIfPositive<T>(this int value, [InvokerParameterName] string paramName)
 		where T : ArgumentException
 		=> value < 0
@@ -410,20 +340,17 @@ public static class Check
 			   : value;
 
 	// Throw if value is zero
-	[DebuggerStepThrough]
+	
 	public static bool ThrowIfZero(this int value)
 		=> value.ThrowIfZero<ArgumentZeroException>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static bool ThrowIfZero(this int value, [InvokerParameterName] string paramName)
 		=> value.ThrowIfZero<ArgumentZeroException>(paramName);
 
-	[DebuggerStepThrough]
 	public static bool ThrowIfZero<T>(this int value)
 		where T : ArgumentException
 		=> value.ThrowIfZero<T>(nameof(value));
 
-	[DebuggerStepThrough]
 	public static bool ThrowIfZero<T>(this int value, [InvokerParameterName] string paramName)
 		where T : ArgumentException
 		=> value == 0
@@ -432,17 +359,14 @@ public static class Check
 
 	#region IfNull
 
-	[DebuggerStepThrough]
 	public static bool TrueIfNull<T>(this T? value)
 		=> value is null;
 
-	[DebuggerStepThrough]
 	public static bool FalseIfNull<T>(this T? value)
 		=> !value.TrueIfNull();
 
 	#endregion
 
-	[DebuggerStepThrough]
 	public static T ThrowIfOutOfRange<T>(this T index, [NonNegativeValue] T lower, [NonNegativeValue] T upper)
 		where T : IBinaryInteger<T>
 	{
