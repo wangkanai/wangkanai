4c1d2862faeb6de2824ac6b6de1097c99191fc6c
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-06-24 10:22:15 +0700

    [System] Type extensions refactoring and unit tests

--- System/src/Extensions/TypeExtensions.cs
+++ System/src/Extensions/TypeExtensions.cs
@@ -4,155 +4,142 @@ using System.Collections.Concurrent;
 
 namespace Wangkanai.Extensions;
 
-#nullable disable
-
+[DebuggerStepThrough]
 public static class TypeExtensions
 {
-	[DebuggerStepThrough]
-    public static Type[] GetTypeInheritanceChainTo(this Type type, Type toBaseType)
-    {
-        var retVal   = new List<Type> { type };
-        var baseType = type.BaseType;
-        while (baseType != toBaseType && baseType != typeof(object))
-        {
-            retVal.Add(baseType);
-            baseType = baseType.BaseType;
-        }
+	private static readonly ConcurrentDictionary<Type, string> PrettyPrintCache = new();
+	private static readonly ConcurrentDictionary<Type, string> TypeCacheKeys    = new();
 
-        return retVal.ToArray();
-    }
+	public static Type[] GetTypeInheritanceChainTo(this Type child, Type parent)
+	{
+		var retVal   = new List<Type> { child };
+		var baseType = child.BaseType;
+		while (baseType != parent && baseType != typeof(object))
+		{
+			retVal.Add(baseType);
+			baseType = baseType.BaseType;
+		}
 
-    private static readonly ConcurrentDictionary<Type, string> PrettyPrintCache = new();
+		return retVal.ToArray();
+	}
 
-    [DebuggerStepThrough]
-    public static string PrettyPrint(this Type type)
-        => PrettyPrintCache.GetOrAdd(type, t =>
-        {
-            try
-            {
-                return PrettyPrintRecursive(t, 0);
-            }
-            catch (Exception)
-            {
-                return t.Name;
-            }
-        });
 
-    private static readonly ConcurrentDictionary<Type, string> TypeCacheKeys = new();
+	public static string PrettyPrint(this Type type)
+		=> PrettyPrintCache.GetOrAdd(type, t => {
+			try
+			{
+				return PrettyPrintRecursive(t, 0);
+			}
+			catch (Exception)
+			{
+				return t.Name;
+			}
+		});
 
-    [DebuggerStepThrough]
-    public static string GetCacheKey(this Type type)
-        => TypeCacheKeys.GetOrAdd(type, t => $"{t.PrettyPrint()}");
 
-    [DebuggerStepThrough]
-    private static string PrettyPrintRecursive(Type type, int depth)
-    {
-        if (depth > 3)
-            return type.Name;
+	public static string GetCacheKey(this Type type)
+		=> TypeCacheKeys.GetOrAdd(type, t => $"{t.PrettyPrint()}");
 
-        var nameParts = type.Name.Split('`');
-        if (nameParts.Length == 1)
-            return nameParts[0];
+	private static string PrettyPrintRecursive(Type type, int depth)
+	{
+		if (depth > 3)
+			return type.Name;
 
-        var genericArgs = type.GetTypeInfo().GetGenericArguments();
-        return !type.IsConstructedGenericType
-                   ? $"{nameParts[0]}<{new string(',', genericArgs.Length - 1)}>"
-                   : $"{nameParts[0]}<{string.Join(",", genericArgs.Select(t => PrettyPrintRecursive(t, depth + 1)))}>";
-    }
+		var nameParts = type.Name.Split('`');
+		if (nameParts.Length == 1)
+			return nameParts[0];
 
-    [DebuggerStepThrough]
-    public static IEnumerable<KeyValuePair<string, object>> TraverseObjectGraph(this object original)
-    {
-        foreach (var result in original.TraverseObjectGraphRecursively(new List<object>(), original.GetType().Name))
-            yield return result;
-    }
+		var genericArgs = type.GetTypeInfo().GetGenericArguments();
+		return !type.IsConstructedGenericType
+			       ? $"{nameParts[0]}<{new string(',', genericArgs.Length - 1)}>"
+			       : $"{nameParts[0]}<{string.Join(",", genericArgs.Select(t => PrettyPrintRecursive(t, depth + 1)))}>";
+	}
 
-    [DebuggerStepThrough]
-    private static IEnumerable<KeyValuePair<string, object>> TraverseObjectGraphRecursively(this object obj, List<object> visited, string memberPath)
-    {
-        yield return new KeyValuePair<string, object>(memberPath, obj);
-        if (obj != null)
-        {
-            var typeOfOriginal = obj.GetType();
-            // ReferenceEquals is a mandatory approach
-            if (!IsPrimitive(typeOfOriginal) && !visited.Any(x => ReferenceEquals(obj, x)))
-            {
-                visited.Add(obj);
-                if (obj is IEnumerable objEnum)
-                {
-                    var originalEnumerator = objEnum.GetEnumerator();
-                    var index              = 0;
-                    while (originalEnumerator.MoveNext())
-                    {
-                        foreach (var result in originalEnumerator.Current.TraverseObjectGraphRecursively(visited, $@"{memberPath}[{index++}]"))
-                            yield return result;
-                    }
-                }
-                else
-                {
-                    foreach (var propertyInfo in typeOfOriginal.GetProperties(BindingFlags.Instance | BindingFlags.Public))
-                    {
-                        foreach (var result in propertyInfo.GetValue(obj).TraverseObjectGraphRecursively(visited, $@"{memberPath}.{propertyInfo.Name}"))
-                            yield return result;
-                    }
-                }
-            }
-        }
-    }
+	public static IEnumerable<KeyValuePair<string, object>> TraverseObjectGraph(this object original)
+	{
+		foreach (var result in original.TraverseObjectGraphRecursively(new List<object>(), original.GetType().Name))
+			yield return result;
+	}
 
-    /// <summary>
-    /// Check if type is a value-type, primitive type or string
-    /// </summary>
-    /// <param name="type"></param>
-    /// <returns></returns>
-    [DebuggerStepThrough]
-    private static bool IsPrimitive(this Type type)
-    {
-        if (type == typeof(string))
-            return true;
-        return type.IsValueType || type.IsPrimitive;
-    }
+	private static IEnumerable<KeyValuePair<string, object>> TraverseObjectGraphRecursively(this object obj, List<object> visited, string memberPath)
+	{
+		yield return new KeyValuePair<string, object>(memberPath, obj);
+		if (obj != null)
+		{
+			var typeOfOriginal = obj.GetType();
+			// ReferenceEquals is a mandatory approach
+			if (!IsPrimitive(typeOfOriginal) && !visited.Any(x => ReferenceEquals(obj, x)))
+			{
+				visited.Add(obj);
+				if (obj is IEnumerable objEnum)
+				{
+					var originalEnumerator = objEnum.GetEnumerator();
+					var index              = 0;
+					while (originalEnumerator.MoveNext())
+					{
+						foreach (var result in originalEnumerator.Current.TraverseObjectGraphRecursively(visited, $@"{memberPath}[{index++}]"))
+							yield return result;
+					}
+				}
+				else
+				{
+					foreach (var propertyInfo in typeOfOriginal.GetProperties(BindingFlags.Instance | BindingFlags.Public))
+					{
+						foreach (var result in propertyInfo.GetValue(obj).TraverseObjectGraphRecursively(visited, $@"{memberPath}.{propertyInfo.Name}"))
+							yield return result;
+					}
+				}
+			}
+		}
+	}
 
-    /// <summary>
-    /// Check if type is a value-type, primitype or string
-    /// </summary>
-    /// <param name="obj"></param>
-    /// <returns></returns>
-    [DebuggerStepThrough]
-    public static bool IsPrimitive(this object obj)
-        => obj == null || obj.GetType().IsPrimitive();
+	/// <summary>
+	/// Check if type is a value-type, primitive type or string
+	/// </summary>
+	/// <param name="type"></param>
+	/// <returns></returns>
+	private static bool IsPrimitive(this Type type)
+	{
+		if (type == typeof(string))
+			return true;
+		return type.IsValueType || type.IsPrimitive;
+	}
 
-    [DebuggerStepThrough]
-    public static bool IsNullableType(this Type type)
-    {
-        var typeInfo = type.GetTypeInfo();
+	/// <summary>
+	/// Check if type is a value-type, primitype or string
+	/// </summary>
+	/// <param name="obj"></param>
+	/// <returns></returns>
+	public static bool IsPrimitive(this object obj)
+		=> obj == null || obj.GetType().IsPrimitive();
 
-        return !typeInfo.IsValueType
-               || typeInfo.IsGenericType
-               && typeInfo.GetGenericTypeDefinition() == typeof(Nullable<>);
-    }
+	public static bool IsNullableType(this Type type)
+	{
+		var typeInfo = type.GetTypeInfo();
 
-    [DebuggerStepThrough]
-    public static Type MakeNullable(this Type type, bool nullable = true)
-        => type.IsNullableType() == nullable
-               ? type
-               : nullable
-                   ? typeof(Nullable<>).MakeGenericType(type)
-                   : type.GetGenericArguments()[0];
+		return !typeInfo.IsValueType
+		       || typeInfo.IsGenericType
+		       && typeInfo.GetGenericTypeDefinition() == typeof(Nullable<>);
+	}
 
-    [DebuggerStepThrough]
-    public static Type UnwrapNullableType(this Type type)
-        => Nullable.GetUnderlyingType(type) ?? type;
+	public static Type MakeNullable(this Type type, bool nullable = true)
+		=> type.IsNullableType() == nullable
+			   ? type
+			   : nullable
+				   ? typeof(Nullable<>).MakeGenericType(type)
+				   : type.GetGenericArguments()[0];
 
-    [DebuggerStepThrough]
-    public static Type UnwrapEnumType(this Type type)
-    {
-        var isNullable                = type.IsNullableType();
-        var underlyingNonNullableType = isNullable ? type.UnwrapNullableType() : type;
-        if (!underlyingNonNullableType.GetTypeInfo().IsEnum)
-            return type;
+	public static Type UnwrapNullableType(this Type type)
+		=> Nullable.GetUnderlyingType(type) ?? type;
 
-        var underlyingEnumType = Enum.GetUnderlyingType(underlyingNonNullableType);
-        return isNullable ? MakeNullable(underlyingEnumType) : underlyingEnumType;
-    }
+	public static Type UnwrapEnumType(this Type type)
+	{
+		var isNullable                = type.IsNullableType();
+		var underlyingNonNullableType = isNullable ? type.UnwrapNullableType() : type;
+		if (!underlyingNonNullableType.GetTypeInfo().IsEnum)
+			return type;
+
+		var underlyingEnumType = Enum.GetUnderlyingType(underlyingNonNullableType);
+		return isNullable ? MakeNullable(underlyingEnumType) : underlyingEnumType;
+	}
 }
--- System/tests/Extensions/TypeExtensionsTests.cs
+++ System/tests/Extensions/TypeExtensionsTests.cs
@@ -1,5 +1,54 @@
 ﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
+#nullable enable
+
+using Xunit;
+
 namespace Wangkanai.Extensions;
 
-public class TypeExtensionsTests { }
+public class TypeExtensionsTests
+{
+	private class Parent { }
+
+	private class Child : Parent { }
+
+	[Fact]
+	public void SingleTypeInheritanceChainToParent()
+	{
+		var parent = typeof(Parent);
+		var child  = typeof(Child);
+		var result = child.GetTypeInheritanceChainTo(parent);
+		Assert.Single(result);
+		Assert.Equal(typeof(Child), result[0]);
+	}
+
+	private class Animal { }
+
+	private class Dog : Animal { }
+
+	private class Husky : Dog { }
+
+	[Fact]
+	public void MultipleTypeInheritanceChainToParent()
+	{
+		var animal = typeof(Animal);
+		var dog    = typeof(Dog);
+		var husky  = typeof(Husky);
+
+		var result = husky.GetTypeInheritanceChainTo(animal);
+		Assert.Equal(2, result.Length);
+		Assert.Equal(typeof(Husky), result[0]);
+		Assert.Equal(typeof(Dog), result[1]);
+	}
+
+	private class Alone { }
+
+	[Fact]
+	public void SingleTypeInheritanceChainToSelf()
+	{
+		var alone  = typeof(Alone);
+		var result = alone.GetTypeInheritanceChainTo(alone);
+		Assert.Single(result);
+		Assert.Equal(typeof(Alone), result[0]);
+	}
+}
