6018c98e32f4d84042c37153c198166a169d51a0
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-07-04 05:43:29 +0700

    Refactor comments to take up less lines

--- Detection/src/DependencyInjection/IDetectionBuilder.cs
+++ Detection/src/DependencyInjection/IDetectionBuilder.cs
@@ -2,16 +2,10 @@
 
 namespace Microsoft.Extensions.DependencyInjection;
 
-/// <summary>
-///     Helper functions for configuring detection services.
-/// </summary>
+/// <summary>Helper functions for configuring detection services.</summary>
 public interface IDetectionBuilder
 {
-	/// <summary>
-	///     Gets the <see cref="IServiceCollection" /> services are attached to.
-	/// </summary>
-	/// <value>
-	///     The <see cref="IServiceCollection" /> services are attached to.
-	/// </value>
+	/// <summary>Gets the <see cref="IServiceCollection" /> services are attached to.</summary>
+	/// <value>The <see cref="IServiceCollection" /> services are attached to.</value>
 	IServiceCollection Services { get; }
 }
