9ed6e7d099c7548627210aaf8db7283d7c4da6eb
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-07-04 07:35:06 +0700

    update federation dependency injection

--- Federation/src/EntityFramework/Entities/IdentityPermission.cs
+++ Federation/src/EntityFramework/Entities/IdentityPermission.cs
@@ -5,10 +5,10 @@ namespace Wangkanai.Federation.Entities;
 [Flags]
 public enum IdentityPermission
 {
-	None   = 0,
-	View   = 1 << 0,
-	Edit   = 1 << 1,
-	Create = 1 << 2,
-	Delete = 1 << 3,
-	All    = ~None
+	None   = 0,      // 0000
+	View   = 1 << 0, // 0001
+	Edit   = 1 << 1, // 0010
+	Create = 1 << 2, // 0100
+	Delete = 1 << 3, // 1000
+	All    = ~None   // 1111
 }
