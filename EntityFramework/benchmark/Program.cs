5c39c34413496be591ddeb7f9184627c8680f111
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-07-07 23:28:30 +0700

    [EntityFramework] Refactoring project library

--- EntityFramework/benchmark/Program.cs
+++ EntityFramework/benchmark/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿global using BenchmarkDotNet.Running;
+global using BenchmarkDotNet.Attributes;
+
+BenchmarkRunner.Run<EntityFrameworkBenchmark>();
