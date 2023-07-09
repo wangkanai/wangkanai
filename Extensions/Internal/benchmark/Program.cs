421bf23cd9ecc5b449ac9957efdbeee1f2800b33
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-07-09 21:57:30 +0700

    [Identity] add benchmark project

--- Extensions/Internal/benchmark/Program.cs
+++ Extensions/Internal/benchmark/Program.cs
@@ -1,5 +1,7 @@
-﻿global using BenchmarkDotNet.Attributes;
+﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+global using BenchmarkDotNet.Attributes;
 global using BenchmarkDotNet.Running;
 
-	
+
 BenchmarkRunner.Run<FirstBenchmark>();
--- Identity/benchmark/Program.cs
+++ Identity/benchmark/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+global using BenchmarkDotNet.Attributes;
+global using BenchmarkDotNet.Running;
+
+
+BenchmarkRunner.Run<FirstBenchmark>();
+
+[MemoryDiagnoser]
+public class FirstBenchmark
+{
+	[Benchmark]
+	public void Benchmark()
+	{
+		// Method intentionally left empty.
+	}
+}
