be54b70756eca7ddab4c88819c410881dda4099a
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-07-12 08:00:06 +0700

    [Testing] Add unit tests code coverage

---
+++ Testing/tests/Extensions/AssertionHelperExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Wangkanai.Testing.Tests.Extensions;
+
+public class AssertionHelperExtensionsTests
+{
+	[Fact]
+	public void Null_Services()
+	{
+		ServiceCollection       services    = null!;
+		List<ServiceDescriptor> descriptors = new List<ServiceDescriptor>();
+		descriptors.AssertServices(services);
+		Assert.Null(services);
+		Assert.Empty(descriptors);
+		
+	}
+	
+	[Fact]
+	public void Null_Descriptors()
+	{
+		ServiceCollection       services    = new ServiceCollection();
+		List<ServiceDescriptor> descriptors = null!;
+		Assert.Throws<NullReferenceException>(() => descriptors.AssertServices(services));
+	}
+}
