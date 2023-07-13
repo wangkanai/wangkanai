9d85d424ab86113d65715e58d87dfc61c37cae2b
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-07-13 15:50:02 +0700

    fixed sign build & publish

--- Blazor/sign.ps1
+++ Blazor/sign.ps1
@@ -1,16 +1,16 @@
 remove-item -path .\signed\*.*
 
 dotnet --version
-dotnet clean Blazor.slnf
-dotnet restore Blazor.slnf
-dotnet build -c Release Blazor.slnf
+dotnet clean .\src\
+dotnet restore .\src\
+dotnet build -c Release .\src\
 Get-ChildItem .\src\ -Recurse Wangkanai.*.dll | where { $_.Name -like "*release*" } | foreach {
     signtool sign /fd SHA256 /n "Sarin Na Wangkanai" $_.FullName
 }
 
 Remove-Item .\artifacts\*.*
 
-dotnet pack Blazor.slnf -c Release -o .\artifacts --include-symbols -p:SymbolPackageFormat=snupkg
+dotnet pack .\src\ -c Release -o .\artifacts --include-symbols -p:SymbolPackageFormat=snupkg
 
 dotnet nuget sign .\artifacts\*.nupkg -v diag --timestamper http://timestamp.digicert.com --certificate-subject-name "Sarin Na Wangkanai" -o .\signed
 dotnet nuget sign .\artifacts\*.snupkg -v diag --timestamper http://timestamp.digicert.com --certificate-subject-name "Sarin Na Wangkanai" -o .\signed
--- Tabler/sign.ps1
+++ Tabler/sign.ps1
@@ -2,15 +2,15 @@ remove-item -path .\signed\*.*
 new-item -Path signed -ItemType Directory -Force
 
 dotnet --version
-dotnet clean Tabler.slnf
-dotnet restore Tabler.slnf
-dotnet build -c Release Tabler.slnf
+dotnet clean .\src\
+dotnet restore .\src\
+dotnet build -c Release .\src\
 Get-ChildItem .\src\ -Recurse Wangkanai.*.dll | where { $_.Name -like "*release*" } | foreach {
     signtool sign /fd SHA256 /n "Sarin Na Wangkanai" $_.FullName
 }
 Remove-Item .\artifacts\*.*
 
-dotnet pack Tabler.slnf -c Release -o .\artifacts --include-symbols -p:SymbolPackageFormat=snupkg
+dotnet pack .\src\ -c Release -o .\artifacts --include-symbols -p:SymbolPackageFormat=snupkg
 
 dotnet nuget sign .\artifacts\*.nupkg -v diag --timestamper http://timestamp.digicert.com --certificate-subject-name "Sarin Na Wangkanai" -o .\signed
 dotnet nuget sign .\artifacts\*.snupkg -v diag --timestamper http://timestamp.digicert.com --certificate-subject-name "Sarin Na Wangkanai" -o .\signed
