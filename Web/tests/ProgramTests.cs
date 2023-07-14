19e256873450bdbe2122627e1e780d8124c74c58
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-07-14 18:18:31 +0700

    web add unit tests

---
+++ Web/tests/ProgramTests.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Wangkanai.Web.Server.Tests;
+
+public class ProgramTests
+{
+	[Fact]
+	public void BuilderServicesAddRouting()
+	{
+		var builder = WebApplication.CreateBuilder();
+		builder.Services.AddRouting(options => options.LowercaseUrls = true);
+		var app = builder.Build();
+		Assert.NotNull(app.Services.GetService(typeof(Microsoft.AspNetCore.Routing.IRouter)));
+	}
+}
