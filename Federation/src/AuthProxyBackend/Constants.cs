afa1546de5174b8aa0639f7400d7dcf3c85e269f
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-07-18 10:36:22 +0700

    [Federation] Refactor folder structure

---
+++ Federation/src/AuthProxyBackend/Constants.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Federation.AuthProxyBackend;
+
+public static class Constants
+{
+	internal const string MiddlewareMarker = "Wangkanai.Federation.AuthProxyBackend.MiddlewareMarker";
+}
--- Federation/src/BackendForFrontend/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
-
-namespace Wangkanai.Federation.BackendForFrontend;
-
-public static class Constants
-{
-	internal const string MiddlewareMarker = "Wangkanai.Federation.BackendForFrontend.MiddlewareMarker";
-}
