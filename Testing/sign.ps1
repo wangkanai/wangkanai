b931f9c104b06d9139221b06bcc14a3fcdce926e
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-07-20 13:28:23 +0700

    publish nuget packages

--- Testing/sign.ps1
+++ Testing/sign.ps1
@@ -16,5 +16,5 @@ dotnet pack .\src\ -c Release -o .\artifacts\ --include-symbols -p:SymbolPackage
 dotnet nuget sign .\artifacts\*.nupkg -v diag --timestamper http://timestamp.digicert.com --certificate-subject-name "Sarin Na Wangkanai" -o .\signed
 dotnet nuget sign .\artifacts\*.snupkg -v diag --timestamper http://timestamp.digicert.com --certificate-subject-name "Sarin Na Wangkanai" -o .\signed
 
-dotnet nuget push .\signed\*.nupkg -k $env:NUGET_API_KEY -s https://api.nuget.org/v3/index.json --skip-duplicate
-# dotnet nuget push .\signed\*.snupkg -k $env:NUGET_API_KEY -s https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet nuget push .\signed\*.nupkg -k $env:NUGET_API_KEY  -s https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet nuget push .\signed\*.nupkg -k $env:GITHUB_API_PAT -s https://nuget.pkg.github.com/wangkanai/index.json --skip-duplicate
