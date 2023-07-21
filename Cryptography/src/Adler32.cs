ee62c8391ce7fc881f5bbcda54db6d4c0e14ba6b
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-07-22 00:21:31 +0700

    lay the foundation of adler32

--- Cryptography/src/Adler32.cs
+++ Cryptography/src/Adler32.cs
@@ -4,20 +4,52 @@ using System.Security.Cryptography;
 
 namespace Wangkanai.Cryptography;
 
-public class Adler32 : HashAlgorithm
+public sealed class Adler32 : HashAlgorithm
 {
+	// Reference: https://en.wikipedia.org/wiki/Adler-32
+
+	private const ushort Base = 0xFFF1; // 65521
+	private const ushort Max  = 0x15B0; //  5552
+
+	private ushort _a;
+	private ushort _b;
+
+	private Adler32() => Initialize();
+
+	//public static int Checksum(string text) { }
+
+	public override int HashSize => 32;
+
 	public override void Initialize()
 	{
-		throw new NotImplementedException();
+		// reset the sum values
+		_a = 1;
+		_b = 0;
 	}
 
-	protected override void HashCore(byte[] array, int ibStart, int cbSize)
+	protected override void HashCore(byte[] data, int start, int length)
 	{
-		throw new NotImplementedException();
+		var index = start;
+		var end   = start + length;
+
+		while (end > 0)
+		{
+			var endian = end < Max ? end : Max;
+			end -= endian;
+			for (int i = 0; i < endian; i++)
+			{
+				_a += data[index++];
+				_b += _a;
+			}
+
+			_a %= Base;
+			_b %= Base;
+		}
 	}
 
 	protected override byte[] HashFinal()
 	{
-		throw new NotImplementedException();
+		var concat = (uint)(_b << 16) | _a;
+		return BitConverter.GetBytes(concat);
 	}
 }
