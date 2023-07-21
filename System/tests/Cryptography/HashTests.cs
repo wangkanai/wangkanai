e15192e11df65d77e9fda32dfd6e6df5b1275469
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-07-21 14:06:17 +0700

    add cryptography hash unit tests

---
+++ System/tests/Cryptography/HashTests.cs
@@ -0,0 +1,32 @@
+// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using Xunit;
+
+namespace Wangkanai.Cryptography;
+
+public class HashTests
+{
+	[Fact]
+	public void Md5_Null()
+	{
+		Assert.Throws<ArgumentNullException>(() => Hash.HashMd5(null!));
+	}
+	
+	[Fact]
+	public void Sha256_Null()
+	{
+		Assert.Throws<ArgumentNullException>(() => Hash.HashSha256(null!));
+	}
+	
+	[Fact]
+	public void She384_Null()
+	{
+		Assert.Throws<ArgumentNullException>(() => Hash.HashSha384(null!));
+	}
+	
+	[Fact]
+	public void Sha512_Null()
+	{
+		Assert.Throws<ArgumentNullException>(() => Hash.HashSha512(null!));
+	}
+}
