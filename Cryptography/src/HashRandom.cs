77369d48b340c52355614e485849f9eeb7cb8d8c
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-07-21 19:56:27 +0700

    add hash random unit tests for better code coverage

--- Cryptography/src/HashRandom.cs
+++ Cryptography/src/HashRandom.cs
@@ -15,7 +15,7 @@ public class HashRandom : Random
 	public HashRandom() { }
 
 	public HashRandom(int seedIgnored) { }
-	
+
 	public static byte[] CreateRandomKey(int length)
 	{
 		length.ThrowIfLessThan(1);
@@ -24,25 +24,67 @@ public class HashRandom : Random
 		Generator.GetBytes(bytes);
 		return bytes;
 	}
-	
+
 	public static string CreateUniqueId(int length = 32, OutputFormat format = OutputFormat.Base64Url)
 	{
 		length.ThrowIfLessThan(1);
-		
+
 		var bytes = CreateRandomKey(length);
 		return format switch
 		{
-			OutputFormat.Base64Url   => Base64Url.Encode(bytes),
-			OutputFormat.Base64      => Convert.ToBase64String(bytes),
-			OutputFormat.Hexadecimal => BitConverter.ToString(bytes).Replace("-", ""),
-			_                        => throw new ArgumentException("Invalid output format", nameof(format))
+			OutputFormat.Base64Url => Base64Url.Encode(bytes),
+			OutputFormat.Base64    => Convert.ToBase64String(bytes),
+			OutputFormat.Hex       => BitConverter.ToString(bytes).Replace("-", ""),
+			_                      => throw new ArgumentException("Invalid output format", nameof(format))
 		};
+	}
+
+	public override int Next()
+	{
+		Generator.GetBytes(_buffer);
+		return BitConverter.ToInt32(_buffer, 0) & 0x7FFFFFFF;
+	}
+
+	public override int Next(int maxValue)
+	{
+		maxValue.ThrowIfLessThan(0);
+		return Next(0, maxValue);
+	}
+
+	public override int Next(int minValue, int maxValue)
+	{
+		minValue.ThrowIfMoreThan(maxValue);
+		if (minValue == maxValue) return minValue;
+		var diff = maxValue - minValue;
+
+		while (true)
+		{
+			Generator.GetBytes(_buffer);
+			var random = BitConverter.ToUInt32(_buffer, 0);
+			var max    = 1 + (long)uint.MaxValue;
+			var remainder = max % diff;
+			if (random < max - remainder)
+				return (int)(minValue + (random % diff));
+		}
+	}
+
+	public override double NextDouble()
+	{
+		Generator.GetBytes(_buffer);
+		var random = BitConverter.ToUInt32(_buffer, 0);
+		return random / (1.0 + uint.MaxValue);
+	}
+
+	public override void NextBytes(byte[] buffer)
+	{
+		buffer.ThrowIfNull();
+		Generator.GetBytes(buffer);
 	}
 
 	public enum OutputFormat
 	{
 		Base64Url,
 		Base64,
-		Hexadecimal
+		Hex
 	}
 }
