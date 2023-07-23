f5e31cc5365077be4fed26432e2eb21146a44a3b
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-07-23 07:39:09 +0700

    its better to write the constant in hex for easy to understanding.

--- Cryptography/src/Adler32.cs
+++ Cryptography/src/Adler32.cs
@@ -11,12 +11,12 @@ public sealed class Adler32 : HashAlgorithm
 
 	private const ushort Base = 0xFFF1; // 65521
 	private const ushort Max  = 0x15B0; //  5552
+	private const ushort Mod  = 0xFFFF; // 65535
 
 	private ushort _a;
-	private ushort _b; 
-	
-	public Adler32()
-		=> Initialize();
+	private ushort _b;
+
+	public Adler32() => Initialize();
 
 	public static int Checksum(string text)
 		=> Checksum(Encoding.ASCII.GetBytes(text));
@@ -24,11 +24,13 @@ public sealed class Adler32 : HashAlgorithm
 	public static int Checksum(byte[] bytes)
 	{
 		bytes.ThrowIfNull();
-		
+
 		var adler32 = new Adler32();
 		adler32.HashCore(bytes, 0, bytes.Length);
 		return BitConverter.ToInt32(adler32.HashFinal(), 0);
 	}
+
+	#region override
 
 	public override int HashSize => 32;
 
@@ -64,4 +66,6 @@ public sealed class Adler32 : HashAlgorithm
 		var concat = (uint)(_b << 16) | _a;
 		return BitConverter.GetBytes(concat);
 	}
+
+	#endregion
 }
