eb8b21c34607ff935579507a360c3c22f569b3db
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-09-23 23:29:24 +0700

    [Tabler] header dynamic configuration sample

---
+++ Tabler/src/Components/Models/BgColor.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Tabler.Components.Models;
+
+public enum BgColor
+{
+	None = 0,
+	Transparent,
+	Blue,
+	Azure,
+	Indigo,
+	Purple,
+	Pink,
+	Red,
+	Orange,
+	Yellow,
+	Lime,
+	Green,
+	Teal,
+	Cyan,
+	Dark,
+	Muted,
+}
---
+++ Tabler/src/Components/Models/TextColor.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Tabler.Components.Models;
+
+public enum TextColor
+{
+	None = 0,
+	Blue,
+	Azure,
+	Indigo,
+	Purple,
+	Pink,
+	Red,
+	Orange,
+	Yellow,
+	Lime,
+	Green,
+	Teal,
+	Cyan,
+	Dark,
+	Muted,
+}
