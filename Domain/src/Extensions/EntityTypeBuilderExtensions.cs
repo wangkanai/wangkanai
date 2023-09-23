e6c6bb14ee5d58690f1ca526753a288d83d50ed7
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-09-23 12:35:18 +0700

    add domain unit tests

--- Domain/src/Extensions/EntityTypeBuilderExtensions.cs
+++ Domain/src/Extensions/EntityTypeBuilderExtensions.cs
@@ -1,22 +1,29 @@
 ﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
+using System.Numerics;
+
 using Wangkanai.Domain;
 
 namespace Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 public static class EntityTypeBuilderExtensions
 {
-	public static void NewKeyOnAdd<T>(this EntityTypeBuilder<T> builder)
+	public static void NewGuidOnAdd<T>(this EntityTypeBuilder<T> builder)
 		where T : class, IEntity<Guid>
 		=> builder.Property(x => x.Id)
 		          .ValueGeneratedOnAdd();
-	
+
+	public static void NewKeyOnAdd<T>(this EntityTypeBuilder<T> builder)
+		where T : class, IEntity<int>
+		=> builder.Property(x => x.Id)
+		          .ValueGeneratedOnAdd();
+
 	public static void HasDefaultCreated<T>(this EntityTypeBuilder<T> builder)
 		where T : class, ICreatedEntity
 		=> builder.Property(x => x.Created)
 		          .HasDefaultValue(DateTime.Now)
 		          .ValueGeneratedOnAdd();
-	
+
 	public static void HasDefaultCreatedAndUpdated<T>(this EntityTypeBuilder<T> builder)
 		where T : class, ICreatedEntity, IUpdatedEntity
 	{
--- Domain/tests/Extensions/EntityTypeBuilderTests.cs
+++ Domain/tests/Extensions/EntityTypeBuilderTests.cs
@@ -10,12 +10,24 @@ namespace Wangkanai.Domain.Extensions;
 public class EntityTypeBuilderTests
 {
 	[Fact]
-	public void NewKeyOnAdd_ShouldHaveId()
+	public void NewKeyOnAdd_Guid_ShouldHaveId()
 	{
 		var builder = MockExtensions.GetEntityTypeBuilder<GuidEntity, GuidEntityTypeConfiguration>();
+		var entity  = builder.Metadata;
+		var id      = entity.FindProperty(nameof(GuidEntity.Id));
+		
+		builder.NewGuidOnAdd();
+		Assert.True(id!.ValueGenerated == ValueGenerated.OnAdd);
+	}
+
+	[Fact]
+	public void NewKeyOnAdd_Int_ShouldHaveId()
+	{
+		var builder = MockExtensions.GetEntityTypeBuilder<IntEntity, IntEntityTypeConfiguration>();
+		var entity  = builder.Metadata;
+		var id      = entity.FindProperty(nameof(GuidEntity.Id));
+		
 		builder.NewKeyOnAdd();
-		var entity = builder.Metadata;
-		var id     = entity.FindProperty(nameof(GuidEntity.Id));
 		Assert.True(id!.ValueGenerated == ValueGenerated.OnAdd);
 	}
 }
