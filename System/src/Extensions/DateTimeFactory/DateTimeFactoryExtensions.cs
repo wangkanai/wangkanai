6d0aaae6133169e3c2a9653f9f373b41dd76c878
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-09-25 15:16:53 +0700

    add to all months

--- System/src/Extensions/DateTimeFactory/DateTimeFactoryExtensions.cs
+++ System/src/Extensions/DateTimeFactory/DateTimeFactoryExtensions.cs
@@ -5,15 +5,15 @@ namespace Wangkanai.Extensions.DateTimeFactory;
 public static class DateTimeFactoryExtensions
 {
 	public static DateTime January(this   int day, int year) => new(year, 1, day,0,0,0,DateTimeKind.Utc);
-	public static DateTime February(this  int day, int year) => new(year, 2, day);
-	public static DateTime March(this     int day, int year) => new(year, 3, day);
-	public static DateTime April(this     int day, int year) => new(year, 4, day);
-	public static DateTime May(this       int day, int year) => new(year, 5, day);
-	public static DateTime June(this      int day, int year) => new(year, 6, day);
-	public static DateTime July(this      int day, int year) => new(year, 7, day);
-	public static DateTime August(this    int day, int year) => new(year, 8, day);
-	public static DateTime September(this int day, int year) => new(year, 9, day);
-	public static DateTime October(this   int day, int year) => new(year, 10, day);
-	public static DateTime November(this  int day, int year) => new(year, 11, day);
-	public static DateTime December(this  int day, int year) => new(year, 12, day);
+	public static DateTime February(this  int day, int year) => new(year, 2, day,0,0,0,DateTimeKind.Utc);
+	public static DateTime March(this     int day, int year) => new(year, 3, day,0,0,0,DateTimeKind.Utc);
+	public static DateTime April(this     int day, int year) => new(year, 4, day,0,0,0,DateTimeKind.Utc);
+	public static DateTime May(this       int day, int year) => new(year, 5, day,0,0,0,DateTimeKind.Utc);
+	public static DateTime June(this      int day, int year) => new(year, 6, day,0,0,0,DateTimeKind.Utc);
+	public static DateTime July(this      int day, int year) => new(year, 7, day,0,0,0,DateTimeKind.Utc);
+	public static DateTime August(this    int day, int year) => new(year, 8, day,0,0,0,DateTimeKind.Utc);
+	public static DateTime September(this int day, int year) => new(year, 9, day,0,0,0,DateTimeKind.Utc);
+	public static DateTime October(this   int day, int year) => new(year, 10, day,0,0,0,DateTimeKind.Utc);
+	public static DateTime November(this  int day, int year) => new(year, 11, day,0,0,0,DateTimeKind.Utc);
+	public static DateTime December(this  int day, int year) => new(year, 12, day,0,0,0,DateTimeKind.Utc);
 }
