0e0019a5ad38c6e5de5ada89d8e58d9256c01867
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-09-28 10:49:20 +0700

    add generic type info unit tests

---
+++ System/tests/Domain/GenericTypeInfoTests.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+#nullable enable
+using Xunit;
+
+namespace Wangkanai.Domain;
+
+public class GenericTypeInfoTests
+{
+	[Fact]
+	public void TypeIsNull()
+	{
+		GenericTypeInfo<Parent> _null = null!;
+
+		Assert.Throws<NullReferenceException>(() => _null.GetType());
+		Assert.Throws<NullReferenceException>(() => _null.GetService<Parent>());
+		Assert.Throws<NullReferenceException>(() => _null.IsAssignableTo(nameof(Parent)));
+		Assert.Throws<NullReferenceException>(() => _null.AllSubclasses);
+	}
+}
