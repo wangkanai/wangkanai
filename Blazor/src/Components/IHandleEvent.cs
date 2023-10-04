7178e2dec0df5f214be9d02ac2bb7e6854dad82e
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-10-04 22:37:11 +0700

    [Blazor] clean up for .net 8.0

--- Blazor/src/Components/IHandleEvent.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
-
-namespace Wangkanai.Blazor.Components;
-
-public interface IHandleEvent
-{
-	Task HandleEventAsync(EventCallbackWorkItem item, object? arg);
-}
--- Blazor/src/Web/Core/BlazorComponentBase.cs
+++ Blazor/src/Web/Core/BlazorComponentBase.cs
@@ -1,8 +1,7 @@
 // Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
 using Microsoft.AspNetCore.Components;
-
-using Wangkanai.Blazor.Components.Rendering;
+using Microsoft.AspNetCore.Components.Rendering;
 
 namespace Wangkanai.Blazor;
 
