74db0d2d2bef57107984a2b3e12a457260b83b89
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-10-19 12:17:36 +0700

    tabler sample migrate from auth to standard

---
+++ Tabler/samples/Client/TextColor.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Host.Main.Client;
+
+public enum TextColor
+{
+	Red,
+	Yellow,
+	Green,
+	Blue,
+	Indigo,
+	Purple,
+	Pink,
+	Orange,
+	Teal,
+	Cyan,
+	Gray,
+	GrayDark,
+	None
+}
