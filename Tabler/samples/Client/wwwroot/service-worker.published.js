7ceb1a0f09c67190570e8d0505c7e737af5d1b49
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-10-19 23:17:12 +0700

    add the remaining pages

--- Tabler/samples/Client/wwwroot/service-worker.published.js
+++ Tabler/samples/Client/wwwroot/service-worker.published.js
@@ -8,8 +8,8 @@ self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
-const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
-const offlineAssetsExclude = [ /^service-worker\.js$/ ];
+const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/];
+const offlineAssetsExclude = [/^service-worker\.js$/];
 
 // Replace with your base path if you are hosting on a subfolder. Ensure there is a trailing '/'.
 const base = "/";
@@ -23,7 +23,7 @@ async function onInstall(event) {
 	const assetsRequests = self.assetsManifest.assets
 		.filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
 		.filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
-		.map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
+		.map(asset => new Request(asset.url, {integrity: asset.hash, cache: 'no-cache'}));
 	await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
 }
 
