403fbad12a90b19009d37d89edc2d420307d5578
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-10-21 12:29:46 +0700

    refactor detection version extension

--- Detection/src/Extensions/VersionExtensions.cs
+++ Detection/src/Extensions/VersionExtensions.cs
@@ -6,20 +6,20 @@ namespace Wangkanai.Detection.Extensions;
 
 public static class VersionExtensions
 {
-	private static readonly Regex VersionCleanupRegex = new(@"\+|\-|\s|beta",
-	                                                        RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase, Constants.RegexTimeout);
+	private const           RegexOptions Options      = RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase;
+	private static readonly Regex        CleanupRegex = new(@"\+|\-|\s|beta", Options, Constants.RegexTimeout);
 
 	public static Version ToVersion(this string version)
 	{
 		if (string.IsNullOrEmpty(version))
 			return new Version();
 
-		version = VersionCleanupRegex.Replace(version, string.Empty);
+		version = CleanupRegex.Replace(version, string.Empty);
 
-		if (!version.Contains(".", StringComparison.Ordinal))
+		if (!version.Contains('.', StringComparison.Ordinal))
 			version += ".0";
 
-		if (version.Contains(",", StringComparison.Ordinal))
+		if (version.Contains(',', StringComparison.Ordinal))
 			version = version.Replace(",", ".");
 
 		return Version.TryParse(version, out var parsedVersion)
