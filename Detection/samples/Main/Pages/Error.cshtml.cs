faf1f7e36b1906b221be5763bdcc398a5c5f5a29
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-10-24 05:04:47 +0700

    refactor detection sample error page

--- Detection/samples/Main/Pages/Error.cshtml.cs
+++ Detection/samples/Main/Pages/Error.cshtml.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,23 +7,15 @@ namespace Host.Main.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
-public class ErrorModel : PageModel
+public class ErrorModel(ILogger<ErrorModel> logger) : PageModel
 {
-    public string? RequestId { get; set; }
+	public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-    private readonly ILogger<ErrorModel> _logger;
-
-    public ErrorModel(ILogger<ErrorModel> logger)
-    {
-        _logger = logger;
-    }
-
-    public void OnGet()
-    {
-        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-        _logger.LogError(RequestId, Activity.Current?.StatusDescription);
-    }
+	public void OnGet()
+	{
+		RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+		logger.LogError(RequestId, Activity.Current?.StatusDescription);
+	}
 }
-
