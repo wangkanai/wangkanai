786d77e2b0fc3e1bc994d7c83a7067649b31e543
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-10-23 20:01:32 +0700

    fixed namespace mistake

--- Extensions/CommandLine/tests/CommandArgumentTests.cs
+++ Extensions/CommandLine/tests/CommandArgumentTests.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
-using Wangkanai.Extensions.CommandLine;
-
-namespace Wangkanai.CommandLine;
+namespace Wangkanai.Extensions.CommandLine;
 
 #pragma warning disable CA1861
 public class CommandArgumentTests
