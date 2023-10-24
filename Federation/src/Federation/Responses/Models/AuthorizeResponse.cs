c4f6eeaa365e718b437cc2b2f43c1b5797a8c926
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-10-24 07:10:43 +0700

    refactor unit tests

--- Federation/src/Federation/Responses/Models/AuthorizeResponse.cs
+++ Federation/src/Federation/Responses/Models/AuthorizeResponse.cs
@@ -11,15 +11,15 @@ public class AuthorizeResponse
 	public string RedirectUri => Request?.RedirectUri;
 	public string State       => Request?.State;
 	public string Scope       => Request?.ValidationResources?.RawScopeValues.SeparateToSpace();
+	public bool   IsError     => Error.IsExist();
 
+	public int    AccessTokenLifetime { get; set; }
 	public string IdentityToken       { get; set; }
 	public string AccessToken         { get; set; }
-	public int    AccessTokenLifetime { get; set; }
 	public string SessionState        { get; set; }
 
 	public string Code        { get; set; }
 	public string Issuer      { get; set; }
 	public string Error       { get; set; }
 	public string Description { get; set; }
-	public bool   IsError     => Error.IsExist();
 }
