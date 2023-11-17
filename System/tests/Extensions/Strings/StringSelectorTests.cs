a697a81ec48fd19d0bc60af8e7dbe9bf65825693
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-11-17 23:33:56 +0700

    refactor & reformat string slash method extensions

--- System/tests/Extensions/Strings/StringSelectorTests.cs
+++ System/tests/Extensions/Strings/StringSelectorTests.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
-
 using Wangkanai.Exceptions;
 
 using Xunit;
@@ -10,116 +9,31 @@ namespace Wangkanai.Extensions.Strings;
 public class StringSelectorTests
 {
 	string? _null  = null;
-	string _empty = string.Empty;
-	string _space = " ";
+	string  _empty = string.Empty;
+	string  _space = " ";
 
-	[Fact]
-	public void Null()
-	{
-		Assert.Throws<ArgumentNullException>(() => _null!.EscapeSelector());
-	}
-
-	[Fact]
-	public void Empty()
-	{
-		Assert.Throws<ArgumentEmptyException>(() => _empty.EscapeSelector());
-	}
-
-	[Fact]
-	public void Space()
-	{
-		Assert.Equal(_space, _space.EscapeSelector());
-	}
-
-	[Fact]
-	public void Parenthesis()
-	{
-		Assert.Equal(@"\\{test\\}", "{test}".EscapeSelector());
-	}
-
-	[Fact]
-	public void Slash()
-	{
-		Assert.Equal(@"test\\/test", "test/test".EscapeSelector());
-	}
-
-	[Fact]
-	public void Dot()
-	{
-		Assert.Equal(@"test\\.test", "test.test".EscapeSelector());
-	}
-
-	[Fact]
-	public void Hash()
-	{
-		Assert.Equal(@"test\\#test", "test#test".EscapeSelector());
-	}
-
-	[Fact]
-	public void Comma()
-	{
-		Assert.Equal(@"test\\,test", "test,test".EscapeSelector());
-	}
-
-	[Fact]
-	public void Colon()
-	{
-		Assert.Equal(@"test\\:test", "test:test".EscapeSelector());
-	}
-
-	[Fact]
-	public void Semicolon()
-	{
-		Assert.Equal(@"test\\;test", "test;test".EscapeSelector());
-	}
-
-	[Fact]
-	public void Plus()
-	{
-		Assert.Equal(@"test\\+test", "test+test".EscapeSelector());
-	}
-
-	[Fact]
-	public void Equal()
-	{
-		Assert.Equal(@"\\=test\\=", "=test=".EscapeSelector());
-	}
-
-	[Fact]
-	public void Question()
-	{
-		Assert.Equal(@"test\\?test", "test?test".EscapeSelector());
-	}
-
-	[Fact]
-	public void Ampersand()
-	{
-		Assert.Equal(@"test\\&test", "test&test".EscapeSelector());
-	}
-
-	[Fact]
-	public void Percent()
-	{
-		Assert.Equal(@"test\\%test", "test%test".EscapeSelector());
-	}
-
-	[Fact]
-	public void Tilde()
-	{
-		Assert.Equal(@"test\\~test", "test~test".EscapeSelector());
-	}
-
-	[Fact]
-	public void Quote()
-	{
-		Assert.Equal(@"test\\'test", "test'test".EscapeSelector());
-	}
-
-	[Fact]
-	public void DoubleQuote()
-	{
-		Assert.Equal("test\\\\\"test", "test\"test".EscapeSelector());
-	}
+	[Fact] public void Null()             => Assert.Throws<ArgumentNullException>(() => _null!.EscapeSelector());
+	[Fact] public void Empty()            => Assert.Throws<ArgumentEmptyException>(() => _empty.EscapeSelector());
+	[Fact] public void Space()            => Assert.Equal(_space, _space.EscapeSelector());
+	[Fact] public void Parenthesis()      => Assert.Equal(@"\\{test\\}", "{test}".EscapeSelector());
+	[Fact] public void Slash()            => Assert.Equal(@"test\\/test", "test/test".EscapeSelector());
+	[Fact] public void Dot()              => Assert.Equal(@"test\\.test", "test.test".EscapeSelector());
+	[Fact] public void Hash()             => Assert.Equal(@"test\\#test", "test#test".EscapeSelector());
+	[Fact] public void Comma()            => Assert.Equal(@"test\\,test", "test,test".EscapeSelector());
+	[Fact] public void Colon()            => Assert.Equal(@"test\\:test", "test:test".EscapeSelector());
+	[Fact] public void Semicolon()        => Assert.Equal(@"test\\;test", "test;test".EscapeSelector());
+	[Fact] public void Plus()             => Assert.Equal(@"test\\+test", "test+test".EscapeSelector());
+	[Fact] public void Equal()            => Assert.Equal(@"\\=test\\=", "=test=".EscapeSelector());
+	[Fact] public void Question()         => Assert.Equal(@"test\\?test", "test?test".EscapeSelector());
+	[Fact] public void Ampersand()        => Assert.Equal(@"test\\&test", "test&test".EscapeSelector());
+	[Fact] public void Percent()          => Assert.Equal(@"test\\%test", "test%test".EscapeSelector());
+	[Fact] public void Tilde()            => Assert.Equal(@"test\\~test", "test~test".EscapeSelector());
+	[Fact] public void Quote()            => Assert.Equal(@"test\\'test", "test'test".EscapeSelector());
+	[Fact] public void DoubleQuote()      => Assert.Equal("test\\\\\"test", "test\"test".EscapeSelector());
+	[Fact] public void LeftBracket()      => Assert.Equal(@"test\\[test", "test[test".EscapeSelector());
+	[Fact] public void RightBracket()     => Assert.Equal(@"test\\]test", "test]test".EscapeSelector());
+	[Fact] public void LeftParenthesis()  => Assert.Equal(@"test\\(test", "test(test".EscapeSelector());
+	[Fact] public void RightParenthesis() => Assert.Equal(@"test\\)test", "test)test".EscapeSelector());
 
 	// [Fact]
 	// public void Backslash()
@@ -127,27 +41,4 @@ public class StringSelectorTests
 	// 	Assert.Equal(@"\\test", "\\test".EscapeSelector());
 	// }
 
-	[Fact]
-	public void LeftBracket()
-	{
-		Assert.Equal(@"test\\[test", "test[test".EscapeSelector());
-	}
-
-	[Fact]
-	public void RightBracket()
-	{
-		Assert.Equal(@"test\\]test", "test]test".EscapeSelector());
-	}
-
-	[Fact]
-	public void LeftParenthesis()
-	{
-		Assert.Equal(@"test\\(test", "test(test".EscapeSelector());
-	}
-
-	[Fact]
-	public void RightParenthesis()
-	{
-		Assert.Equal(@"test\\)test", "test)test".EscapeSelector());
-	}
 }
