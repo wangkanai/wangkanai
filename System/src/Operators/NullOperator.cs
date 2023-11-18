e68e836ccbb561a357191cfedba25d7eae195e2e
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-11-18 12:38:07 +0700

    [System] Refactor & reformat system operators
    - simplify the operator code
    - fix warning CS8600
    - fix warning CS8601
    - fix warning CS8618

--- System/src/Operators/NullOperator.cs
+++ System/src/Operators/NullOperator.cs
@@ -4,35 +4,7 @@ namespace Wangkanai.Operators;
 
 public interface INullOperator<T>
 {
-	bool HasValue(T         value);
+	bool HasValue(T value);
+
 	bool AddIfNotNull(ref T accumulator, T value);
-}
-
-public sealed class StructNullOperator<T> : INullOperator<T>
-	where T : struct
-{
-	public bool HasValue(T value)
-		=> true;
-
-	public bool AddIfNotNull(ref T accumulator, T value)
-	{
-		accumulator = Operator<T>.Add(accumulator, value);
-		return true;
-	}
-}
-
-public sealed class ClassNullOperator<T> : INullOperator<T>
-	where T : class
-{
-	public bool HasValue(T value)
-		=> value != null;
-
-	public bool AddIfNotNull(ref T accumulator, T value)
-	{
-		if (value == null)
-			return false;
-
-		accumulator = accumulator == null ? value : Operator<T>.Add(accumulator, value);
-		return true;
-	}
 }
---
+++ System/src/Operators/StructNullOperator.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Operators;
+
+public sealed class StructNullOperator<T> : INullOperator<T>
+	where T : struct
+{
+	public bool HasValue(T value) => true;
+
+	public bool AddIfNotNull(ref T accumulator, T value)
+	{
+		accumulator = Operator<T>.Add(accumulator, value);
+		return true;
+	}
+}
