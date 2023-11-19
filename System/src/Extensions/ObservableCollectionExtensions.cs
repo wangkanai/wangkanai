1d900e50f5e21fd432d8e78bb42799e8296ab570
wangkanai <sarin@wangkanai.com>
2023-11-19 23:06:51 +0700

    fixed warning CS8602: Dereference of a possibly null reference

--- System/src/Extensions/ObservableCollectionExtensions.cs
+++ System/src/Extensions/ObservableCollectionExtensions.cs
@@ -7,15 +7,15 @@ namespace Wangkanai.Extensions;
 
 public static class ObservableCollectionExtensions
 {
-	public static void Observe<T>(this ObservableCollection<T> collection, Action<T> addAction, Action<T> removeAction)
+	public static void Observe<T>(this ObservableCollection<T> collection, Action<T>? addAction, Action<T>? removeAction)
 	{
 		collection.CollectionChanged += (sender, args) => {
 			if (args.Action == NotifyCollectionChangedAction.Add && addAction != null)
-				foreach (var newItem in args.NewItems)
+				foreach (var newItem in args.NewItems!)
 					addAction((T)newItem);
 
 			if (args.Action == NotifyCollectionChangedAction.Remove && removeAction != null)
-				foreach (var removeItem in args.OldItems)
+				foreach (var removeItem in args.OldItems!)
 					removeAction((T)removeItem);
 		};
 	}
