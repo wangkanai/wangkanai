d04692de81bbb16a27102725b3cdb070d78f49dc
wangkanai <sarin@wangkanai.com>
2023-11-20 10:11:31 +0700

    complete local build scripts

--- Blazor/build.ps1
+++ Blazor/build.ps1
@@ -1,8 +1,8 @@
 dotnet --version
-dotnet clean   .\src\ -c Release -tl
-dotnet restore .\src\
-dotnet build   .\src\ -c Release -tl
+dotnet clean   .\src\Core -c Release -tl
+dotnet restore .\src\Core
+dotnet build   .\src\Core -c Release -tl
 
-dotnet clean   .\tests\ -c Release -tl
+dotnet clean   .\tests\   -c Release -tl
 dotnet restore .\tests\
-dotnet build   .\tests\ -c Release -tl
+dotnet build   .\tests\   -c Release -tl
--- Federation/build.ps1
+++ Federation/build.ps1
@@ -1,7 +1,7 @@
 dotnet --version
-dotnet clean   .\src\ -c Release -tl
-dotnet restore .\src\
-dotnet build   .\src\ -c Release -tl
+dotnet clean   .\src\Federation -c Release -tl
+dotnet restore .\src\Federation
+dotnet build   .\src\Federation -c Release -tl
 
 dotnet clean   .\tests\ -c Release -tl
 dotnet restore .\tests\
