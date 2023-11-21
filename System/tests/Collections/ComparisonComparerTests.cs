76f1d88ee11cd4fa7316be75ce18ba3f97b73020
wangkanai <sarin@wangkanai.com>
2023-11-21 10:59:33 +0700

    refactor collection comparison comparer tests

--- System/tests/Collections/ComparisonComparerTests.cs
+++ System/tests/Collections/ComparisonComparerTests.cs
@@ -8,28 +8,16 @@ namespace Wangkanai.Collections;
 
 public class ComparisonComparerTests
 {
-	[Fact]
-	public void ConstructorWithNull()
-	{
-		Assert.Throws<ArgumentNullException>(() => new ComparisonComparer<string>(null));
-	}
-
-	[Fact]
-	public void CreateComparisonWithNull()
-	{
-		Assert.Throws<ArgumentNullException>(() => ComparisonComparer<string>.CreateComparison(null));
-	}
+	[Fact] public void ConstructorWithNull() => Assert.Throws<ArgumentNullException>(() => new ComparisonComparer<string>(null));
+	[Fact] public void CreateComparisonWithNull() => Assert.Throws<ArgumentNullException>(() => ComparisonComparer<string>.CreateComparison(null));
 
 	[Fact]
 	public void CreateAndCall()
 	{
 		var mock = new Mock<Comparison<string>>();
-		mock.Setup(x => x("hello", "there"))
-		    .Returns(5);
-		mock.Setup(x => x("x", "y"))
-		    .Returns(-3);
-		mock.Setup(x => x("throw", "exception"))
-		    .Throws<Exception>();
+		mock.Setup(x => x("hello", "there")).Returns(5);
+		mock.Setup(x => x("x", "y")).Returns(-3);
+		mock.Setup(x => x("throw", "exception")).Throws<Exception>();
 
 		var comparer = new ComparisonComparer<string>(mock.Object);
 		Assert.Equal(5, comparer.Compare("hello", "there"));
@@ -41,12 +29,9 @@ public class ComparisonComparerTests
 	public void CreateAndCallComparison()
 	{
 		var mock = new Mock<IComparer<string>>();
-		mock.Setup(x => x.Compare("hello", "there"))
-		    .Returns(5);
-		mock.Setup(x => x.Compare("x", "y"))
-		    .Returns(-3);
-		mock.Setup(x => x.Compare("throw", "exception"))
-		    .Throws<Exception>();
+		mock.Setup(x => x.Compare("hello", "there")).Returns(5);
+		mock.Setup(x => x.Compare("x", "y")).Returns(-3);
+		mock.Setup(x => x.Compare("throw", "exception")).Throws<Exception>();
 
 		var comparison = ComparisonComparer<string>.CreateComparison(mock.Object);
 		Assert.Equal(5, comparison("hello", "there"));
