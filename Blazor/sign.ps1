11bdef9ea54cdeee479c034a3a801cfff491766f
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-11-23 12:39:30 +0700

    Sign delete artifacts folder mute does not exist
    Do need to show if the folder going to delete exist or not. Just force delete silently.

--- Blazor/sign.ps1
+++ Blazor/sign.ps1
@@ -3,8 +3,8 @@ param(
     [bool]$dryrun=$false
 )
 
-remove-item -path .\signed\*.*    -Force
-remove-item -path .\artifacts\*.* -Force
+remove-item -path .\signed\*.*    -Force | out-null
+remove-item -path .\artifacts\*.* -Force | out-null
 
 new-item -Path artifacts -ItemType Directory -Force | out-null
 new-item -Path signed    -ItemType Directory -Force | out-null
