93bcab8e03becefc789de2a0ea82e37de1a12afe
wangkanai <sarinwangkanai@outlook.com>
2023-12-13 09:46:12 +0700

    Add NotNullAttribute class
    A new NotNullAttribute class has been added to the Attributes folder in the System source code. This class is sealed and derives from the Attribute class, and it can be used on all attribute targets.

---
+++ System/src/Attributes/NotNullAttribute.cs
@@ -0,0 +1,6 @@
+// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai;
+
+[AttributeUsage(AttributeTargets.All)]
+public sealed class NotNullAttribute : Attribute { }
