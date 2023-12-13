2a67b2860f572dc0d62f3b3469238396d96fdb8b
wangkanai <sarinwangkanai@outlook.com>
2023-12-13 09:33:30 +0700

    Add additional tests to ValueObjectTests class
    Test cases have been added to ensure correct behavior of value objects in different scenarios. They cover various use cases including equality checking, null handling, type comparisons, and hash code generation. Changes were also made to the Address class structure for better testing compatibility.

--- Domain/tests/ValueObjectTests.cs
+++ Domain/tests/ValueObjectTests.cs
@@ -11,13 +11,60 @@ public class ValueObjectTests
 		var value2 = new Address("123 Main St", "Redmond", "WA", "98052");
 		Assert.True(value1.Equals(value2));
 	}
+
+	[Fact]
+	public void ValueObject_Equals_ShouldBeFalse()
+	{
+		var value1 = new Address("123 Main St", "Redmond", "WA", "98052");
+		var value2 = new Address("123 Main St", "Redmond", "WA", "98053");
+		Assert.False(value1.Equals(value2));
+	}
+
+	[Fact]
+	public void ValueObject_Equals_ShouldBeFalse_WhenNull()
+	{
+		var value1 = new Address("123 Main St", "Redmond", "WA", "98052");
+		Assert.False(value1.Equals(null));
+	}
+
+	[Fact]
+	public void ValueObject_Equals_ShouldBeFalse_WhenDifferentType()
+	{
+		var value1 = new Address("123 Main St", "Redmond", "WA", "98052");
+		Assert.False(value1.Equals(new object()));
+	}
+
+	[Fact]
+	public void ValueObject_GetHashCode_ShouldBeTrue()
+	{
+		var value1 = new Address("123 Main St", "Redmond", "WA", "98052");
+		var value2 = new Address("123 Main St", "Redmond", "WA", "98052");
+		Assert.Equal(value1.GetHashCode(), value2.GetHashCode());
+	}
+
+	[Fact]
+	public void ValueObject_GetHashCode_ShouldBeFalse()
+	{
+		var value1 = new Address("123 Main St", "Redmond", "WA", "98052");
+		var value2 = new Address("123 Main St", "Redmond", "WA", "98053");
+		Assert.NotEqual(value1.GetHashCode(), value2.GetHashCode());
+	}
 }
 
-public class Address(string street, string city, string state, string zip)
-	: ValueObject
+public class Address : ValueObject
 {
-	public string Street { get; set; } = street;
-	public string City   { get; set; } = city;
-	public string State  { get; set; } = state;
-	public string Zip    { get; set; } = zip;
+	public Address() { }
+
+	public Address(string street, string city, string state, string zip) : this()
+	{
+		Street = street;
+		City   = city;
+		State  = state;
+		Zip    = zip;
+	}
+
+	public string Street { get; set; }
+	public string City   { get; set; }
+	public string State  { get; set; }
+	public string Zip    { get; set; }
 }
