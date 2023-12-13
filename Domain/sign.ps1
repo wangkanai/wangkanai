9e5857209b230b586840abdd55e0e68024012eb4
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-12-13 21:54:54 +0700

    Update signing script and version number
    The signing process in sign.ps1 script has been updated to include visual feedback on signing progress and to use a new timestamp URL. Additionally, the version number in the Directory.Build.props file is incremented from 3.10.0 to 3.11.0.

--- Domain/sign.ps1
+++ Domain/sign.ps1
@@ -13,18 +13,20 @@ dotnet --version
 dotnet clean   .\src\ -c Release -tl
 dotnet restore .\src\
 dotnet build   .\src\ -c Release -tl
-Get-ChildItem  .\src\ -Recurse Wangkanai.*.dll | where { $_.Name -like "*release*" } | foreach {
-    signtool sign /fd SHA256 /n "Sarin Na Wangkanai" $_.FullName
+Get-ChildItem  .\src\ -Recurse Wangkanai.*.dll | where { $_.FullName -like "*Release*" } | foreach {
+	write-host "Signing" $_.FullName -ForegroundColor Green;
+    signtool sign /fd sha256 /tr http://ts.ssl.com /td sha256 /n "Sarin Na Wangkanai" $_.FullName
+	#signtool verify /pa $_.FullName
 }
-dotnet pack .\src\ -c Release -tl -o .\artifacts --include-symbols -p:SymbolPackageFormat=snupkg
+#dotnet pack .\src\ -c Release -tl -o .\artifacts --include-symbols -p:SymbolPackageFormat=snupkg
 
-dotnet nuget sign .\artifacts\*.nupkg  -v normal --timestamper http://timestamp.digicert.com --certificate-subject-name "Sarin Na Wangkanai" -o .\signed
-dotnet nuget sign .\artifacts\*.snupkg -v normal --timestamper http://timestamp.digicert.com --certificate-subject-name "Sarin Na Wangkanai" -o .\signed
+#dotnet nuget sign .\artifacts\*.nupkg  -v normal --timestamper http://ts.ssl.com --certificate-subject-name "Sarin Na Wangkanai" -o .\signed
+#dotnet nuget sign .\artifacts\*.snupkg -v normal --timestamper http://ts.ssl.com --certificate-subject-name "Sarin Na Wangkanai" -o .\signed
 
 if ($dryrun)
 {
     write-host "Dryrun: Domain" -ForegroundColor Yellow;
     exit;
 }
-dotnet nuget push .\signed\*.nupkg --skip-duplicate -k $env:NUGET_API_KEY  -s https://api.nuget.org/v3/index.json
-dotnet nuget push .\signed\*.nupkg --skip-duplicate -k $env:GITHUB_API_PAT -s https://nuget.pkg.github.com/wangkanai/index.json
+#dotnet nuget push .\signed\*.nupkg --skip-duplicate -k $env:NUGET_API_KEY  -s https://api.nuget.org/v3/index.json
+#dotnet nuget push .\signed\*.nupkg --skip-duplicate -k $env:GITHUB_API_PAT -s https://nuget.pkg.github.com/wangkanai/index.json
