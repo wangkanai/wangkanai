e64a15c331db176507937d36f93654b16c0959ed
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2024-04-18 13:18:46 +0700

    Rename class and extend database functionality
    Renamed DatabaseMigrationExtensions to DatabaseBuilderExtensions and added two new methods, `CreateDatabase` and `CreateDatabaseAsync`. These methods enable creation of a database and its asynchronous version, extending the database's functionality. Commented out unused entity categories in sign.ps1.

--- EntityFramework/src/DatabaseBuilderExtensions.cs
+++ EntityFramework/src/DatabaseBuilderExtensions.cs
@@ -6,8 +6,34 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Wangkanai.EntityFramework;
 
-public static class DatabaseMigrationExtensions
+public static class DatabaseBuilderExtensions
 {
+	public static IApplicationBuilder CreateDatabase<T>(this IApplicationBuilder app)
+		where T : DbContext
+	{
+		using var scope   = app.ApplicationServices.CreateScope();
+		using var context = scope.ServiceProvider.GetRequiredService<T>();
+
+		context.ThrowIfNull();
+
+		context.Database.EnsureCreated();
+
+		return app;
+	}
+
+	public static async Task<IApplicationBuilder> CreateDatabaseAsync<T>(this IApplicationBuilder app)
+		where T : DbContext
+	{
+		await using var scope   = app.ApplicationServices.CreateAsyncScope();
+		await using var context = scope.ServiceProvider.GetRequiredService<T>();
+
+		context.ThrowIfNull();
+
+		await context.Database.EnsureCreatedAsync();
+
+		return app;
+	}
+
     public static IApplicationBuilder MigrateDatabase<T>(this IApplicationBuilder app)
         where T : DbContext
     {
