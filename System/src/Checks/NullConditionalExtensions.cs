cdac1cc288021e786002e864857ecd1464684b14
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2024-04-27 22:35:19 +0700

    Update null check methods in NullConditionalExtensions
    The null check methods TrueIfNull and FalseIfNull in the NullConditionalExtensions class have been updated. The input parameters no longer accept nullable types. Also, a warning for non-null parameters when exiting has been added.

--- System/src/Checks/NullConditionalExtensions.cs
+++ System/src/Checks/NullConditionalExtensions.cs
@@ -1,15 +1,15 @@
 // Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
+#pragma warning disable CS8777 // Parameter must have a non-null value when exiting.
+
 namespace Wangkanai;
 
 [DebuggerStepThrough]
 public static class NullConditionalExtensions
 {
-	[return: NotNull]
-	public static bool TrueIfNull<T>([NotNull] this T? value)
+	public static bool TrueIfNull<T>([NotNull] this T value)
 		=> value is null;
 
-	[return: NotNull]
-	public static bool FalseIfNull<T>([NotNull] this T? value)
+	public static bool FalseIfNull<T>([NotNull] this T value)
 		=> value is not null;
 }
