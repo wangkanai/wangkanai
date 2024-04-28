40807695d271cd8ebbfd86e52b945f2faa3d0268
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2024-04-28 17:49:35 +0700

    Add null and empty checks for Span extensions
    Introduced new utility methods namely `IsNull`, `IsEmpty` and `IsNullOrEmpty` to the `SpanExtensions` class to check if a span instance is null, empty, or either null or empty. Corresponding unit tests have also been added to ensure that the new methods function as expected.

--- System/src/Extensions/SpanExtensions.cs
+++ System/src/Extensions/SpanExtensions.cs
@@ -4,6 +4,12 @@ namespace Wangkanai.Extensions;
 
 public static class SpanExtensions
 {
+	public static bool IsNull<T>([NotNull] this Span<T> span)
+		=> span == null;
+
 	public static bool IsEmpty<T>([NotNull] this Span<T> span)
 		=> span.Length == 0;
+
+	public static bool IsNullOrEmpty<T>([NotNull] this Span<T> span)
+		=> span == null || span.Length == 0;
 }
