95a7b2d11194006f5003648c878d45ee2a024e30
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2024-04-30 01:19:16 +0700

    Add tests and improve BinaryScalingExtensions
    Implemented tests for the BinaryScalingExtensions. Enhanced the ToHumanReadable() method to handle negative numbers and short data type. Now, the method is more comprehensive and robust, handling different scenarios and uncovering any potential issues.

--- System/src/Extensions/BinaryScalingExtensions.cs
+++ System/src/Extensions/BinaryScalingExtensions.cs
@@ -7,6 +7,10 @@ public static class BinaryScalingExtensions
 	private static readonly string[] Sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
 	[DebuggerStepThrough]
+	public static string ToHumanReadable(this short length)
+		=> ((long)length).ToHumanReadable();
+
+	[DebuggerStepThrough]
 	public static string ToHumanReadable(this int length)
 		=> ((long)length).ToHumanReadable();
 
@@ -14,11 +18,21 @@ public static class BinaryScalingExtensions
 	public static string ToHumanReadable(this long length)
 	{
 		var order = 0;
-		while (length >= 1024 && order + 1 < Sizes.Length)
-		{
-			order++;
-			length /= 1024;
-		}
+		// positive
+		if (length >= 0)
+			while (length >= 1024 && order + 1 < Sizes.Length)
+			{
+				order++;
+				length /= 1024;
+			}
+
+		// negative
+		if (length < 0)
+			while (length <= -1024 && order + 1 < Sizes.Length)
+			{
+				order++;
+				length /= 1024;
+			}
 
 		return $"{length:0.##} {Sizes[order]}";
 	}
@@ -26,7 +40,7 @@ public static class BinaryScalingExtensions
 	[DebuggerStepThrough]
 	public static string ToHumanReadable(this ulong length)
 	{
-		int order = 0;
+		var order = 0;
 		while (length >= 1024 && order + 1 < Sizes.Length)
 		{
 			order++;
---
+++ System/tests/Extensions/BinaryScalingExtensionsTests.cs
@@ -0,0 +1,122 @@
+// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Extensions;
+
+using Xunit;
+
+using Wangkanai.Extensions;
+
+public class BinaryScalingExtensionsTests
+{
+	[Fact]
+	public void ConvertsBytesToHumanReadableFormat()
+	{
+		var value    = 1024; // 1 KB in bytes
+		var expected = "1 KB";
+		Assert.Equal(expected, value.ToHumanReadable());
+	}
+
+	[Fact]
+	public void ConvertsLargeNumberToHumanReadableFormat()
+	{
+		var value    = 1048576; // 1 MB in bytes
+		var expected = "1 MB";
+		Assert.Equal(expected, value.ToHumanReadable());
+	}
+
+	[Fact]
+	public void ConvertsZeroBytesToHumanReadableFormat()
+	{
+		var value    = 0;
+		var expected = "0 B";
+		Assert.Equal(expected, value.ToHumanReadable());
+	}
+
+	[Fact]
+	public void ConvertsNegativeNumber()
+	{
+		var value    = -1024;
+		var expected = "-1 KB";
+		Assert.Equal(expected, value.ToHumanReadable());
+	}
+
+	[Fact]
+	public void ConvertsNegativeLargeNumber()
+	{
+		var value    = -1048576;
+		var expected = "-1 MB";
+		Assert.Equal(expected, value.ToHumanReadable());
+	}
+
+	[Fact]
+	public void ConvertLongGiga()
+	{
+		var value    = 1024L * 1024L * 1024L; // 1 GB in bytes
+		var expected = "1 GB";
+		Assert.Equal(expected, value.ToHumanReadable());
+	}
+
+	[Fact]
+	public void ConvertLongTera()
+	{
+		var value    = 1024L * 1024L * 1024L * 1024L; // 1 TB in bytes
+		var expected = "1 TB";
+		Assert.Equal(expected, value.ToHumanReadable());
+	}
+
+	[Fact]
+	public void ConvertLongPeta()
+	{
+		var value    = 1024L * 1024L * 1024L * 1024L * 1024L; // 1 PB in bytes
+		var expected = "1 PB";
+		Assert.Equal(expected, value.ToHumanReadable());
+	}
+
+	[Fact]
+	public void ConvertLongExa()
+	{
+		var value    = 1024L * 1024L * 1024L * 1024L * 1024L * 1024L; // 1 EB in bytes
+		var expected = "1 EB";
+		Assert.Equal(expected, value.ToHumanReadable());
+	}
+
+	[Fact]
+	public void ConvertULongGiga()
+	{
+		var value    = 1024UL * 1024UL * 1024UL; // 1 GB in bytes
+		var expected = "1 GB";
+		Assert.Equal(expected, value.ToHumanReadable());
+	}
+
+	[Fact]
+	public void ConvertULongTera()
+	{
+		var value    = 1024UL * 1024UL * 1024UL * 1024UL; // 1 TB in bytes
+		var expected = "1 TB";
+		Assert.Equal(expected, value.ToHumanReadable());
+	}
+
+	[Fact]
+	public void ConvertULongPeta()
+	{
+		var value    = 1024UL * 1024UL * 1024UL * 1024UL * 1024UL; // 1 PB in bytes
+		var expected = "1 PB";
+		Assert.Equal(expected, value.ToHumanReadable());
+	}
+
+	[Fact]
+	public void ConvertULongExa()
+	{
+		var value    = 1024UL * 1024UL * 1024UL * 1024UL * 1024UL * 1024UL; // 1 EB in bytes
+		var expected = "1 EB";
+		Assert.Equal(expected, value.ToHumanReadable());
+	}
+
+	[Fact]
+	public void ConvertShortKilo()
+	{
+		var value    = (short)1024; // 1 KB in bytes
+		var expected = "1 KB";
+		Assert.Equal(expected, value.ToHumanReadable());
+	}
+}
