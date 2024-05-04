42cc10ff436e0fa55c182e027a9b5ec757b60628
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2024-05-04 21:37:40 +0700

    Add summary comments to ThrowIfEqualExtensions.cs
    Added summary comments to improve the documentation of the ThrowIfEqualExtensions class and its extension methods. This will improve understanding for users and maintainers of the code regarding its purpose and usage.

--- System/src/Checks/ThrowIfEqualExtensions.cs
+++ System/src/Checks/ThrowIfEqualExtensions.cs
@@ -4,16 +4,38 @@ using Wangkanai.Exceptions;
 
 namespace Wangkanai;
 
+/// <summary>
+/// Contains extension methods for throwing an exception if a value is equal to the expected value.
+/// </summary>
 [DebuggerStepThrough]
 public static class ThrowIfEqualExtensions
 {
+	/// <summary>
+	/// Throws an exception if the value is equal to the expected value.
+	/// </summary>
+	/// <param name="value">The value to compare.</param>
+	/// <param name="expected">The expected value.</param>
+	/// <returns>Returns false if the value is not equal to the expected value. Otherwise, throws an exception.</returns>
 	public static bool ThrowIfEqual([NotNull] this int value, int expected)
 		=> value.ThrowIfEqual<ArgumentEqualException>(expected, nameof(value));
 
+	/// <summary>
+	/// Throws an exception if the value is equal to the expected value.
+	/// </summary>
+	/// <param name="value">The value to compare.</param>
+	/// <param name="expected">The expected value.</param>
+	/// <returns>Returns false if the value is not equal to the expected value. Otherwise, throws an exception.</returns>
 	public static bool ThrowIfEqual<T>([NotNull] this int value, int expected)
 		where T : ArgumentException
 		=> value.ThrowIfEqual<T>(expected, nameof(value));
 
+	/// <summary>
+	/// Throws an exception if the value is equal to the expected value.
+	/// </summary>
+	/// <param name="value">The value to compare.</param>
+	/// <param name="expected">The expected value.</param>
+	/// <typeparam name="T">The generic instance.</typeparam>
+	/// <returns>Returns false if the value is not equal to the expected value. Otherwise, throws an exception.</returns>
 	public static bool ThrowIfEqual<T>([NotNull] this int value, int expected, string paramName)
 		where T : ArgumentException
 		=> value == expected
