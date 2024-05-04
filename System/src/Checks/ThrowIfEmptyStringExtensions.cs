8591b437c061a0c6ea6a3e714c77e882f57692dc
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2024-05-04 16:53:39 +0700

    Add detailed comments to ThrowIfEmpty and NullConditional extension methods
    Expanded comments for ThrowIfEmptyEnumerableExtensions, ThrowIfEmptyStringExtensions, and NullConditionalExtensions to provide more information about each method's functionality. This will help the users to understand the purpose and usage of these extension methods more clearly.

--- System/src/Checks/ThrowIfEmptyStringExtensions.cs
+++ System/src/Checks/ThrowIfEmptyStringExtensions.cs
@@ -5,27 +5,63 @@ using Wangkanai.Extensions;
 
 namespace Wangkanai;
 
+/// <summary>
+/// Contains extension methods for throwing exceptions when a string is empty.
+/// </summary>
 [DebuggerStepThrough]
 public static class ThrowIfEmptyStringExtensions
 {
+	/// <summary>
+	/// Throws an exception if the string is empty.
+	/// </summary>
+	/// <param name="value">The string to check.</param>
+	/// <returns>The input string if it is not empty.</returns>
 	public static string ThrowIfEmpty([NotNull] this string? value)
 		=> value.ThrowIfNull().ThrowIfEmpty<ArgumentEmptyException>();
 
+	/// <summary>
+	/// Throws an exception if the string is empty.
+	/// </summary>
+	/// <param name="value">The string to check.</param>
+	/// <param name="message">The exception error message.</param>
+	/// <returns>The input string if it is not empty.</returns>
+	/// <exception cref="ArgumentEmptyException">The string is empty.</exception>
 	public static string ThrowIfEmpty([NotNull] this string? value, string message)
 		=> value.ThrowIfNull().ThrowIfEmpty<ArgumentEmptyException>(message);
 
+	/// <summary>
+	/// Throws an exception if the string is empty.
+	/// </summary>
+	/// <param name="value">The string to check.</param>
+	/// <returns>The input string if it is not empty.</returns>
 	public static string ThrowIfEmpty<T>([NotNull] this string? value)
 		where T : ArgumentException
 		=> value.ThrowIfNull<T>().IsEmpty()
 			   ? throw ExceptionActivator.CreateArgumentInstance<T>(nameof(value))
 			   : value!;
 
+	/// <summary>
+	/// Throws an exception if the string is empty.
+	/// </summary>
+	/// <param name="value">The string to check.</param>
+	/// <param name="message">The exception error message.</param>
+	/// <returns>The input string if it is not empty.</returns>
+	/// <typeparam name="T">The type of the exception to throw.</typeparam>
+	/// <exception cref="T">The string is empty.</exception>
 	public static string ThrowIfEmpty<T>([NotNull] this string? value, string message)
 		where T : ArgumentException
 		=> value.ThrowIfNull<T>().IsEmpty()
 			   ? throw ExceptionActivator.CreateArgumentInstance<T>(nameof(value), message)
 			   : value!;
 
+	/// <summary>
+	/// Throws an exception if the string is empty.
+	/// </summary>
+	/// <param name="value">The string to check.</param>
+	/// <param name="message">The exception error meesage</param>
+	/// <param name="paramName">The name of the parameter.</param>
+	/// <returns>The input string if it is not empty.</returns>
+	/// <exception cref="T">The string is empty.</exception>
 	public static string ThrowIfEmpty<T>([NotNull] this string? value, string message, string paramName)
 		where T : ArgumentException
 		=> value.ThrowIfNull<T>().IsEmpty()
