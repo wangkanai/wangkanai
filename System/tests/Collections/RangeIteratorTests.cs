25a074ed2079c357864492588660ff0063907d82
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2024-05-05 14:03:56 +0700

    Refactor variable names in RangeIteratorTests
    Changed all camel case variable names to Pascal case in 'RangeIteratorTests.cs' as part of the ongoing effort to enforce naming conventions and improve code readability.

--- System/tests/Collections/RangeIteratorTests.cs
+++ System/tests/Collections/RangeIteratorTests.cs
@@ -4,69 +4,69 @@ namespace Wangkanai.Collections;
 
 public class RangeIteratorTests
 {
-	private static readonly int[] zero_four = { 0, 1, 2, 3, 4 };
-	private static readonly int[] zero_five = { 0, 1, 2, 3, 4, 5 };
-	private static readonly int[] one_four  = { 1, 2, 3, 4 };
-	private static readonly int[] one_five  = { 1, 2, 3, 4, 5 };
-	private static readonly int[] five_one  = { 5, 4, 3, 2, 1 };
-	private static readonly int[] five_zero = { 5, 4, 3, 2, 1, 0 };
-	private static readonly int[] four_one  = { 4, 3, 2, 1 };
-	private static readonly int[] four_zero = { 4, 3, 2, 1, 0 };
+	private static readonly int[] ZeroFour = [0, 1, 2, 3, 4];
+	private static readonly int[] ZeroFive = [0, 1, 2, 3, 4, 5];
+	private static readonly int[] OneFour  = [1, 2, 3, 4];
+	private static readonly int[] OneFive  = [1, 2, 3, 4, 5];
+	private static readonly int[] FiveOne  = [5, 4, 3, 2, 1];
+	private static readonly int[] FiveZero = [5, 4, 3, 2, 1, 0];
+	private static readonly int[] FourOne  = [4, 3, 2, 1];
+	private static readonly int[] FourZero = [4, 3, 2, 1, 0];
 
 	[Fact]
 	public void InclusiveRange()
 	{
 		var subject = new RangeIterator<int>(new Range<int>(0, 5), x => x + 1);
-		Assert.True(subject.SequenceEqual(zero_five));
+		Assert.True(subject.SequenceEqual(ZeroFive));
 	}
 
 	[Fact]
 	public void RangeExcludingStart()
 	{
 		var subject = new RangeIterator<int>(new Range<int>(0, 5).ExcludeStart(), x => x + 1);
-		Assert.True(subject.SequenceEqual(one_five));
+		Assert.True(subject.SequenceEqual(OneFive));
 	}
 
 	[Fact]
 	public void RangeExcludingEnd()
 	{
 		var subject = new RangeIterator<int>(new Range<int>(0, 5).ExcludeEnd(), x => x + 1);
-		Assert.True(subject.SequenceEqual(zero_four));
+		Assert.True(subject.SequenceEqual(ZeroFour));
 	}
 
 	[Fact]
 	public void RangeExcludingBoth()
 	{
 		var subject = new RangeIterator<int>(new Range<int>(0, 5).ExcludeStart().ExcludeEnd(), x => x + 1);
-		Assert.True(subject.SequenceEqual(one_four));
+		Assert.True(subject.SequenceEqual(OneFour));
 	}
 
 	[Fact]
 	public void DescendingInclusiveRange()
 	{
 		var subject = new RangeIterator<int>(new Range<int>(0, 5), x => x - 1, false);
-		Assert.True(subject.SequenceEqual(five_zero));
+		Assert.True(subject.SequenceEqual(FiveZero));
 	}
 
 	[Fact]
 	public void DescendingRangeExcludingStart()
 	{
 		var subject = new RangeIterator<int>(new Range<int>(0, 5).ExcludeStart(), x => x - 1, false);
-		Assert.True(subject.SequenceEqual(five_one));
+		Assert.True(subject.SequenceEqual(FiveOne));
 	}
 
 	[Fact]
 	public void DescendingRangeExcludingEnd()
 	{
 		var subject = new RangeIterator<int>(new Range<int>(0, 5).ExcludeEnd(), x => x - 1, false);
-		Assert.True(subject.SequenceEqual(four_zero));
+		Assert.True(subject.SequenceEqual(FourZero));
 	}
 
 	[Fact]
 	public void DescendingRangeExcludingBoth()
 	{
 		var subject = new RangeIterator<int>(new Range<int>(0, 5).ExcludeStart().ExcludeEnd(), x => x - 1, false);
-		Assert.True(subject.SequenceEqual(four_one));
+		Assert.True(subject.SequenceEqual(FourOne));
 	}
 
 	[Fact]
