1aae2e56d0444dead1d6ea2db1ee74c1673f02c3
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2024-05-08 11:14:16 +0700

    Added summary comments for methods and properties in GenericTypeInfo and AbstractTypeFactory classes
    The GenericTypeInfo and AbstractTypeFactory classes have been updated with summary comments for each of their methods and properties to provide better understanding of their functionalities. This will improve maintainability of the code by providing clear descriptions of methods' and properties' purposes, their inputs, outputs and any side effects.

--- System/src/Domain/GenericTypeInfo.cs
+++ System/src/Domain/GenericTypeInfo.cs
@@ -4,6 +4,10 @@ using Wangkanai.Extensions;
 
 namespace Wangkanai.Domain;
 
+/// <summary>
+/// Represents generic type information.
+/// </summary>
+/// <typeparam name="TBaseType">The base type of the generic type.</typeparam>
 public class GenericTypeInfo<TBaseType>(Type type)
 {
 	public ICollection<object> Services    { get; private set; } = new List<object>();
@@ -13,44 +17,84 @@ public class GenericTypeInfo<TBaseType>(Type type)
 	public Func<TBaseType>?    Factory     { get; private set; }
 	public Action<TBaseType>?  SetupAction { get; private set; }
 
+	/// <summary>
+	/// Sets the type name for the generic type.
+	/// </summary>
+	/// <param name="name">The name of the type.</param>
+	/// <returns>The modified GenericTypeInfo instance.</returns>
 	public GenericTypeInfo<TBaseType> WithTypeName(string name)
 	{
 		TypeName = name;
 		return this;
 	}
 
+	/// <summary>
+	/// Returns the service of type T from the collection of services.
+	/// </summary>
+	/// <typeparam name="T">The type of the service to retrieve.</typeparam>
+	/// <returns>The service of type T if it exists in the collection, otherwise null.</returns>
 	public T? GetService<T>()
 		=> Services.OfType<T>().FirstOrDefault();
 
+	/// <summary>
+	/// Retrieves all subclasses of the generic type.
+	/// </summary>
+	/// <typeparam name="TBaseType">The base type of the generic type.</typeparam>
+	/// <returns>An IEnumerable of Type objects representing the subclasses of the generic type.</returns>
 	public IEnumerable<Type> GetAllSubclasses()
 		=> (Type[])Type.GetTypeInheritanceChainTo(typeof(TBaseType)).Clone();
 
+	/// <summary>
+	/// Determines whether the current GenericTypeInfo is assignable to a given type name.
+	/// </summary>
+	/// <param name="typeName">The type name to check.</param>
+	/// <returns>True if the GenericTypeInfo is assignable to the given type name; otherwise, false.</returns>
 	public bool IsAssignableTo(string typeName)
 		=> Type.GetTypeInheritanceChainTo(typeof(TBaseType))
 		       .Concat(new[] { typeof(TBaseType) })
 		       .Any(t => typeName.EqualsInvariant(t.Name));
 
+	/// <summary>
+	/// Adds a service to the GenericTypeInfo instance.
+	/// </summary>
+	/// <typeparam name="T">The type of the service.</typeparam>
+	/// <param name="service">The service instance to be added.</param>
+	/// <returns>The modified GenericTypeInfo instance.</returns>
 	public GenericTypeInfo<TBaseType> WithService<T>(T service)
 	{
 		service.ThrowIfNull();
 		if (!Services.Contains(service!))
 			Services.Add(service!);
-
 		return this;
 	}
 
+	/// <summary>
+	/// Maps the generic type to a specified type.
+	/// </summary>
+	/// <typeparam name="T">The type to map the generic type to.</typeparam>
+	/// <returns>The modified GenericTypeInfo instance.</returns>
 	public GenericTypeInfo<TBaseType> MapToType<T>()
 	{
 		MappedType = typeof(T);
 		return this;
 	}
 
+	/// <summary>
+	/// Sets a factory method for creating an instance of the generic type.
+	/// </summary>
+	/// <param name="factory">The factory method.</param>
+	/// <returns>The modified GenericTypeInfo instance.</returns>
 	public GenericTypeInfo<TBaseType> WithFactory(Func<TBaseType> factory)
 	{
 		Factory = factory;
 		return this;
 	}
 
+	/// <summary>
+	/// Sets the setup action for the generic type.
+	/// </summary>
+	/// <param name="setupAction">The setup action to be invoked when setting up the generic type.</param>
+	/// <returns>The modified GenericTypeInfo instance.</returns>
 	public GenericTypeInfo<TBaseType> WithSetupAction(Action<TBaseType> setupAction)
 	{
 		SetupAction = setupAction;
