da6d0de53ced54ad53f606df43c4a92bb5950888
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2024-05-08 11:39:50 +0700

    Add detailed documentation and move attributes in Extensions classes
    The commit moves the DebuggerStepThrough attribute to class level and adds XML documentation comments explaining the purpose and use of methods in DictionaryExtensions, ComparerExtensions, and DateTimeExtensions classes. The changes kick off a broader effort to improve readability and accessibility of the codebase to new developers.

--- System/src/Extensions/DateTimeExtensions.cs
+++ System/src/Extensions/DateTimeExtensions.cs
@@ -2,8 +2,18 @@
 
 namespace Wangkanai.Extensions;
 
+/// <summary>
+/// Provides extension methods for the DateTime class.
+/// </summary>
+[DebuggerStepThrough]
 public static class DateTimeExtensions
 {
+	/// <summary>
+	/// Truncates a DateTime value based on a given TimeSpan.
+	/// </summary>
+	/// <param name="dateTime">The DateTime value to be truncated.</param>
+	/// <param name="timeSpan">The TimeSpan value representing the unit to truncate to.</param>
+	/// <returns>The truncated DateTime value.</returns>
 	public static DateTime Truncate(this DateTime dateTime, TimeSpan timeSpan)
 		=> timeSpan != TimeSpan.Zero
 			   ? dateTime.AddTicks(-(dateTime.Ticks % timeSpan.Ticks)) // Or could throw an ArgumentException
