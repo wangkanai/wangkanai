3f80bba8718af2c3308dc582fd63a5cd80593fd7
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2024-05-08 10:54:31 +0700

    Add comments to "ThrowIfPositive" methods in ThrowIfPositiveExtensions class
    This commit adds summary comments to each of the methods in the ThrowIfPositiveExtensions class in the Wangkanai namespace. These comments provide more context about each method's functionality, which should facilitate easier comprehension and maintenance.

--- System/src/Checks/ThrowIfPositiveExtensions.cs
+++ System/src/Checks/ThrowIfPositiveExtensions.cs
@@ -4,19 +4,46 @@ using Wangkanai.Exceptions;
 
 namespace Wangkanai;
 
+/// <summary>
+/// Provides extension methods to throw an exception if a value is positive.
+/// </summary>
 [DebuggerStepThrough]
 public static class ThrowIfPositiveExtensions
 {
+	/// <summary>
+	/// Throws an exception if the specified value is positive.
+	/// </summary>
+	/// <param name="value">The value to check.</param>
+	/// <returns>The specified value if it is not positive.</returns>
 	public static int ThrowIfPositive([NotNull] this int value)
 		=> value.ThrowIfPositive<ArgumentPositiveException>();
 
+	/// <summary>
+	/// Throws an exception if the specified value is positive.
+	/// </summary>
+	/// <param name="value">The value to check.</param>
+	/// <param name="message">The custom error message of the exception.</param>
+	/// <returns>The specified value if it is not positive.</returns>
 	public static int ThrowIfPositive([NotNull] this int value, string message)
 		=> value.ThrowIfPositive<ArgumentPositiveException>(message);
 
+	/// <summary>
+	/// Throws an exception if the specified value is positive.
+	/// </summary>
+	/// <typeparam name="T">The type of exception should throw.</typeparam>
+	/// <param name="value">The value to check.</param>
+	/// <returns>The specified value if it is not positive.</returns>
 	public static int ThrowIfPositive<T>([NotNull] this int value)
 		where T : ArgumentException
 		=> value.ThrowIfPositive<T>(nameof(value));
 
+	/// <summary>
+	/// Throws an exception if the specified value is positive.
+	/// </summary>
+	/// <typeparam name="T">The type of exception should throw.</typeparam>
+	/// <param name="value">The value to check.</param>
+	/// <param name="message">The custom error message of the exception.</param>
+	/// <returns>The specified value if it is not positive.</returns>
 	public static int ThrowIfPositive<T>([NotNull] this int value, string message)
 		where T : ArgumentException
 		=> value > 0
