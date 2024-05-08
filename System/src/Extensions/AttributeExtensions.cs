d12d9a5f70310efb7bc9f6112ff110bf11de2a05
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2024-05-08 11:18:05 +0700

    Update copyright and add documentation in AttributeExtensions
    Updated the copyright from 2022 to 2024 in AttributeExtensions file. Added detailed description and summary for the class AttributeExtensions and the method SplitString in the form of XML comments.

--- System/src/Extensions/AttributeExtensions.cs
+++ System/src/Extensions/AttributeExtensions.cs
@@ -1,11 +1,21 @@
-﻿// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
 // ReSharper disable UseCollectionExpression
 namespace Wangkanai.Extensions;
 
+/// <summary>
+/// Provides extension methods for working with attributes.
+/// </summary>
+[DebuggerStepThrough]
 public static class AttributeExtensions
 {
-	[DebuggerStepThrough]
+	/// <summary>
+	/// Splits the given string into an array of strings based on a specified separator.
+	/// </summary>
+	/// <param name="attribute">The Attribute object calling this method.</param>
+	/// <param name="original">The original string to split.</param>
+	/// <param name="separator">The separator used to split the string.</param>
+	/// <returns>An array of strings after splitting the original string.</returns>
 	public static string[] SplitString(this Attribute attribute, string original, char separator)
 		=> string.IsNullOrEmpty(original)
 			   ? Array.Empty<string>()
