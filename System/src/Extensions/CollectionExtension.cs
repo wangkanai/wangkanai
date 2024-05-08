e55aa41b64bb8cd64002b2756b1a450dc5fd8993
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2024-05-08 11:29:00 +0700

    Add methods for collection manipulation in CollectionExtension
    Added methods like 'IsNullOrEmpty', 'AddDistinct', and 'Replace' in the CollectionExtension class to enhance the functionality of collection manipulation. These methods make it easy to perform common operations on a collection, like testing for null or empty collection, adding distinct items, or replacing all items in a collection.

--- System/src/Extensions/CollectionExtension.cs
+++ System/src/Extensions/CollectionExtension.cs
@@ -8,12 +8,30 @@ namespace Wangkanai.Extensions;
 [DebuggerStepThrough]
 public static class CollectionExtension
 {
+	/// <summary>
+	/// Determines whether the collection is null.
+	/// </summary>
+	/// <typeparam name="T">The type of objects within the collection.</typeparam>
+	/// <param name="list">The collection to check.</param>
+	/// <returns>True if the collection is null; otherwise, false.</returns>
 	public static bool IsNull<T>(this ICollection<T>? list)
 		=> list is null;
 
+	/// <summary>
+	/// Determines whether the collection is empty.
+	/// </summary>
+	/// <typeparam name="T">The type of objects within the collection.</typeparam>
+	/// <param name="list">The collection to check.</param>
+	/// <returns>True if the collection is empty or null; otherwise, false.</returns>
 	public static bool IsEmpty<T>(this ICollection<T>? list)
 		=> list is null || list.Count <= 0;
 
+	/// <summary>
+	/// Determines whether the collection is empty or null.
+	/// </summary>
+	/// <typeparam name="T">The type of objects within the collection.</typeparam>
+	/// <param name="list">The collection to check.</param>
+	/// <returns>True if the collection is empty or null; otherwise, false.</returns>
 	public static bool IsNullOrEmpty<T>(this ICollection<T>? list)
 		=> list is null || list.Count <= 0;
 
@@ -24,7 +42,7 @@ public static class CollectionExtension
 	/// <param name="items">The collection to add items to.</param>
 	/// <typeparam name="T">the collection.</typeparam>
 	/// <returns>The collection.</returns>
-	/// <exception cref="System.ArgumentNullException">An <see cref="System.ArgumentNullException"/> is thrown if <paramref name="list"/> or <paramref name="items"/> is <see langword="null"/>.</exception>
+	/// <exception cref="ArgumentNullException">An <see cref="ArgumentNullException"/> is thrown if <paramref name="list"/> or <paramref name="items"/> is <see langword="null"/>.</exception>
 	public static ICollection<T> AddRangeSafe<T>(this ICollection<T> list, IEnumerable<T> items)
 	{
 		list.ThrowIfNull();
@@ -36,9 +54,25 @@ public static class CollectionExtension
 		return list;
 	}
 
+	/// <summary>
+	/// Adds distinct items to the collection.
+	/// </summary>
+	/// <typeparam name="T">The type of objects within the collection.</typeparam>
+	/// <param name="list">The collection to add items to.</param>
+	/// <param name="items">The items to add.</param>
+	/// <returns>The modified collection with the distinct items added.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="list"/> or <paramref name="items"/> is null.</exception>
 	public static ICollection<T> AddDistinct<T>(this ICollection<T> list, params T[] items)
 		=> AddDistinct(list, null!, items);
 
+	/// <summary>
+	/// Adds distinct items to the collection.
+	/// </summary>
+	/// <param name="list">The collection to add the items to.</param>
+	/// <param name="comparer">An optional comparer to use for comparing items.</param>
+	/// <param name="items">The items to add to the collection.</param>
+	/// <typeparam name="T">The type of items in the collection.</typeparam>
+	/// <returns>The collection with the distinct items added.</returns>
 	public static ICollection<T> AddDistinct<T>(this ICollection<T> list, IEqualityComparer<T> comparer, params T[] items)
 	{
 		list.ThrowIfNull()
@@ -56,6 +90,13 @@ public static class CollectionExtension
 		return list;
 	}
 
+	/// <summary>
+	/// Replaces the items in the collection with the specified items.
+	/// </summary>
+	/// <typeparam name="T">The type of objects within the collection.</typeparam>
+	/// <param name="list">The collection to replace items in.</param>
+	/// <param name="items">The items to replace the collection with.</param>
+	/// <returns>The modified collection with the specified items.</returns>
 	public static ICollection<T> Replace<T>(this ICollection<T> list, IEnumerable<T> items)
 	{
 		list.ThrowIfNull().ThrowIfEmpty();
