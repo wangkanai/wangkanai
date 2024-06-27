d56f909fd492cdf7b00f361dcb3798455891a0a4
User name <email@example.com>
2024-06-27 14:35:55 +0700

    Refactor solver base codes

--- Solver/src/Builders/SolverBuilder.cs
+++ Solver/src/Builders/SolverBuilder.cs
@@ -1,11 +1,9 @@
-// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
 namespace Wangkanai.Solver;
 
 public partial class Solver
 {
 	public static Solver CreateSolver(SolverType type)
-	{
-		return new Solver(type);
-	}
+		=> new(type);
 }
--- Solver/src/Linear/VariableWrapper.cs
+++ Solver/src/Linear/VariableWrapper.cs
@@ -1,29 +1,21 @@
-// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
 namespace Wangkanai.Solver.Linear;
 
-public class VariableWrapper : LinearExpression
+public class VariableWrapper(Variable var)
+	: LinearExpression
 {
-	private readonly Variable _var;
-
-	public VariableWrapper(Variable var)
-	{
-		_var = var;
-	}
-
 	public override string ToString()
-	{
-		return _var.Name;
-	}
+		=> var.Name;
 
 	public override double DoVisit(Dictionary<Variable, double> coefficients, double multiplier)
 	{
 		if (multiplier == 0.0) return 0.0;
 
-		if (coefficients.ContainsKey(_var))
-			coefficients[_var] += multiplier;
+		if (coefficients.ContainsKey(var))
+			coefficients[var] += multiplier;
 		else
-			coefficients[_var] = multiplier;
+			coefficients[var] = multiplier;
 
 		return 0.0;
 	}
