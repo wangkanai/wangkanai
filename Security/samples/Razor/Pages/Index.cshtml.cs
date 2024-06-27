a38cd5e18db26e5bbc3aaed9085330dd8bc471a7
User name <email@example.com>
2024-06-27 14:28:02 +0700

    Update license header for security

--- Security/samples/Razor/Pages/Index.cshtml.cs
+++ Security/samples/Razor/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Wangkanai.Security.RazorApp.Pages;
 
