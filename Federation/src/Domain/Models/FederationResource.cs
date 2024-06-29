3343a1ac486d27a6cdd6d7ccd13bff871b53b7d4
User name <email@example.com>
2024-06-29 17:36:49 +0700

    Add new properties to FederationResource model
    Two new properties have been added to the FederationResource model: "Required", which specifies whether the user can deselect the scope of the consent screen, and "Emphasize", which defines if the consent screen will emphasize this scope. These additions improve the flexibility and user interaction with the consent screen.

--- Federation/src/Domain/Models/FederationResource.cs
+++ Federation/src/Domain/Models/FederationResource.cs
@@ -12,8 +12,26 @@ public class FederationResource : AbstractResource
 {
 	private string DebuggerDisplay => Name ?? $"{{{typeof(FederationResource)}}}";
 
+	/// <summary>
+	/// specifies whether the user can de-select the scope of the consent screen.
+	/// </summary>
+	public bool Required { get; set; }
+
+	/// <summary>
+	/// Specifies whether the consent screen will emphasize this scope.
+	/// </summary>
+	public bool Emphasize { get; set; } = false;
+
+	/// <summary>
+	/// initializes a new instance of the <see cref="FederationResource"/> class.
+	/// </summary>
 	public FederationResource() { }
 
+	/// <summary>
+	/// Initializes a new instance of the <see cref="FederationResource"/> class.
+	/// </summary>
+	/// <param name="name">The name</param>
+	/// <param name="claims">List of associated user claims that should be included when this resource is requested.</param>
 	public FederationResource(string name, IEnumerable<string> claims)
 		: this(name, name, claims) { }
 
