a3897b1a90f7829f00bf8b0751ed4d5476e03c15
User name <email@example.com>
2024-06-29 17:10:32 +0700

    Add documentation and constructors to FederationResource model
    In this commit, added docstrings for the properties in the 'Resource' class and expanded the 'FederationResource' class. Added constructors to the 'FederationResource' class for easier object creation and comment annotations to the 'Resource' properties to enhance their understanding.

--- Federation/src/Domain/Models/FederationResource.cs
+++ Federation/src/Domain/Models/FederationResource.cs
@@ -4,17 +4,40 @@ using System.Diagnostics;
 
 namespace Wangkanai.Federation.Models;
 
+/// <summary>
+/// Models the common data Api and identity resources.
+/// </summary>
 [DebuggerDisplay("{DebuggerDisplay,nq}")]
 public abstract class Resource
 {
 	private string DebuggerDisplay => Name ?? $"{{{typeof(Resource)}}}";
 
-	public bool   Enable          { get; set; } = true;
-	public string Name            { get; set; }
+	/// <summary>
+	/// Indicates if this resource is enabled. Defaults to true.
+	/// </summary>
+	public bool Enable { get; set; } = true;
+
+	/// <summary>
+	/// The unique name of the resource.
+	/// </summary>
+	public string Name { get; set; } = default!;
+
 	public string DisplayName     { get; set; }
 	public string Description     { get; set; }
 	public bool   ShowInDiscovery { get; set; } = true;
 
 	public ICollection<string>         UserClaims { get; set; } = new HashSet<string>();
 	public IDictionary<string, string> Properties { get; set; } = new Dictionary<string, string>();
+}
+
+public class FederationResource : Resource
+{
+	public FederationResource() { }
+	public FederationResource(string name) : this(name, name) { }
+
+	public FederationResource(string name, string displayName)
+	{
+		Name        = name;
+		DisplayName = displayName;
+	}
 }
