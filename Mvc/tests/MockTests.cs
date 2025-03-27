5eb5bdb73dc5eb4ca8ffcf0077abd1b64f09ed93
wangkanai <sarin@wangkanai.com>
2025-03-27 13:58:32 +0700

    Add MockTests.cs and remove version properties from tests
    Removed `AssemblyVersion` and `FileVersion` from the `Wangkanai.System.Tests.csproj` for cleaner configuration management. Added a new `MockTests.cs` file in the Mvc test project with a basic unit test for setup.

---
+++ Mvc/tests/MockTests.cs
@@ -0,0 +1,13 @@
+// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Mvc.Tests;
+
+public class MockTests
+{
+	[Fact]
+	public void MockTest()
+	{
+		var mock = 1;
+		Assert.Equal(1, mock);
+	}
+}
