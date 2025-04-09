e1e592a58e376ed4cff5ff523a6e3f37ee67eb3e
wangkanai <sarin@wangkanai.com>
2025-04-09 12:44:39 +0700

    Add TrailType enum and AuditTrail class
    Introduce the `TrailType` enum with values for None, Create, Update, and Delete to represent action types. Add the `AuditTrail` class, which includes a `TrailType` property for tracking changes.

---
+++ Domain/src/TrailType.cs
@@ -0,0 +1,11 @@
+// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Domain;
+
+public enum TrailType
+{
+	None   = 0,
+	Create = 1,
+	Update = 2,
+	Delete = 3
+}
