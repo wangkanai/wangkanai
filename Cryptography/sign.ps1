57fb567191693afe862db28862deda9a811f0c5e
wangkanai <sarin@wangkanai.com>
2025-05-20 13:30:26 +0700

    Update parameter from 'dryrun' to 'update' in sign scripts
    Replaced the 'dryrun' parameter with 'update' across Detection, Cryptography, and Blazor sign scripts for clarity and consistency. Updated conditional logic accordingly to reflect the parameter change.

--- Cryptography/sign.ps1
+++ Cryptography/sign.ps1
@@ -1,6 +1,6 @@
 param(
     [Parameter(mandatory=$false)]
-    [bool]$dryrun=$false,
+    [bool]$update=$false,
     [Parameter(mandatory=$false)]
     [string]$name="Open Source Developer, Sarin Na Wangkanai"
 )
@@ -24,7 +24,7 @@ dotnet pack .\src\ -c Release -tl -o .\artifacts --include-symbols -p:SymbolPack
 dotnet nuget sign .\artifacts\*.nupkg  -v normal --timestamper http://timestamp.digicert.com --certificate-subject-name $name -o .\signed
 dotnet nuget sign .\artifacts\*.snupkg -v normal --timestamper http://timestamp.digicert.com --certificate-subject-name $name -o .\signed
 
-if ($dryrun)
+if (!$update)
 {
     write-host "Dryrun: Cryptography" -ForegroundColor Yellow;
     exit;
