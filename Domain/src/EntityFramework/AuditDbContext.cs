2c455eaee9c36026e0e9b2fe48fe95d55d803dc8
wangkanai <sarin@wangkanai.com>
2025-05-29 11:04:05 +0700

    Add `AuditDbContext` and `IAuditDbContext` to support audit-aware database operations

---
+++ Domain/src/EntityFramework/AuditDbContext.cs
@@ -0,0 +1,18 @@
+// Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved. Apache License, Version 2.0
+
+namespace Wangkanai.Domain.EntityFramework;
+
+/// <summary>
+/// Represents an abstract database context that supports auditing capabilities.
+/// This class extends the standard DbContext and implements the IAuditDbContext interface,
+/// providing the necessary functionality to integrate auditing mechanisms in entity framework operations.
+/// </summary>
+public abstract partial class AuditDbContext : DbContext, IAuditDbContext
+{
+	/// <summary>
+	/// Gets the current database context instance.
+	/// Provides access to the underlying <see cref="DbContext"/> for performing database operations
+	/// and supporting audit-related functionalities in implementations of <see cref="IAuditDbContext"/>.
+	/// </summary>
+	public DbContext DbContext => this;
+}
