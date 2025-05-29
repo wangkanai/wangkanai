14a670c712ed0b70b3ccc07998c42b6ac62bdddb
wangkanai <sarin@wangkanai.com>
2025-05-29 14:08:41 +0700

    Remove unused `using` directive in `IQueryableAuditStore` for cleaner codebase.

--- Domain/src/Stores/IQueryableAuditStore.cs
+++ Domain/src/Stores/IQueryableAuditStore.cs
@@ -1,7 +1,5 @@
 // Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved. Apache License, Version 2.0
 
-using Microsoft.AspNetCore.Identity;
-
 namespace Wangkanai.Domain.Stores;
 
 /// <summary>Represents an abstraction for a queryable audit trail store, providing access to audit trail records for tracking changes in the system.</summary>
