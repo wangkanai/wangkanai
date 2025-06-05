3d98eb8fb9f989d9c72f10a7cdfa60e4f2027500
wangkanai <sarin@wangkanai.com>
2025-06-05 23:30:00 +0700

    [Tests] Remove commented and unused test cases in `PositiveIntegerAttributeTests` and `ZeroIntegerAttributeTests`

--- Annotations/tests/ZeroIntegerAttributeTests.cs
+++ Annotations/tests/ZeroIntegerAttributeTests.cs
@@ -21,46 +21,9 @@ public class ZeroIntegerAttributeTests
 		var attribute = argument!.GetCustomAttribute<ZeroIntegerAttribute>();
 		Assert.Null(attribute);
 	}
-
-	[Fact]
-	public void Parameter_Attribute_Exist()
-	{
-		// var method    = typeof(ZeroInteger).GetMethod(nameof(ZeroInteger.ParameterExit));
-		// var argument  = method!.GetParameters()[0];
-		// var attribute = argument!.GetCustomAttribute<ZeroIntegerAttribute>();
-		// var expected  = "The value must be zero integer.";
-		// Assert.NotNull(attribute);
-		// Assert.Equal(expected, attribute!.Message);
-	}
-
-	[Fact]
-	public void Parameter_Attribute_Message()
-	{
-		// var method    = typeof(ZeroInteger).GetMethod(nameof(ZeroInteger.ParameterMessage));
-		// var argument  = method!.GetParameters()[0];
-		// var attribute = argument!.GetCustomAttribute<ZeroIntegerAttribute>();
-		// var expected  = "message";
-		// Assert.NotNull(attribute);
-		// Assert.Equal(expected, attribute!.Message);
-	}
-
-	[Fact]
-	public void Parameter_Attribute_Error()
-	{
-		// var method    = typeof(ZeroInteger).GetMethod(nameof(ZeroInteger.ParameterError));
-		// var argument  = method!.GetParameters()[0];
-		// var attribute = argument!.GetCustomAttribute<ZeroIntegerAttribute>();
-		// var expected  = "error";
-		// Assert.NotNull(attribute);
-		// Assert.Equal(expected, attribute!.Message);
-		// Assert.True(attribute.IsError);
-	}
 }
 
 public class ZeroInteger
 {
-	public void ParameterDefault(int                            value) { }
-	public void ParameterExit([ZeroInteger]                 int value) { }
-	// public void ParameterMessage([ZeroInteger("message")] int value) { }
-	// public void ParameterError([ZeroInteger("error", true)] int value) { }
+	public void ParameterDefault(int value) { }
 }
