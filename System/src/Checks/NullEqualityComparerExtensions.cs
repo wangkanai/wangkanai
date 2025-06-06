e692bbe1381aefa4c9e67b4053bbe2a6762c1269
wangkanai <sarin@wangkanai.com>
2025-06-06 12:36:33 +0700

    [System] Add `NotNull` extension and integrate it into `BinarySearch` for clearer null checks

--- System/src/Checks/NullEqualityComparerExtensions.cs
+++ System/src/Checks/NullEqualityComparerExtensions.cs
@@ -11,4 +11,11 @@ public static class NullEqualityComparerExtensions
 	/// <returns>True if the specified value is null or the default for the type; otherwise, false.</returns>
 	public static bool EqualNull<T>(this T? value)
 		=> EqualityComparer<T>.Default.Equals(value, default);
+
+	/// <summary>Determines whether the specified value is not equal to null or the default value of the type.</summary>
+	/// <typeparam name="T">The type of the value to compare.</typeparam>
+	/// <param name="value">The value to be compared.</param>
+	/// <returns>True if the specified value is not null and not the default for the type; otherwise, false.</returns>
+	public static bool NotNull<T>(this T? value)
+		=> !value.EqualNull();
 }
