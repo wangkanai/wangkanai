25076403f77cbca5ac9c6be7a870c02636e68e2f
wangkanai <sarin@wangkanai.com>
2025-06-06 13:00:10 +0700

    [System] Simplify XML documentation and update copyright year for consistency

--- System/src/Decorator.cs
+++ System/src/Decorator.cs
@@ -1,10 +1,8 @@
-﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+﻿// Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved. Apache License, Version 2.0
 
 namespace Wangkanai;
 
-/// <summary>
-/// Represents a generic decorator that wraps an instance of a specified type.
-/// </summary>
+/// <summary>Represents a generic decorator that wraps an instance of a specified type.</summary>
 /// <param name="instance">The instance to be wrapped and implemented.</param>
 /// <typeparam name="TService">The type of the instance to be wrapped.</typeparam>
 public class Decorator<TService>(TService instance)
@@ -12,25 +10,19 @@ public class Decorator<TService>(TService instance)
 	public TService Instance { get; } = instance;
 }
 
-/// <summary>
-/// Represents a generic decorator that wraps an instance of a specified type.
-/// </summary>
+/// <summary>Represents a generic decorator that wraps an instance of a specified type.</summary>
 /// <param name="instance">The instance to be wrapped and implemented.</param>
 /// <typeparam name="TService">The type of the instance to be wrapped.</typeparam>
 /// <typeparam name="TImplementation">The type of the instance to be implemented.</typeparam>
 public class Decorator<TService, TImplementation>(TImplementation instance) : Decorator<TService>(instance)
 	where TImplementation : class, TService;
 
-/// <summary>
-/// Represents a generic decorator that wraps an instance of a specified type and implements <see cref="IDisposable"/>.
-/// </summary>
+/// <summary>Represents a generic decorator that wraps an instance of a specified type and implements <see cref="IDisposable"/>.</summary>
 /// <param name="instance">The instance to be wrapped and implemented.</param>
 /// <typeparam name="TService">The type of the instance to be wrapped.</typeparam>
 public class DisposableDecorator<TService>(TService instance) : Decorator<TService>(instance), IDisposable
 {
-	/// <summary>
-	/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-	/// </summary>
+	/// <summary>Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.</summary>
 	public void Dispose()
 	{
 		(Instance as IDisposable)?.Dispose();
