aaaf2c3f8cba3dd09fa5c7dce2ccc4b842b15b93
wangkanai <sarin@wangkanai.com>
2025-06-06 12:55:07 +0700

    [System] Simplify XML documentation formatting in `Math` for consistency and update copyright year

--- System/src/Math.cs
+++ System/src/Math.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2022 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+// Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved. Apache License, Version 2.0
 
 namespace Wangkanai;
 
@@ -7,90 +7,70 @@ namespace Wangkanai;
 /// </summary>
 public static partial class Math
 {
-	/// <summary>
-	/// Divides two byte values.
-	/// </summary>
+	/// <summary>Divides two byte values.</summary>
 	/// <param name="value">The dividend.</param>
 	/// <param name="divider">The divisor.</param>
 	/// <returns>The quotient of the division. If the divisor is 0, returns 0.</returns>
 	public static byte Divider(byte value, byte divider)
 		=> (byte)(divider != 0 ? value / divider : 0);
 
-	/// <summary>
-	/// Divides two sbyte values.
-	/// </summary>
+	/// <summary>Divides two sbyte values.</summary>
 	/// <param name="value">The dividend.</param>
 	/// <param name="divider">The divisor.</param>
 	/// <returns>The quotient of the division. If the divisor is 0, returns 0.</returns>
 	public static sbyte Divider(sbyte value, sbyte divider)
 		=> (sbyte)(divider != 0 ? value / divider : 0);
 
-	/// <summary>
-	/// Divides two short values.
-	/// </summary>
+	/// <summary>Divides two short values.</summary>
 	/// <param name="value">The dividend.</param>
 	/// <param name="divider">The divisor.</param>
 	/// <returns>The quotient of the division. If the divisor is 0, returns 0.</returns>
 	public static short Divider(short value, short divider)
 		=> (short)(divider != 0 ? value / divider : 0);
 
-	/// <summary>
-	/// Divides two int values.
-	/// </summary>
+	/// <summary>Divides two int values.</summary>
 	/// <param name="value">The dividend.</param>
 	/// <param name="divider">The divisor.</param>
 	/// <returns>The quotient of the division. If the divisor is 0, returns 0.</returns>
 	public static int Divider(int value, int divider)
 		=> divider != 0 ? value / divider : 0;
 
-	/// <summary>
-	/// Divides two uint values.
-	/// </summary>
+	/// <summary>Divides two uint values.</summary>
 	/// <param name="value">The dividend.</param>
 	/// <param name="divider">The divisor.</param>
 	/// <returns>The quotient of the division. If the divisor is 0, returns 0.</returns>
 	public static uint Divider(uint value, uint divider)
 		=> divider != 0 ? value / divider : 0;
 
-	/// <summary>
-	/// Divides two long values.
-	/// </summary>
+	/// <summary>Divides two long values.</summary>
 	/// <param name="value">The dividend.</param>
 	/// <param name="divider">The divisor.</param>
 	/// <returns>The quotient of the division. If the divisor is 0, returns 0.</returns>
 	public static long Divider(long value, long divider)
 		=> divider != 0 ? value / divider : 0;
 
-	/// <summary>
-	/// Divides two ulong values.
-	/// </summary>
+	/// <summary>Divides two ulong values. </summary>
 	/// <param name="value">The dividend.</param>
 	/// <param name="divider">The divisor.</param>
 	/// <returns>The quotient of the division. If the divisor is 0, returns 0.</returns>
 	public static ulong Divider(ulong value, ulong divider)
 		=> divider != 0 ? value / divider : 0;
 
-	/// <summary>
-	/// Divides two float values.
-	/// </summary>
+	/// <summary>Divides two float values.</summary>
 	/// <param name="value">The dividend.</param>
 	/// <param name="divider">The divisor.</param>
 	/// <returns>The quotient of the division. If the divisor is 0, returns 0.</returns>
 	public static float Divider(float value, float divider)
 		=> System.Math.Abs(divider) >= float.Epsilon ? value / divider : 0;
 
-	/// <summary>
-	/// Divides two double values.
-	/// </summary>
+	/// <summary>Divides two double values.</summary>
 	/// <param name="value">The dividend.</param>
 	/// <param name="divider">The divisor.</param>
 	/// <returns>The quotient of the division. If the divisor is 0, returns 0.</returns>
 	public static double Divider(double value, double divider)
 		=> System.Math.Abs(divider) >= double.Epsilon ? value / divider : 0;
 
-	/// <summary>
-	/// Divides two decimal values.
-	/// </summary>
+	/// <summary>Divides two decimal values.</summary>
 	/// <param name="value">The dividend.</param>
 	/// <param name="divider">The divisor.</param>
 	/// <returns>The quotient of the division. If the divisor is 0, returns 0.</returns>
