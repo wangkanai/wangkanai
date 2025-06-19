aae73b073a3ca788dae4d390c4b5e34f8f0a3d17
wangkanai <sarin@wangkanai.com>
2025-06-19 11:09:35 +0700

    [Refactor] Update namespace references and upgrade xUnit dependency to v3

--- Audit/src/Stores/AuditStore.cs
+++ Audit/src/Stores/AuditStore.cs
@@ -1,6 +1,6 @@
 // Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved. Apache License, Version 2.0
 
-using Wangkanai.Audit.Primitives;
+using Wangkanai.Domain.Primitives;
 
 namespace Wangkanai.Audit.Stores;
 
