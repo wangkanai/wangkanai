97c8f97d02f366717cda91a6cee850102087acda
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2025-07-16 07:19:38 +0700

    Rename WangkanaiMath class to Mathematical and update references in tests

--- System/src/Math.cs
+++ System/src/Math.cs
@@ -5,7 +5,7 @@ namespace Wangkanai;
 /// <summary>
 /// Provides mathematical operations.
 /// </summary>
-public static partial class WangkanaiMath
+public static partial class Mathematical
 {
 	/// <summary>Divides two byte values.</summary>
 	/// <param name="value">The dividend.</param>
