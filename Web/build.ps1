4b5e2d0bc969c6f5086e10226826c937523fafb4
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2025-08-31 15:49:29 +0700

    refactor: remove unused Web folder from solution file

--- Web/build.ps1
+++
@@ -1,8 +0,0 @@
-dotnet --version
-dotnet clean   .\src\ -c Release -tl
-dotnet restore .\src\
-dotnet build   .\src\ -c Release -tl
-
-dotnet clean   .\tests\ -c Release -tl
-dotnet restore .\tests\
-dotnet build   .\tests\ -c Release -tl
