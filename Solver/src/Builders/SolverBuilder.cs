b6b5b93a2eca6e5cdb1f692a52a2a8a6eaa678db
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2025-08-31 09:28:47 +0700

    refactor: Update configuration files and remove unused paths

--- Solver/src/Builders/SolverBuilder.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved.
-
-namespace Wangkanai.Solver;
-
-public partial class Solver
-{
-	public static Solver CreateSolver(SolverType type)
-		=> new(type);
-}
